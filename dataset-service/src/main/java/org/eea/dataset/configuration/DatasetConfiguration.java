e9ccdc1f1c211586169084b2df6bf27b75db0f82
jlanton <ext.jose.luis.anton@altia.es>
2019-04-18 09:32:06 +0200

    moved to configuration transaction management in data set. Corrected clob error during initialization of microservices

--- dataset-service/src/main/java/org/eea/dataset/configuration/DatasetConfiguration.java
+++ dataset-service/src/main/java/org/eea/dataset/configuration/DatasetConfiguration.java
@@ -4,23 +4,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.eea.dataset.multitenancy.MultiTenantDataSource;
 import org.eea.interfaces.controller.recordstore.RecordStoreController.RecordStoreControllerZull;
 import org.eea.interfaces.vo.recordstore.ConnectionDataVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,6 +28,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
 @EnableJpaRepositories(basePackages = "org.eea.dataset.persistence.repository")
+@EntityScan(basePackages = "org.eea.dataset.persistence.domain")
 public class DatasetConfiguration implements WebMvcConfigurer {
 
 
@@ -44,22 +41,7 @@ public class DatasetConfiguration implements WebMvcConfigurer {
   @Autowired
   private RecordStoreControllerZull recordStoreControllerZull;
 
-  /**
-   * Entity manager factory bean local container entity manager factory bean.
-   *
-   * @return the local container entity manager factory bean
-   */
-  @Bean
-  @Primary
-  public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
-    final LocalContainerEntityManagerFactoryBean lfb = new LocalContainerEntityManagerFactoryBean();
-    lfb.setDataSource(dataSource());
-    lfb.setPackagesToScan("org.eea.dataset.persistence.domain");
-    lfb.setJpaProperties(hibernateProps());
-    lfb.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    return lfb;
-  }
-
+ 
   /**
    * Data source data source.
    *
@@ -99,31 +81,12 @@ public class DatasetConfiguration implements WebMvcConfigurer {
   private Properties hibernateProps() {
     final Properties properties = new Properties();
     properties.setProperty(PROPERTY_DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
-    properties
-        .setProperty("spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation", "true");
+
     properties.setProperty("spring.jpa.database", "postgresql");
+    properties.setProperty("spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation",
+        "true");
     return properties;
   }
 
-  /**
-   * Entity manager factory entity manager factory.
-   *
-   * @return the entity manager factory
-   */
-  @Bean
-  public EntityManagerFactory entityManagerFactory() {
-    return entityManagerFactoryBean().getObject();
-  }
 
-  /**
-   * Transaction manager jpa transaction manager.
-   *
-   * @return the jpa transaction manager
-   */
-  @Bean
-  public JpaTransactionManager transactionManager() {
-    final JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(entityManagerFactory());
-    return transactionManager;
-  }
 }
