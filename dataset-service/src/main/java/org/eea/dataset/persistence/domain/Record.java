d42b45549503786d52ea25448fdfd3b016496df4
ruben.lozano <ruben.lozano@altia.es>
2019-05-10 09:31:10 +0200

    Data model changes : Adding Lombok and table names cleaning

--- dataset-service/src/main/java/org/eea/dataset/persistence/domain/Record.java
+++ dataset-service/src/main/java/org/eea/dataset/persistence/domain/Record.java
@@ -1,61 +1,44 @@
 package org.eea.dataset.persistence.domain;
 
 import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
-@Table(name = "record")
+@Getter
+@Setter
+@ToString
+@Table(name = "RECORD")
 public class Record {
 
+	@Id
+	@Column(name = "ID")
+	private Integer id;
+	@Column(name = "NAME")
+	private String name;
 
-  @Id
-  @Column(name = "id")
-  private Integer id;
-  @Column(name = "name")
-  private String name;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Record record = (Record) o;
+		return id.equals(record.id) && name.equals(record.name);
+	}
 
-  public Integer getId() {
-    return id;
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Record record = (Record) o;
-    return id.equals(record.id) &&
-        name.equals(record.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-  @Override
-  public String toString() {
-    return "Record{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        '}';
-  }
 }
