9d830e875c9244972373e98c5db21c5bde0cac29
jlanton <ext.jose.luis.anton@altia.es>
2019-05-10 10:02:31 +0200

    cleared Kafka Receiver

--- common-utitlities/src/main/java/org/eea/kafka/io/KafkaReceiver.java
+++ common-utitlities/src/main/java/org/eea/kafka/io/KafkaReceiver.java
@@ -1,6 +1,5 @@
 package org.eea.kafka.io;
 
-import java.util.Collection;
 import org.eea.kafka.domain.EEAEventVO;
 import org.eea.kafka.handler.EEAEventHandler;
 import org.slf4j.Logger;
@@ -10,6 +9,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+/**
+ * The type Kafka receiver.
+ */
 @Component
 public class KafkaReceiver {
 
@@ -18,25 +20,18 @@ public class KafkaReceiver {
 
   private static final Logger LOG = LoggerFactory.getLogger(KafkaReceiver.class);
 
-//  @KafkaListener( topicPartitions = @TopicPartition(topic = "Hello-Kafka2",
-//      partitionOffsets = {
-//          @PartitionOffset(partition = "0", initialOffset = "0")
-//      }))
-//@KafkaListener(topics = "Hello-Kafka2")
-  public void listen(Collection<EEAEventVO> messages) {
-    messages.stream().forEach(message -> System.out.println("Received Messasge in group foo: " + message));
 
-  }
-
-//  @KafkaListener( topicPartitions = @TopicPartition(topic = "Hello-Kafka2",
-//      partitionOffsets = {
-//          @PartitionOffset(partition = "0", initialOffset = "8")
-//      }))
-@KafkaListener(topics = "Hello-Kafka2")
-  public void listenMessage(Message<EEAEventVO> message) {
-  LOG.info("Received message {}",message.getPayload());
-
-  handler.processMessage(message.getPayload());
+  /**
+   * Listen message.
+   *
+   * @param message the message
+   */
+  @KafkaListener(topics = "Hello-Kafka2")
+  public void listenMessage(final Message<EEAEventVO> message) {
+    LOG.info("Received message {}", message.getPayload());
+    if (null != handler) {
+      handler.processMessage(message.getPayload());
+    }
 
   }
 }
