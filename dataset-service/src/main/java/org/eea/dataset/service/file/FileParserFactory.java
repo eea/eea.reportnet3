94e2c45f28a3bfc625804ecbe38c9b854a2bfdbe
Bea <bse.ies.so@gmail.com>
2019-05-14 15:42:23 +0200

    Fixed get type from file name

--- dataset-service/src/main/java/org/eea/dataset/service/file/FileParserFactory.java
+++ dataset-service/src/main/java/org/eea/dataset/service/file/FileParserFactory.java
@@ -3,19 +3,19 @@ package org.eea.dataset.service.file;
 import org.eea.dataset.service.file.interfaces.FileParseContext;
 
 public class FileParserFactory {
-	public static FileParseContext createContext(String mimeType) {
-		FileParseContextImpl context = null;
-		switch (mimeType) {
-		case "text/csv":
-			context = new FileParseContextImpl(new CSVReaderStrategy());
-			break;
-		case "application/xml":
-			context = new FileParseContextImpl(new XMLReaderStrategy());
-			break;
-		default:
-			context = null;
-			break;
-		}
-		return context;
-	}
+  public static FileParseContext createContext(String mimeType) {
+    FileParseContextImpl context = null;
+    switch (mimeType) {
+      case "csv":
+        context = new FileParseContextImpl(new CSVReaderStrategy());
+        break;
+      case "xml":
+        context = new FileParseContextImpl(new XMLReaderStrategy());
+        break;
+      default:
+        context = null;
+        break;
+    }
+    return context;
+  }
 }
--- dataset-service/src/main/java/org/eea/dataset/service/impl/DatasetServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/DatasetServiceImpl.java
@@ -2,16 +2,13 @@ package org.eea.dataset.service.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.sql.DataSource;
 import javax.transaction.Transactional;
-
 import org.eea.dataset.multitenancy.DatasetId;
 import org.eea.dataset.persistence.domain.Record;
 import org.eea.dataset.persistence.repository.RecordRepository;
@@ -29,7 +26,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import ch.qos.logback.core.ConsoleAppender;
 
 /**
@@ -38,79 +34,81 @@ import ch.qos.logback.core.ConsoleAppender;
 @Service("datasetService")
 public class DatasetServiceImpl implements DatasetService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(DatasetServiceImpl.class);
-	private static final Logger LOG_ERROR = LoggerFactory.getLogger("error_logger");
+  private static final Logger LOG = LoggerFactory.getLogger(DatasetServiceImpl.class);
+  private static final Logger LOG_ERROR = LoggerFactory.getLogger("error_logger");
 
-	@Autowired
-	private RecordRepository recordRepository;
+  @Autowired
+  private RecordRepository recordRepository;
 
-	@Autowired
-	private RecordStoreControllerZull recordStoreControllerZull;
+  @Autowired
+  private RecordStoreControllerZull recordStoreControllerZull;
 
-	@Autowired
-	private DataSource multiTenantDataSource;
+  @Autowired
+  private DataSource multiTenantDataSource;
 
-	@Autowired
-	private KafkaSender kafkaSender;
+  @Autowired
+  private KafkaSender kafkaSender;
 
-	@Override
-	public DataSetVO getDatasetById(@DatasetId final String datasetId) {
-		final DataSetVO dataset = new DataSetVO();
-		final List<RecordVO> recordVOs = new ArrayList<>();
-		LOG.info("devolviendo datos chulos {}", dataset);
-		LOG_ERROR.error("hola  {}", datasetId);
-		ConsoleAppender a;
-		final Date start = new Date();
-		final List<Record> records = recordRepository.specialFind(datasetId);
-		if (records.size() > 0) {
-			for (final Record record : records) {
-				final RecordVO vo = new RecordVO();
-				vo.setId(record.getId().toString());
-				vo.setName(record.getName());
-				recordVOs.add(vo);
-			}
-			dataset.setRecords(recordVOs);
-			dataset.setId(datasetId);
-		}
+  @Override
+  public DataSetVO getDatasetById(@DatasetId final String datasetId) {
+    final DataSetVO dataset = new DataSetVO();
+    final List<RecordVO> recordVOs = new ArrayList<>();
+    LOG.info("devolviendo datos chulos {}", dataset);
+    LOG_ERROR.error("hola  {}", datasetId);
+    ConsoleAppender a;
+    final Date start = new Date();
+    final List<Record> records = recordRepository.specialFind(datasetId);
+    if (records.size() > 0) {
+      for (final Record record : records) {
+        final RecordVO vo = new RecordVO();
+        vo.setId(record.getId().toString());
+        vo.setName(record.getName());
+        recordVOs.add(vo);
+      }
+      dataset.setRecords(recordVOs);
+      dataset.setId(datasetId);
+    }
 
-		return dataset;
-	}
+    return dataset;
+  }
 
-	@Override
-	@Transactional
-	public void addRecordToDataset(@DatasetId final String datasetId, final List<RecordVO> records) {
+  @Override
+  @Transactional
+  public void addRecordToDataset(@DatasetId final String datasetId, final List<RecordVO> records) {
 
-		for (final RecordVO recordVO : records) {
-			final Record r = new Record();
-			r.setId(Integer.valueOf(recordVO.getId()));
-			r.setName(recordVO.getName());
-			recordRepository.save(r);
-		}
+    for (final RecordVO recordVO : records) {
+      final Record r = new Record();
+      r.setId(Integer.valueOf(recordVO.getId()));
+      r.setName(recordVO.getName());
+      recordRepository.save(r);
+    }
 
-	}
+  }
 
-	@Override
-	public void createEmptyDataset(final String datasetName) {
-		recordStoreControllerZull.createEmptyDataset(datasetName);
-	}
+  @Override
+  public void createEmptyDataset(final String datasetName) {
+    recordStoreControllerZull.createEmptyDataset(datasetName);
+  }
 
-	@Override
-	public void processFile(MultipartFile file) throws IOException {
-		String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-		FileParseContext context = FileParserFactory.createContext(mimeType);
+  @Override
+  public void processFile(MultipartFile file) throws IOException {
+    // String mimeType = URLConnection.guessContentTypeFromName(file.getOriginalFilename());
+    int i = file.getOriginalFilename().lastIndexOf('.');
+    String mimeType = file.getOriginalFilename().substring(i + 1);
 
-		try (InputStream inputStream = file.getInputStream()) {
-			DataSetVO datasetVO = context.parse(inputStream);
-			sendMessage(EventType.DATASET_PARSED_FILE_EVENT, datasetVO);
-		}
-	}
-	
-	public void sendMessage(EventType eventType, Object data) {
-		EEAEventVO event = new EEAEventVO();
-		event.setEventType(eventType);
-		Map<String, Object> dataOutput = new HashMap<>();
-		dataOutput.put("data", data);
-		event.setData(dataOutput);
-		kafkaSender.sendMessage(event);
-	}
+    try (InputStream inputStream = file.getInputStream()) {
+      FileParseContext context = FileParserFactory.createContext(mimeType);
+      DataSetVO datasetVO = context.parse(inputStream);
+      sendMessage(EventType.DATASET_PARSED_FILE_EVENT, datasetVO);
+    }
+  }
+
+  public void sendMessage(EventType eventType, Object data) {
+    EEAEventVO event = new EEAEventVO();
+    event.setEventType(eventType);
+    Map<String, Object> dataOutput = new HashMap<>();
+    dataOutput.put("data", data);
+    event.setData(dataOutput);
+    kafkaSender.sendMessage(event);
+  }
 }
