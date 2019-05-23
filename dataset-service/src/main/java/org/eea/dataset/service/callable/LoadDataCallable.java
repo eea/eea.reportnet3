3b4c6e9ad520875c1271f54a2912ae99e9f3873e
jlanton <ext.jose.luis.anton@altia.es>
2019-05-23 14:07:16 +0200

    added javadoc

--- dataset-service/src/main/java/org/eea/dataset/service/callable/LoadDataCallable.java
+++ dataset-service/src/main/java/org/eea/dataset/service/callable/LoadDataCallable.java
@@ -9,12 +9,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * The type Load data callable.
+ */
 public class LoadDataCallable implements Callable<Void> {
 
   private final DatasetService datasetService;
   private final MultipartFile file;
   private final Long datasetId;
 
+  /**
+   * Instantiates a new Load data callable.
+   *
+   * @param datasetService the dataset service
+   * @param dataSetId the data set id
+   * @param file the file
+   */
   public LoadDataCallable(final DatasetService datasetService, final Long dataSetId,
       final MultipartFile file) {
     this.datasetService = datasetService;
@@ -26,12 +36,12 @@ public class LoadDataCallable implements Callable<Void> {
   public Void call() throws Exception {
     try {
       datasetService.processFile(datasetId, file);
-    } catch (EEAException e) {
+    } catch (final EEAException e) {
       if (e.getMessage().equals(EEAErrorMessage.FILE_FORMAT)
           || e.getMessage().equals(EEAErrorMessage.FILE_EXTENSION)) {
         throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage(), e);
       }
-    } catch (IOException e) {
+    } catch (final IOException e) {
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
     }
 
