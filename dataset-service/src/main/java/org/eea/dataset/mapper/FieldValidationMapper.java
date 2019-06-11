9c893d57036b11a25519e88b84f6267d6233792c
Bea <bse.ies.so@gmail.com>
2019-06-11 17:31:07 +0200

    Fixed Mapper

--- dataset-service/src/main/java/org/eea/dataset/mapper/FieldValidationMapper.java
+++ dataset-service/src/main/java/org/eea/dataset/mapper/FieldValidationMapper.java
@@ -4,9 +4,22 @@ import org.eea.dataset.persistence.data.domain.FieldValidation;
 import org.eea.interfaces.vo.dataset.FieldValidationVO;
 import org.eea.mapper.IMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring"
-)
+@Mapper(componentModel = "spring")
 public interface FieldValidationMapper extends IMapper<FieldValidation, FieldValidationVO> {
+
+
+  @Mapping(source = "fieldValue", target = "fieldValue", ignore = true)
+  FieldValidation classToEntity(FieldValidationVO vo);
+
+  /**
+   * Entity to class.
+   *
+   * @param entity the entity
+   * @return the t
+   */
+  @Mapping(source = "fieldValue", target = "fieldValue", ignore = true)
+  FieldValidationVO entityToClass(FieldValidation entity);
 
 }
