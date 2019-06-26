f198c846e33bc0ff089cb1898724dfa5d07d8e38
icojocaru <icojocaru-ext@tracasa.es>
2019-06-26 08:24:15 +0200

    add BreadCrumbs to DocumentationDataSet

--- frontend-service/src/components/Pages/DocumentationDataSet/DocumentationDataSet.jsx
+++ frontend-service/src/components/Pages/DocumentationDataSet/DocumentationDataSet.jsx
@@ -1,25 +1,41 @@
-import React ,{ useState, useEffect}from 'react'
+import React ,{ useState, useEffect, useContext}from 'react'
 import style from './DocumentationDataSet.module.scss';
 import MainLayout from '../../Layout/main-layout.component';
-
+import {BreadCrumb} from 'primereact/breadcrumb';
 import uploadDummy from '../../../assets/jsons/uploadDummy';
+import ResourcesContext from '../../Context/ResourcesContext';
 import { TabView, TabPanel } from "primereact/tabview";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
 
 const DocumentationDataSet = () => {
+
+    const resources = useContext(ResourcesContext);  
+
     const [file, setFile] = useState();
     const [cols, setCols] = useState();
-
+    const [breadCrumbItems,setBreadCrumbItems] = useState([]);
+    const home = {icon: resources.icons["home"], url: '#'};
+    //Data Fetching
     useEffect(() => {
         setFile(uploadDummy);
     }, [file]);
 
+    //Bread Crumbs settings
+    useEffect(()=>{
+        setBreadCrumbItems( [
+            {label: resources.messages["newDataset"], url: '#'},
+            {label: resources.messages["viewData"], url: '#'}
+        ]); 
+    } , [] )
+    
+      
+
     if (file) {
     return (
         <MainLayout>
-       
+         <BreadCrumb model={breadCrumbItems} home={home}/>
             <TabView>
 
                 <TabPanel header="Documents">
