3efbdd3cf0dba8cb16a54cf20332021b4086a390
Pablo Arrastia <parrastia-ext@tracasa.es>
2019-06-27 14:12:56 +0200

    add subscribe functionality

--- frontend-service/src/components/Layout/UI/DataFlowColumn/DataFlowColumn.js
+++ frontend-service/src/components/Layout/UI/DataFlowColumn/DataFlowColumn.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState, useEffect, useContext, Suspense} from 'react';
 import PropTypes from "prop-types";
 import { Button } from "primereact/button";
 import { IconComponent } from "./duck";
 import config from "./duck/config";
 import styles from "./DataFlowColumn.module.css";
+import resourcesContext from '../../../Context/ResourcesContext';
+import ConfirmDialog from '../ConfirmDialog/ConfirmDialog';
+import HTTPRequester from '../../../../services/HTTPRequester/HTTPRequester';
+import ButtonsBar from '../ButtonsBar/ButtonsBar';
 
 const DataFlowColumn = ({ navTitle, dataFlowTitle, search = false }) => {
+	const resources = useContext(resourcesContext);
+	const [subscribeDialogVisible, setSubscribeDialogVisible] = useState(false);
+
+	console.log('Start DataFlowColumn...');
+
+	const setVisibleHandler = (fnUseState, visible) => {
+		fnUseState(visible);
+	}
+
+	const onConfirmSubscribeHandler = () =>{
+		console.log('onConfirmSubscribeHandler');
+		setSubscribeDialogVisible(false);
+		HTTPRequester.get(
+			{url:'/subscribe/dataflow', queryString: {}}
+		);
+		console.log('/subscribe/dataflow');
+	}
+
 	return (
 		<div className="nav rep-col-12 rep-col-sm-3">
 			<h2 className={styles.title}>{navTitle}</h2>
@@ -33,7 +55,20 @@ const DataFlowColumn = ({ navTitle, dataFlowTitle, search = false }) => {
 					icon={config.icons.plus}
 					label="Suscribe to a data flow"
 					className="p-button-primary"
+					onClick={() => {
+						setVisibleHandler(setSubscribeDialogVisible, true);
+					}}
 				/>
+				<ConfirmDialog 	onConfirm={onConfirmSubscribeHandler}
+							onHide={() => setVisibleHandler(setSubscribeDialogVisible, false)}
+							visible={subscribeDialogVisible}
+							header={resources.messages['subscribeButton']}
+							maximizable={false}
+							labelConfirm={resources.messages["yes"]}
+							labelCancel={resources.messages["close"]}
+			>
+				{resources.messages["subscribeDataFlow"]}
+			</ConfirmDialog>
 			</div>
 		</div>
 	);
