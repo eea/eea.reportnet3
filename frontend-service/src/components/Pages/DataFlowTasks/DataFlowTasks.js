5343495ae2a42c6dc65e61d853a41445080d7c3a
mazpilic <mazpilicueta-ext@tracasa.es>
2019-06-28 10:24:25 +0200

    update data flow task to add a breadcrum bar and a submenu tab menu

--- frontend-service/src/components/Pages/DataFlowTasks/DataFlowTasks.js
+++ frontend-service/src/components/Pages/DataFlowTasks/DataFlowTasks.js
@@ -1,15 +1,30 @@
-import React, { useEffect, /* useContext,*/ useState } from "react";
+import React, { useEffect, useContext, useState } from "react";
+
 import styles from "./DataFlowTasks.module.scss";
-import DataFlowList from "./DataFlowList/DataFlowList";
+
 import DataFlaws from "../../../assets/jsons/DataFlaws.json";
+import ResourcesContext from "../../Context/ResourcesContext";
+
+import DataFlowList from "./DataFlowList/DataFlowList";
+
+import { BreadCrumb } from "primereact/breadcrumb";
 import MainLayout from "../../Layout/main-layout.component";
 import DataFlowColumn from "../../Layout/UI/DataFlowColumn/DataFlowColumn";
+import { TabMenu } from "primereact/tabmenu";
+
 //import HTTPRequesterAPI from '../../../services/HTTPRequester/HTTPRequester';
 //import ResourcesContext from '../../Context/ResourcesContext';
 
 const DataFlowTasks = () => {
+	const resources = useContext(ResourcesContext);
 	const [pendingDataFlows, setPendingDataFlows] = useState([]);
 	const [acceptetDataFlows, setAcceptedDataFlows] = useState([]);
+	const [tabMenuItems, setTabMenuItems] = useState([
+		{ label: "Pendding" },
+		{ label: "completed" }
+	]);
+	const [tabMenuActiveItem, setTabMenuActiveItem] = useState(tabMenuItems[0]);
+	const home = { icon: resources.icons["home"], url: "/" };
 
 	/*   const resources = useContext(ResourcesContext);  
   // This is here just for example purpose 
@@ -32,20 +47,29 @@ const DataFlowTasks = () => {
 
 	return (
 		<MainLayout>
+			<BreadCrumb
+				model={[{ label: "Reporting data flow", url: "" }]}
+				home={home}
+			/>
 			<div className="rep-container">
 				<div className="rep-row">
 					<DataFlowColumn navTitle="data flows" search={false} />
 					<div className="subscribe-df rep-col-xs-12 rep-col-md-9">
+						<TabMenu
+							model={tabMenuItems}
+							activeItem={tabMenuActiveItem}
+							onTabChange={e => setTabMenuActiveItem(e.value)}
+						/>
 						<DataFlowList
 							listContent={pendingDataFlows}
 							listType="pending"
-							listTitle="Pending data flows."
+							listTitle="Pending data flows"
 							listDescription="You are required to accept and report data to these data flows"
 						/>
 						<DataFlowList
 							listContent={acceptetDataFlows}
 							listType="accepted"
-							listTitle="My data flows."
+							listTitle="My data flows"
 							listDescription="Please proceed to report before deadline"
 						/>
 					</div>
