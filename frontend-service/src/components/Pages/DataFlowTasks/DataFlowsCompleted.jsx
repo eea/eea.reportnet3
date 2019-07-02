1b1015b17e41c1ff7044e6ed7ea73807ac82acb4
Igor Cojocaru <icojocaru-ext@tracasa.es>
2019-07-02 10:49:47 +0200

    update Data Flow Item CSS style on Hover

--- frontend-service/src/components/Pages/DataFlowTasks/DataFlowsCompleted.jsx
+++ frontend-service/src/components/Pages/DataFlowTasks/DataFlowsCompleted.jsx
@@ -1,28 +1,56 @@
 import React, { useEffect, useContext, useState } from "react";
-//import styles from "./DataFlowTasks.module.scss";
+import styles from "./DataFlowTasks.module.scss";
 import DataFlowList from "./DataFlowList/DataFlowList";
+import DataFlaws from "../../../assets/jsons/DataFlaws.json";
+import MainLayout from "../../Layout/main-layout.component";
+import DataFlowColumn from "../../Layout/UI/DataFlowColumn/DataFlowColumn";
 import ResourcesContext from "../../Context/ResourcesContext";
-import { ProgressSpinner } from "primereact/progressspinner";
 //import HTTPRequesterAPI from '../../../services/HTTPRequester/HTTPRequester';
+//import ResourcesContext from '../../Context/ResourcesContext';
 
-const DataFlowsCompleted = props => {
-	const { listData } = props;
+const DataFlowsCompleted = () => {
 	const resources = useContext(ResourcesContext);
-	const [completedDataFlows, setCompletedDataFlows] = useState([]);
+	const [pendingDataFlows, setPendingDataFlows] = useState([]);
+	const [acceptetDataFlows, setAcceptedDataFlows] = useState([]);
+
 
 	useEffect(() => {
 		//GET JSON    --->   TODO implement this function with real API call
+		const jsonMimic = DataFlaws;
 
-		setCompletedDataFlows([...listData]);
-	}, [completedDataFlows, listData]);
+		const arrayPending = jsonMimic.filter(
+			jsonData => jsonData.dataFlowStatus === "pending"
+		);
+		const arrayAccepted = jsonMimic.filter(
+			jsonData => jsonData.dataFlowStatus === "accepted"
+		);
+
+		setPendingDataFlows([...arrayPending]);
+		setAcceptedDataFlows([...arrayAccepted]);
+	}, []);
 
 	return (
-		<DataFlowList
-			listContent={completedDataFlows}
-			listType="completed"
-			listTitle={resources.messages["completedDataFlowTitle"]}
-			listDescription={resources.messages["completedDataFlowText"]}
-		/>
+		<MainLayout>
+			<div className="rep-container">
+				<div className="rep-row">
+					<DataFlowColumn navTitle={resources.messages['dataFlow']} search={false} />
+					<div className="subscribe-df rep-col-xs-12 rep-col-md-9">
+						<DataFlowList
+							listContent={pendingDataFlows}
+							listType="pending"
+							listTitle={resources.messages["pendingDataFlowTitle"]}
+							listDescription={resources.messages["pendingDataFlowText"]}
+						/>
+						<DataFlowList
+							listContent={acceptetDataFlows}
+							listType="accepted"
+							listTitle={resources.messages["acceptedDataFlowTitle"]}
+							listDescription={resources.messages["acceptedDataFlowText"]}
+						/>
+					</div>
+				</div>
+			</div>
+		</MainLayout>
 	);
 };
 export default DataFlowsCompleted;
