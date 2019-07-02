2b13639c765e9da59d91e7637998b27297f60422
mazpilic <mazpilicueta-ext@tracasa.es>
2019-07-02 08:26:03 +0200

    update DataFlowTask to eliminate duplications

--- frontend-service/src/components/Pages/DataFlowTasks/DataFlowTasks.js
+++ frontend-service/src/components/Pages/DataFlowTasks/DataFlowTasks.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useContext, useState } from "react";
-import styles from "./DataFlowTasks.module.scss";
 
 import ResourcesContext from "../../Context/ResourcesContext";
+
+import DataFlaws from "../../../assets/jsons/DataFlaws.json";
+
+import styles from "./DataFlowTasks.module.scss";
 
 import { BreadCrumb } from "primereact/breadcrumb";
 import MainLayout from "../../Layout/main-layout.component";
 import DataFlowColumn from "../../Layout/UI/DataFlowColumn/DataFlowColumn";
 import { TabMenu } from "primereact/tabmenu";
-import DataFlowsPendingAccepted from "./DataFlowsPendingAccepted";
-import DataFlowsCompleted from "./DataFlowsCompleted";
+
 import DataFlowList from "./DataFlowList/DataFlowList";
-import DataFlawsCompleted from "../../../assets/jsons/DataFlawsCompleted.json";
-import DataFlaws from "../../../assets/jsons/DataFlaws.json";
+
+//example of a namespace to messages keys
+const i18nKey = "app.components.pages.dataFlowTasks";
 
 //import HTTPRequesterAPI from '../../../services/HTTPRequester/HTTPRequester';
 //import ResourcesContext from '../../Context/ResourcesContext';
@@ -24,39 +27,28 @@ const DataFlowTasks = () => {
 		{ label: "completed" }
 	]);
 	const [tabMenuActiveItem, setTabMenuActiveItem] = useState(tabMenuItems[0]);
-	const [activeTab, setActiveTab] = useState("pending");
 	const [tabData, setTabData] = useState([]);
 	const home = { icon: resources.icons["home"], url: "/" };
 
-	/*   const resources = useContext(ResourcesContext);  
-  // This is here just for example purpose 
-	const home = {icon: resources.icons["home"], url: '#'}; */
-
 	useEffect(() => {
 		const result = DataFlaws;
-		const listData = [];
+		const listKeys = [];
 		if (tabMenuActiveItem.label === "pending") {
-			listData.push({
-				listContent: result.filter(data => data.dataFlowStatus === "pending"),
-				listType: "pending",
-				listTitle: resources.messages["pendingDataFlowTitle"],
-				listDescription: resources.messages["pendingDataFlowText"]
-			});
-			listData.push({
-				listContent: result.filter(data => data.dataFlowStatus === "accepted"),
-				listType: "accepted",
-				listTitle: resources.messages["acceptedDataFlowTitle"],
-				listDescription: resources.messages["acceptedDataFlowText"]
-			});
+			listKeys.push("pending");
+			listKeys.push("accepted");
 		} else {
-			listData.push({
-				listContent: result.filter(data => data.dataFlowStatus === "completed"),
-				listType: "completed",
-				listTitle: resources.messages["completedDataFlowTitle"],
-				listDescription: resources.messages["completedDataFlowText"]
-			});
+			listKeys.push("completed");
 		}
-		setTabData(listData);
+		setTabData(
+			listKeys.map(key => {
+				return {
+					listContent: result.filter(data => data.dataFlowStatus === key),
+					listType: key,
+					listTitle: resources.messages[`${key}DataFlowTitle`],
+					listDescription: resources.messages[`${key}DataFlowText`]
+				};
+			})
+		);
 	}, [resources.messages, tabMenuActiveItem]);
 
 	return (
@@ -79,6 +71,7 @@ const DataFlowTasks = () => {
 						/>
 						{tabData.map((data, i) => (
 							<DataFlowList {...data} key={i} />
+							//TODO completed pagination
 						))}
 					</div>
 				</div>
