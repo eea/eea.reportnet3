80210237530cffc7863e95e5b1db551055e338dd
Eduardo Ayala Collado <eduardo.ayala@altia.es>
2019-07-04 10:19:03 +0200

    [UPDATE]update configuration files
    Eduardo Ayala Collado
    Altia Alicante

--- document-container-service/src/main/java/org/eea/document/configuration/DocumentConfiguration.java
+++ document-container-service/src/main/java/org/eea/document/configuration/DocumentConfiguration.java
@@ -1,6 +1,8 @@
 package org.eea.document.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * The Class DocumentConfiguration.
  */
 @Configuration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
 @EnableWebMvc
 public class DocumentConfiguration implements WebMvcConfigurer {
 
