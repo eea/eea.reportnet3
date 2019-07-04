82f634d711ecc89fcba678c73304797737059acc
jlanton <ext.jose.luis.anton@altia.es>
2019-07-04 08:51:23 +0200

    NEW proposal for record recovering

--- dataset-service/src/main/java/org/eea/dataset/persistence/data/repository/RecordRepositoryImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/persistence/data/repository/RecordRepositoryImpl.java
@@ -6,6 +6,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,26 +19,36 @@ import org.eea.dataset.persistence.data.util.SortField;
  */
 public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
 
-  /** The entity manager. */
+  /**
+   * The entity manager.
+   */
   @PersistenceContext
   private EntityManager entityManager;
 
-  /** The Constant DEFAULT_SORT_CRITERIA. */
+  /**
+   * The Constant DEFAULT_SORT_CRITERIA.
+   */
   private static final String DEFAULT_SORT_CRITERIA = "' '";
 
-  /** The Constant SORT_QUERY. */
+  /**
+   * The Constant SORT_QUERY.
+   */
   private final static String SORT_QUERY =
-      "COALESCE((select fv.value from FieldValue fv where fv.id=fields.id and fv.idFieldSchema ='%s' ), "
+      "COALESCE(( select disticnt fv.value from FieldValue fv where fv.record.id=rv.id and fv.idFieldSchema ='%s' ), "
           + DEFAULT_SORT_CRITERIA + ") as order_criteria_%s";
 
-  /** The Constant MASTER_QUERY. */
+  /**
+   * The Constant MASTER_QUERY.
+   */
   private final static String MASTER_QUERY =
-      "SELECT rv %s from RecordValue rv INNER JOIN rv.tableValue tv INNER JOIN FETCH  rv.fields fields "
+      "SELECT rv %s from RecordValue rv INNER JOIN rv.tableValue tv  "
           + "WHERE tv.idTableSchema = :idTableSchema order by %s";
 
-  /** The Constant QUERY_UNSORTERED. */
+  /**
+   * The Constant QUERY_UNSORTERED.
+   */
   private final static String QUERY_UNSORTERED =
-      "SELECT rv from RecordValue rv INNER JOIN rv.tableValue tv INNER JOIN FETCH  rv.fields "
+      "SELECT rv from RecordValue rv INNER JOIN rv.tableValue tv  "
           + "WHERE tv.idTableSchema = :idTableSchema";
 
 
@@ -46,6 +57,7 @@ public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
    *
    * @param idTableSchema the id table schema
    * @param sortFields the sort fields
+   *
    * @return the list
    */
   @Override
@@ -64,8 +76,9 @@ public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
           .append(" ").append(field.getAsc() ? "asc" : "desc");
     }
     Query query = entityManager.createQuery(String.format(MASTER_QUERY, sortQueryBuilder.toString(),
-        directionQueryBuilder.toString().substring(1)));// adding sort address criteria. (Removing
-                                                        // first , char)
+        directionQueryBuilder.toString().substring(1))).setMaxResults(100)
+        .setFirstResult(100);// adding sort address criteria. (Removing
+    // first , char)
     query.setParameter("idTableSchema", idTableSchema);
 
     return this.sanitizeOrderedRecords(query.getResultList(), sortFields[0].getAsc());
@@ -75,6 +88,7 @@ public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
    * Find by table value no order.
    *
    * @param idTableSchema the id table schema
+   *
    * @return the list
    */
   @Override
@@ -88,6 +102,7 @@ public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
    * Removes duplicated records in the query.
    *
    * @param records the records
+   *
    * @return the list
    */
   private List<RecordValue> sanitizeUnOrderedRecords(List<RecordValue> records) {
@@ -109,38 +124,40 @@ public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
    *
    * @param queryResults the query results
    * @param asc the asc
+   *
    * @return the list
    */
   private List<RecordValue> sanitizeOrderedRecords(List<Object[]> queryResults, Boolean asc) {
 
     // First: Copy sortCriteria into de records variables
-    List<RecordValue> records = queryResults.stream().map(resultRecord -> {
-      RecordValue record = (RecordValue) resultRecord[0];
-      StringBuilder sortCriteriaBuilder = new StringBuilder();
-      for (int i = 1; i < resultRecord.length; i++) {
-        sortCriteriaBuilder.append(resultRecord[i]); // concat all sort criteria values as they will
-                                                     // be used to discriminate
-      }
-      record.setSortCriteria(sortCriteriaBuilder.toString());
-      return record;
-    }).collect(Collectors.toList());
-
-    Map<Long, RecordValue> processedRecords = new LinkedHashMap<>();
-    // Once records has the values it is necessary to remove duplicated records preserving the order
-    // (LinkedHashMap helps on this as it keeps the entry order and records are already ordered from
-    // query)
-    for (int i = 0; i < records.size(); i++) {
-      RecordValue recordValue = records.get(i);
-      if (!processedRecords.containsKey(recordValue.getId())) {
-        processedRecords.put(recordValue.getId(), recordValue);
-      } else if (!" ".equals(recordValue.getSortCriteria())) {
-        processedRecords.remove(recordValue.getId());// it's necessary to remove because we don't
-                                                     // want the previous record (it has an invalid
-                                                     // sortCriteria, so wrong order)
-        processedRecords.put(recordValue.getId(), recordValue);
-      }
-    }
-    return processedRecords.values().stream().collect(Collectors.toList());
+    List<RecordValue> records = queryResults.stream()
+        .map(resultRecord -> (RecordValue) resultRecord[0]).collect(Collectors.toList());
+//      RecordValue record = (RecordValue) resultRecord[0];
+//      StringBuilder sortCriteriaBuilder = new StringBuilder();
+//      for (int i = 1; i < resultRecord.length; i++) {
+//        sortCriteriaBuilder.append(resultRecord[i]); // concat all sort criteria values as they will
+//        // be used to discriminate
+//      }
+//      record.setSortCriteria(sortCriteriaBuilder.toString());
+//      return record;
+//    }).collect(Collectors.toList());
+//
+//    Map<Long, RecordValue> processedRecords = new LinkedHashMap<>();
+//    // Once records has the values it is necessary to remove duplicated records preserving the order
+//    // (LinkedHashMap helps on this as it keeps the entry order and records are already ordered from
+//    // query)
+//    for (int i = 0; i < records.size(); i++) {
+//      RecordValue recordValue = records.get(i);
+//      if (!processedRecords.containsKey(recordValue.getId())) {
+//        processedRecords.put(recordValue.getId(), recordValue);
+//      } else if (!" ".equals(recordValue.getSortCriteria())) {
+//        processedRecords.remove(recordValue.getId());// it's necessary to remove because we don't
+//        // want the previous record (it has an invalid
+//        // sortCriteria, so wrong order)
+//        processedRecords.put(recordValue.getId(), recordValue);
+//      }
+//    }
+    return records;
   }
 }
 
