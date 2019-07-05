5bea53bf5861c56d499f9ca2ac6d4b8593c25d01
Pablo Arrastia <parrastia-ext@outlook.es>
2019-07-05 10:07:27 +0200

    update DataFlowTasks,  List & Item new json structure

--- frontend-service/src/components/Pages/DataFlowTasks/DataFlowTasks.js
+++ frontend-service/src/components/Pages/DataFlowTasks/DataFlowTasks.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext, useState } from "react";
 
 import ResourcesContext from "../../Context/ResourcesContext";
 
-import DataFlaws from "../../../assets/jsons/DataFlaws.json";
+import DataFlaws from "../../../assets/jsons/DataFlaws2.json";
 
 import styles from "./DataFlowTasks.module.scss";
 
@@ -20,63 +20,72 @@ const i18nKey = "app.components.pages.dataFlowTasks";
 //import ResourcesContext from '../../Context/ResourcesContext';
 
 const DataFlowTasks = () => {
-	const resources = useContext(ResourcesContext);
+  const resources = useContext(ResourcesContext);
 
-	const [tabMenuItems, setTabMenuItems] = useState([
-		{ label: "pending", className: styles.flow_tab },
-		{ label: "completed", className: styles.flow_tab }
-	]);
-	const [tabMenuActiveItem, setTabMenuActiveItem] = useState(tabMenuItems[0]);
-	const [tabData, setTabData] = useState([]);
-	const home = { icon: resources.icons["home"], url: "/" };
+  const [tabMenuItems, setTabMenuItems] = useState([
+    {
+      label: "Pending / Accepted",
+      className: styles.flow_tab,
+      tabKey: "pending"
+    },
+    {
+      label: "Completed",
+      className: styles.flow_tab,
+      disabled: true,
+      tabKey: "completed"
+    }
+  ]);
+  const [tabMenuActiveItem, setTabMenuActiveItem] = useState(tabMenuItems[0]);
+  const [tabData, setTabData] = useState([]);
+  const home = { icon: resources.icons["home"], url: "/" };
 
-	useEffect(() => {
-		const result = DataFlaws;
-		const listKeys = [];
-		if (tabMenuActiveItem.label === "pending") {
-			listKeys.push("pending");
-			listKeys.push("accepted");
-		} else {
-			listKeys.push("completed");
-		}
-		setTabData(
-			listKeys.map(key => {
-				return {
-					listContent: result.filter(data => data.dataFlowStatus === key),
-					listType: key,
-					listTitle: resources.messages[`${key}DataFlowTitle`],
-					listDescription: resources.messages[`${key}DataFlowText`]
-				};
-			})
-		);
-	}, [resources.messages, tabMenuActiveItem]);
+  useEffect(() => {
+    const result = DataFlaws;
+    const listKeys = [];
+    if (tabMenuActiveItem.tabKey === "pending") {
+      listKeys.push("pending");
+      listKeys.push("accepted");
+    } else {
+      listKeys.push("completed");
+    }
+    setTabData(
+      listKeys.map(key => {
+        return {
+          listContent: result.filter(data => data.status.toLowerCase() === key),
+          listType: key,
+          listTitle: resources.messages[`${key}DataFlowTitle`],
+          listDescription: resources.messages[`${key}DataFlowText`]
+        };
+      })
+    );
+  }, [resources.messages, tabMenuActiveItem]);
 
-	return (
-		<MainLayout>
-			<BreadCrumb
-				model={[{ label: "Reporting data flow", url: "" }]}
-				home={home}
-			/>
-			<div className="rep-container">
-				<div className="rep-row">
-					<DataFlowColumn
-						navTitle={resources.messages["dataFlow"]}
-						search={false}
-					/>
-					<div className={`${styles.container} rep-col-xs-12 rep-col-md-9`}>
-						<TabMenu
-							model={tabMenuItems}
-							activeItem={tabMenuActiveItem}
-							onTabChange={e => setTabMenuActiveItem(e.value)}
-						/>
-						{tabData.map((data, i) => (
-							<DataFlowList {...data} key={i} />
-							//TODO completed pagination
-						))}
-					</div>
-				</div>
-			</div>
-		</MainLayout>
-	);
+  return (
+    <MainLayout>
+      <BreadCrumb
+        model={[{ label: "Reporting data flow", url: "" }]}
+        home={home}
+      />
+      <div className="rep-container">
+        <div className="rep-row">
+          <DataFlowColumn
+            navTitle={resources.messages["dataFlow"]}
+            search={false}
+          />
+          <div className={`${styles.container} rep-col-xs-12 rep-col-md-9`}>
+            <TabMenu
+              model={tabMenuItems}
+              activeItem={tabMenuActiveItem}
+              onTabChange={e => setTabMenuActiveItem(e.value)}
+            />
+            {tabData.map((data, i) => (
+              <DataFlowList {...data} key={i} />
+              //TODO completed pagination
+            ))}
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
 };
 export default DataFlowTasks;
