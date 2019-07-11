2de8d88412ef449fd1a261e851246d5b08affeab
mazpilic <mazpilicueta-ext@tracasa.es>
2019-07-11 10:23:14 +0200

    update navigation bar styles and structure

--- frontend-service/src/components/Navigation/Navigation.js
+++ frontend-service/src/components/Navigation/Navigation.js
@@ -1,24 +1,29 @@
-import React, {useContext} from 'react';
-import UserCard from '../Layout/UI/UserCard/UserCard';
-import logo from '../../assets/images/logo.png';
-import styles from './Navigation.module.css';
-import ResourcesContext from '../Context/ResourcesContext';
+import React, { useContext } from "react";
+import UserCard from "../Layout/UI/UserCard/UserCard";
+import logo from "../../assets/images/logo.png";
+import styles from "./Navigation.module.css";
+import ResourcesContext from "../Context/ResourcesContext";
 
 const Navigation = () => {
-  const resources = useContext(ResourcesContext);
-    return (           
-      <div id="header" className={styles.header}>
-        <div className={styles.headerLeft}>
-          <a href="#home" className={styles.appLogo} title={resources.messages["titleHeader"]}>
-            <img height="50px" src={logo} alt="Reportnet" className={styles.appLogo}></img>
-            <h1 className={styles.appTitle}>{resources.messages["titleHeader"]}</h1>     
-          </a>
-        </div>
-        <div className={styles.headerRight}>
-          <UserCard/>
-        </div>
-      </div>     
-    );
-}
+	const resources = useContext(ResourcesContext);
+	return (
+		<div id="header" className={styles.header}>
+			<a
+				href="#home"
+				className={styles.appLogo}
+				title={resources.messages["titleHeader"]}
+			>
+				<img
+					height="50px"
+					src={logo}
+					alt="Reportnet"
+					className={styles.appLogo}
+				/>
+				<h1 className={styles.appTitle}>{resources.messages["titleHeader"]}</h1>
+			</a>
+			<UserCard />
+		</div>
+	);
+};
 
 export default Navigation;
