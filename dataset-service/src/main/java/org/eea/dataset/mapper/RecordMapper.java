8873a89d004c68053acd01a8cda030dccf2ed2d7
José Ángel <joseangel.belda@altia.es>
2019-07-26 12:40:15 +0200

    FIX records crud and ADD tests

--- dataset-service/src/main/java/org/eea/dataset/mapper/RecordMapper.java
+++ dataset-service/src/main/java/org/eea/dataset/mapper/RecordMapper.java
@@ -3,7 +3,9 @@ package org.eea.dataset.mapper;
 import org.eea.dataset.persistence.data.domain.RecordValue;
 import org.eea.interfaces.vo.dataset.RecordVO;
 import org.eea.mapper.IMapper;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 
 /**
@@ -12,5 +14,9 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface RecordMapper extends IMapper<RecordValue, RecordVO> {
 
+  @AfterMapping
+  default void fillIds(RecordVO recordVO, @MappingTarget RecordValue recordValue) {
+    recordValue.getFields().stream().forEach(field -> field.setRecord(recordValue));
+  }
 }
 
