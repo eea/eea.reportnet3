9466ef4623dde76ee0d605477aa2974c506685c5
jlanton <ext.jose.luis.anton@altia.es>
2019-07-29 13:45:29 +0200

    UPDATED confiugration pointint to keycloak in dev and some comments

--- user-management-service/src/main/java/org/eea/ums/service/SecurityProviderInterfaceService.java
+++ user-management-service/src/main/java/org/eea/ums/service/SecurityProviderInterfaceService.java
@@ -7,19 +7,67 @@ import javax.annotation.Nullable;
 import org.eea.ums.service.vo.UserGroupVO;
 import org.eea.ums.service.vo.UserVO;
 
+/**
+ * The interface Security provider interface service.
+ */
 public interface SecurityProviderInterfaceService {
 
 
+  /**
+   * Do login string.
+   *
+   * @param username the username
+   * @param password the password
+   * @param extraParams the extra params
+   *
+   * @return the string
+   */
   String doLogin(String username, String password, Object... extraParams);
 
+  /**
+   * Gets user group info.
+   *
+   * @param securityToken the security token
+   *
+   * @return the user group info
+   */
   List<UserGroupVO> getUserGroupInfo(String securityToken);
 
+  /**
+   * Gets users.
+   *
+   * @param userId the user id
+   * @param securityToken the security token
+   *
+   * @return the users
+   */
   List<UserVO> getUsers(@Nullable String userId, String securityToken);
 
+  /**
+   * Create user group.
+   *
+   * @param userGroupName the user group name
+   * @param securityToken the security token
+   * @param attributes the attributes
+   */
   void createUserGroup(String userGroupName, String securityToken, Map<String, String> attributes);
 
+  /**
+   * Add user to user group.
+   *
+   * @param userId the user id
+   * @param groupId the group id
+   * @param securityToken the security token
+   */
   void addUserToUserGroup(String userId, String groupId, String securityToken);
 
+  /**
+   * Remove user from user group.
+   *
+   * @param userId the user id
+   * @param groupId the group id
+   * @param securityToken the security token
+   */
   void removeUserFromUserGroup(String userId, String groupId, String securityToken);
 
 
