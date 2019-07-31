d107aba80878e8766bbb728788d87d449db32825
ruben.lozano <ruben.lozano@altia.es>
2019-07-31 12:57:51 +0200

    UPDATE Add new fields to DocumentVO and mapper

--- dataflow-service/src/main/java/org/eea/dataflow/mapper/DataflowMapper.java
+++ dataflow-service/src/main/java/org/eea/dataflow/mapper/DataflowMapper.java
@@ -1,10 +1,15 @@
 package org.eea.dataflow.mapper;
 
+import java.util.List;
+import org.apache.commons.lang.StringUtils;
 import org.eea.dataflow.persistence.domain.Dataflow;
 import org.eea.interfaces.vo.dataflow.DataFlowVO;
+import org.eea.interfaces.vo.document.DocumentVO;
 import org.eea.mapper.IMapper;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 /**
  * The Interface DataflowMapper.
@@ -23,5 +28,17 @@ public interface DataflowMapper extends IMapper<Dataflow, DataFlowVO> {
   @Mapping(source = "deadlineDate", target = "deadlineDate", dateFormat = "dd/MM/yyyy")
   DataFlowVO entityToClass(Dataflow entity);
 
-
+  /**
+   * Fill category.
+   *
+   * @param dataflow the dataflow
+   * @param dataFlowVO the data flow VO
+   */
+  @AfterMapping
+  default void fillCategory(Dataflow dataflow, @MappingTarget DataFlowVO dataFlowVO) {
+    List<DocumentVO> documents = dataFlowVO.getDocuments();
+    documents.stream().filter(document -> StringUtils.isNotBlank(document.getName()))
+        .forEach(document -> document.setCategory(
+            document.getName().substring(document.getName().lastIndexOf('.') + 1).toLowerCase()));
+  }
 }
