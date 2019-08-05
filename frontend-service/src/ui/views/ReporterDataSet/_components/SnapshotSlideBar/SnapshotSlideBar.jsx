2d27c8aacd8a6eea3443e216e684b679b1d1531c
icojocaru <icojocaru-ext@tracasa.es>
2019-08-05 12:52:31 +0200

    add Snapshot description validation

--- frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/SnapshotSlideBar.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/SnapshotSlideBar.jsx
@@ -2,19 +2,27 @@ import React, { useContext, useEffect, useState } from 'react';
 
 import styles from './SnapshotSliderBar.module.css';
 
+import { Formik, Field, Form } from 'formik';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 import { Sidebar } from 'primereact/sidebar';
 import { SnapshotContext } from '../../ReporterDataSet';
 import { SnapshotList } from './_components/SnapshotList';
+import * as Yup from 'yup';
 
 const SnapshotSlideBar = ({ isVisible, setIsVisible, dataSetId, setSnapshotList, snapshotListData }) => {
   const snapshotContext = useContext(SnapshotContext);
   const resources = useContext(ResourcesContext);
-  const [snapshotDescription, setSnapshotDescription] = useState('');
 
   useEffect(() => {
     setSnapshotList();
   }, []);
+
+  const createSnapshotInputValidationSchema = Yup.object().shape({
+    createSnapshotDescriptionInputName: Yup.string()
+      .min(2, 'Too short description!')
+      .max(50, 'Too long description!')
+      .required('Description is required')
+  });
 
   return (
     <Sidebar visible={isVisible} onHide={e => setIsVisible()} position="right">
@@ -23,26 +31,37 @@ const SnapshotSlideBar = ({ isVisible, setIsVisible, dataSetId, setSnapshotList,
           <h3>{resources.messages.createSnapshotTitle}</h3>
         </div>
         <div className={`${styles.newContainer} ${styles.section}`}>
-          <input
-            type="text"
-            placeholder={resources.messages.createSnapshotPlaceholder}
-            onChange={e => {
-              setSnapshotDescription(e.target.value);
-            }}
-          />
-          <button
-            className={snapshotDescription === '' ? "rp-btn secondary" : "rp-btn primary"}
-            disabled = {snapshotDescription === ''}
-            onClick={() =>
+          <Formik
+            initialValues={{ createSnapshot: '' }}
+            validationSchema={createSnapshotInputValidationSchema}
+            onSubmit={snapshotDescriptionInput =>
               snapshotContext.snapshotDispatch({
                 type: 'create_snapshot',
                 payload: {
-                  description: snapshotDescription
+                  description: snapshotDescriptionInput.createSnapshotDescriptionInputName
                 }
               })
-            }>
-            {resources.messages.create}
-          </button>
+            }
+            render={({ errors, touched, isSubmitting }) => (
+              <Form>
+                <Field
+                  type="text"
+                  name="createSnapshotDescriptionInputName"
+                  placeholder={resources.messages.createSnapshotPlaceholder}
+                />
+                {errors.createSnapshotDescriptionInputName && touched.createSnapshotDescriptionInputName ? (
+                  <div className={styles.errors}>{errors.createSnapshotDescriptionInputName}</div>
+                ) : null}
+
+                <button
+                  className={isSubmitting ? 'rp-btn secondary' : 'rp-btn primary'}
+                  type="submit"
+                  disabled={isSubmitting}>
+                  {resources.messages.create}
+                </button>
+              </Form>
+            )}
+          />
         </div>
         <SnapshotList snapshotListData={snapshotListData} />
       </div>
