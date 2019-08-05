6fe20a24376a62e6d9b35511198f230b13f5a34d
Miguel Azanza <mazanza@tracasa.es>
2019-08-05 14:32:15 +0200

    fix downloadDocument functionality

--- frontend-service/src/core/infrastructure/api.js
+++ frontend-service/src/core/infrastructure/api.js
@@ -7,6 +7,7 @@ export const api = {
     const response = await HTTPRequester.get('/characters.json');
     return response.json();
   },
+
   documents: async url => {
     const response = await HTTPRequester.get({
       url: window.env.REACT_APP_JSON ? '/jsons/list-of-documents.json' : url,
@@ -14,21 +15,17 @@ export const api = {
     });
     return response.data.documents;
   },
+
   downloadDocumentById: async documentId => {
-    await HTTPRequester.get({
+    const response = await HTTPRequester.get({
       url: getUrl(config.downloadDocumentByIdAPI.url, {
         documentId: documentId
       }),
-      // url: window.env.REACT_APP_JSON
-      //   ? getUrl(config.downloadDocumentByIdAPI.url, {
-      //       documentId: documentId
-      //     })
-      //   : getUrl(config.downloadDocumentByIdAPI.url, {
-      //       documentId: documentId
-      //     }),
       queryString: {}
     });
+    return response.data;
   },
+
   snapshots: async url => {
     const response = await HTTPRequester.get({
       url: window.env.REACT_APP_JSON ? '/jsons/snapshots.json' : url,
@@ -36,6 +33,7 @@ export const api = {
     });
     return response.data;
   },
+
   webLinks: async url => {
     const response = await HTTPRequester.get({
       url: window.env.REACT_APP_JSON ? '/jsons/list-of-documents.json' : url,
