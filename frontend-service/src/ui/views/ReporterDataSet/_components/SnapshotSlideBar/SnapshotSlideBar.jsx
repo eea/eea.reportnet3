280088816c907e6f342fa433767e9014b53176e6
icojocaru <icojocaru-ext@tracasa.es>
2019-08-06 13:28:33 +0200

    add ScrollBar to SnapshotSideBar

--- frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/SnapshotSlideBar.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/SnapshotSlideBar.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
 
 import primeIcons from 'assets/conf/prime.icons';
-import styles from './SnapshotSliderBar.module.css';
+import styles from './SnapshotSliderBar.module.scss';
 
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { IconComponent } from 'ui/views/_components/IconComponent';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
+import { ScrollPanel } from 'primereact/scrollpanel';
 import { Sidebar } from 'primereact/sidebar';
 import { SnapshotContext } from '../../ReporterDataSet';
 import { SnapshotList } from './_components/SnapshotList';
@@ -27,44 +28,51 @@ const SnapshotSlideBar = ({ isVisible, setIsVisible, dataSetId, setSnapshotList,
   });
 
   return (
-    <Sidebar visible={isVisible} onHide={e => setIsVisible()} position="right">
-      <div className={styles.content}>
-        <div className={styles.title}>
-          <h3>{resources.messages.createSnapshotTitle}</h3>
+    <Sidebar
+      visible={isVisible}
+      onHide={e => setIsVisible()}
+      blockScroll="true"
+      position="right"
+      className={styles.sidebar}>
+      <ScrollPanel style={{ width: '100%', height: '100%' }} className={styles.scrollPanel}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <h3>{resources.messages.createSnapshotTitle}</h3>
+          </div>
+          <div className={`${styles.newContainer} ${styles.section}`}>
+            <Formik
+              initialValues={{ createSnapshot: '' }}
+              validationSchema={createSnapshotInputValidationSchema}
+              onSubmit={snapshotDescriptionInput =>
+                snapshotContext.snapshotDispatch({
+                  type: 'create_snapshot',
+                  payload: {
+                    description: snapshotDescriptionInput.createSnapshotDescriptionInputName
+                  }
+                })
+              }
+              render={({ errors, touched, isSubmitting }) => (
+                <Form className={styles.createForm}>
+                  <div className={styles.inputnAndErrorBox}>
+                    <Field
+                      type="text"
+                      name="createSnapshotDescriptionInputName"
+                      placeholder={resources.messages.createSnapshotPlaceholder}
+                    />
+                    {errors.createSnapshotDescriptionInputName || touched.createSnapshotDescriptionInputName ? (
+                      <div className={styles.errors}>{errors.createSnapshotDescriptionInputName}</div>
+                    ) : null}
+                  </div>
+                  <button className="rp-btn primary" type="submit">
+                    <IconComponent icon={primeIcons.icons.check} />
+                  </button>
+                </Form>
+              )}
+            />
+          </div>
+          <SnapshotList snapshotListData={snapshotListData} />
         </div>
-        <div className={`${styles.newContainer} ${styles.section}`}>
-          <Formik
-            initialValues={{ createSnapshot: '' }}
-            validationSchema={createSnapshotInputValidationSchema}
-            onSubmit={snapshotDescriptionInput =>
-              snapshotContext.snapshotDispatch({
-                type: 'create_snapshot',
-                payload: {
-                  description: snapshotDescriptionInput.createSnapshotDescriptionInputName
-                }
-              })
-            }
-            render={({ errors, touched, isSubmitting }) => (
-              <Form className={styles.createForm}>
-                <div className={styles.inputnAndErrorBox}>
-                  <Field
-                    type="text"
-                    name="createSnapshotDescriptionInputName"
-                    placeholder={resources.messages.createSnapshotPlaceholder}
-                  />
-                  {errors.createSnapshotDescriptionInputName || touched.createSnapshotDescriptionInputName ? (
-                    <div className={styles.errors}>{errors.createSnapshotDescriptionInputName}</div>
-                  ) : null}
-                </div>
-                <button className="rp-btn primary" type="submit">
-                  <IconComponent icon={primeIcons.icons.check} />
-                </button>
-              </Form>
-            )}
-          />
-        </div>
-        <SnapshotList snapshotListData={snapshotListData} />
-      </div>
+      </ScrollPanel>
     </Sidebar>
   );
 };
