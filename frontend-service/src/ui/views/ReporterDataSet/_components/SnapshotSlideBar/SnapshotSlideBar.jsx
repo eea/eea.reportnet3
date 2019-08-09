e89295834260448b407714725f1206cf480c9c7e
Miguel Azanza <mazanza@tracasa.es>
2019-08-09 09:18:44 +0200

    update refactor naming

--- frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/SnapshotSlideBar.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/SnapshotSlideBar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react';
 
 import primeIcons from 'assets/conf/prime.icons';
@@ -8,7 +7,6 @@ import { Formik, Field, Form } from 'formik';
 import { IconComponent } from 'ui/views/_components/IconComponent';
 import { isEmpty } from 'lodash';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
-import { ScrollPanel } from 'primereact/scrollpanel';
 import { Sidebar } from 'primereact/sidebar';
 import { SnapshotContext } from '../../ReporterDataSet';
 import { SnapshotList } from './_components/SnapshotList';
@@ -27,8 +25,8 @@ const SnapshotSlideBar = ({ isVisible, setIsVisible, dataSetId, setSnapshotList,
     isVisible ? (bodySelector.style.overflowY = 'hidden') : (bodySelector.style.overflowY = 'auto');
   }, [isVisible]);
 
-  const createSnapshotInputValidationSchema = Yup.object().shape({
-    createSnapshotDescriptionInputName: Yup.string()
+  const snapshotValidationSchema = Yup.object().shape({
+    createSnapshotDescription: Yup.string()
       .min(2, resources.messages['snapshotDescriptionValidationMin'])
       .max(100, resources.messages['snapshotDescriptionValidationMax'])
       .required(resources.messages['snapshotDescriptionValidationRequired'])
@@ -48,36 +46,34 @@ const SnapshotSlideBar = ({ isVisible, setIsVisible, dataSetId, setSnapshotList,
         <div className={`${styles.newContainer} ${styles.section}`}>
           <Formik
             initialValues={{ createSnapshot: '' }}
-            validationSchema={createSnapshotInputValidationSchema}
+            validationSchema={snapshotValidationSchema}
             onSubmit={snapshotDescriptionInput =>
               snapshotContext.snapshotDispatch({
                 type: 'create_snapshot',
                 payload: {
-                  description: snapshotDescriptionInput.createSnapshotDescriptionInputName
+                  description: snapshotDescriptionInput.createSnapshotDescription
                 }
               })
             }
             render={({ errors, touched, isSubmitting }) => (
               <Form className={styles.createForm}>
                 <div
-                  className={`${styles.createInuputAndButtonBox} ${
-                    !isEmpty(errors.createSnapshotDescriptionInputName) && touched.createSnapshotDescriptionInputName
-                      ? ' error'
-                      : ''
+                  className={`${styles.createInputAndButtonWrapper} ${
+                    !isEmpty(errors.createSnapshotDescription) && touched.createSnapshotDescription ? ' error' : ''
                   }`}>
                   <Field
                     type="text"
-                    name="createSnapshotDescriptionInputName"
+                    name="createSnapshotDescription"
                     placeholder={resources.messages.createSnapshotPlaceholder}
                   />
-                  <div className={styles.createButtonBox}>
+                  <div className={styles.createButtonWrapper}>
                     <button className={`${styles.createSnapshotButton} rp-btn primary`} type="submit">
                       <IconComponent icon={primeIcons.icons.plus} />
                     </button>
                   </div>
                 </div>
-                {errors.createSnapshotDescriptionInputName || touched.createSnapshotDescriptionInputName ? (
-                  <div className="error">{errors.createSnapshotDescriptionInputName}</div>
+                {errors.createSnapshotDescription || touched.createSnapshotDescription ? (
+                  <div className="error">{errors.createSnapshotDescription}</div>
                 ) : null}
               </Form>
             )}
