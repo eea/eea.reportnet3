2da7e6b0774dd361e814bbac8c46969593f9d811
icojocaru <icojocaru-ext@tracasa.es>
2019-08-20 10:42:38 +0200

    update Snapshot Buttons
    added Tooltips for buttons
    addeb Release Button with reducer and Dialog

--- frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/_components/SnapshotList/_components/SnapshotItem/SnapshotItem.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/SnapshotSlideBar/_components/SnapshotList/_components/SnapshotItem/SnapshotItem.jsx
@@ -6,9 +6,12 @@ import styles from './SnapshotItem.module.scss';
 
 import { Button } from 'ui/views/_components/Button';
 import { SnapshotContext } from 'ui/views/ReporterDataSet/ReporterDataSet';
+import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 
 export function SnapshotItem({ itemData }) {
   const snapshotContext = useContext(SnapshotContext);
+
+  const resources = useContext(ResourcesContext);
 
   return (
     <li className={styles.listItem}>
@@ -17,10 +20,11 @@ export function SnapshotItem({ itemData }) {
           <h4>{moment(itemData.creationDate).format('DD/MM/YYYY HH:mm:ss')}</h4>
           <div className={styles.listActions}>
             <Button
+              tooltip={resources.messages.restoreSnapshotTooltip}
+              tooltipOptions={{ position: 'top' }}
               icon="replay"
-              layout="simple"
               disabled={true}
-              className="rp-btn secondary"
+              className={`${styles.btn} rp-btn secondary`}
               onClick={() =>
                 snapshotContext.snapshotDispatch({
                   type: 'restore_snapshot',
@@ -29,10 +33,24 @@ export function SnapshotItem({ itemData }) {
               }
             />
             <Button
-              icon="trash"
-              layout="simple"
+              tooltip={resources.messages.releaseSnapshotTooltip}
+              tooltipOptions={{ position: 'top' }}
+              icon="cloudUpload"
               disabled={false}
-              className="rp-btn warning"
+              className={`${styles.btn} rp-btn secondary`}
+              onClick={() =>
+                snapshotContext.snapshotDispatch({
+                  type: 'release_snapshot',
+                  payload: { ...itemData }
+                })
+              }
+            />
+            <Button
+              tooltip={resources.messages.deleteSnapshotTooltip}
+              tooltipOptions={{ position: 'left' }}
+              icon="trash"
+              disabled={false}
+              className={`${styles.btn} rp-btn warning`}
               onClick={() =>
                 snapshotContext.snapshotDispatch({
                   type: 'delete_snapshot',
