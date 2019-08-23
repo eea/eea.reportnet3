d59551f7b3697fa93da862245008a81ab7564b17
Pablo Arrastia <parrastia-ext@outlook.es>
2019-08-23 10:18:00 +0200

    update documentationDataSet - use toolbar instead of buttonsBar

--- frontend-service/src/ui/views/DocumentationDataSet/DocumentationDataSet.jsx
+++ frontend-service/src/ui/views/DocumentationDataSet/DocumentationDataSet.jsx
@@ -8,7 +8,7 @@ import styles from './DocumentationDataSet.module.scss';
 import { config } from 'conf';
 
 import { BreadCrumb } from 'ui/views/_components/BreadCrumb';
-import { ButtonsBar } from 'ui/views/_components/ButtonsBar';
+import { Button } from 'ui/views/_components/Button';
 import { Column } from 'primereact/column';
 import { DataTable } from 'ui/views/_components/DataTable';
 import { Dialog } from 'ui/views/_components/Dialog';
@@ -20,6 +20,7 @@ import { MainLayout } from 'ui/views/_components/Layout';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 import { Spinner } from 'ui/views/_components/Spinner';
 import { TabView, TabPanel } from 'primereact/tabview';
+import { Toolbar } from 'ui/views/_components/Toolbar';
 
 import { DocumentService } from 'core/services/Document';
 import { WebLinkService } from 'core/services/WebLink';
@@ -28,7 +29,6 @@ export const DocumentationDataSet = ({ match, history }) => {
   const resources = useContext(ResourcesContext);
 
   const [breadCrumbItems, setBreadCrumbItems] = useState([]);
-  const [buttons, setButtons] = useState([]);
   const [documents, setDocuments] = useState([]);
   const [fileName, setFileName] = useState('');
   const [fileToDownload, setFileToDownload] = useState(undefined);
@@ -59,49 +59,6 @@ export const DocumentationDataSet = ({ match, history }) => {
       { label: resources.messages['documents'] }
     ]);
   }, [history, match.params.dataFlowId, resources.messages]);
-
-  //Data Fetching
-  useEffect(() => {
-    //#region Button initialization
-    setButtons([
-      {
-        label: resources.messages['upload'],
-        icon: 'export',
-        group: 'left',
-        disabled: false,
-        onClick: () => setIsUploadDialogVisible(true)
-      },
-      {
-        label: resources.messages['visibility'],
-        icon: 'eye',
-        group: 'left',
-        disabled: true,
-        onClick: null
-      },
-      {
-        label: resources.messages['filter'],
-        icon: 'filter',
-        group: 'left',
-        disabled: true,
-        onClick: null
-      },
-      {
-        label: resources.messages['export'],
-        icon: 'import',
-        group: 'left',
-        disabled: true,
-        onClick: null
-      },
-      {
-        label: resources.messages['refresh'],
-        icon: 'refresh',
-        group: 'right',
-        disabled: false,
-        onClick: () => onLoadDocumentsAndWebLinks()
-      }
-    ]);
-    //#end region Button initialization
-  }, []);
 
   useEffect(() => {
     if (!isUndefined(fileToDownload)) {
@@ -176,7 +133,47 @@ export const DocumentationDataSet = ({ match, history }) => {
     return layout(
       <TabView>
         <TabPanel header={resources.messages['documents']}>
-          <ButtonsBar buttonsList={buttons} />
+          <Toolbar>
+            <div className="p-toolbar-group-left">
+              <Button
+                className={`p-button-rounded p-button-secondary`}
+                disabled={false}
+                icon={'export'}
+                label={resources.messages['upload']}
+                onClick={() => setIsUploadDialogVisible(true)}
+              />
+              <Button
+                className={`p-button-rounded p-button-secondary`}
+                disabled={true}
+                icon={'eye'}
+                label={resources.messages['visibility']}
+                onClick={null}
+              />
+              <Button
+                className={`p-button-rounded p-button-secondary`}
+                disabled={true}
+                icon={'filter'}
+                label={resources.messages['filter']}
+                onClick={null}
+              />
+              <Button
+                className={`p-button-rounded p-button-secondary`}
+                disabled={true}
+                icon={'import'}
+                label={resources.messages['export']}
+                onClick={null}
+              />
+            </div>
+            <div className="p-toolbar-group-right">
+              <Button
+                className={`p-button-rounded p-button-secondary`}
+                disabled={false}
+                icon={'refresh'}
+                label={resources.messages['refresh']}
+                onClick={() => onLoadDocumentsAndWebLinks()}
+              />
+            </div>
+          </Toolbar>
           <Dialog
             header={resources.messages['upload']}
             visible={isUploadDialogVisible}
