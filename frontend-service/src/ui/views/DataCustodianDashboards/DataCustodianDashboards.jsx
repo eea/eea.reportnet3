96d09b4182fb4b53d4913a51d0d2c9805fdb54db
icojocaru <icojocaru-ext@tracasa.es>
2019-08-27 12:21:14 +0200

    add Chart with fake data just for test

--- frontend-service/src/ui/views/DataCustodianDashboards/DataCustodianDashboards.jsx
+++ frontend-service/src/ui/views/DataCustodianDashboards/DataCustodianDashboards.jsx
@@ -6,7 +6,6 @@ import styles from './DataCustodianDashboards.module.scss';
 import { config } from 'conf';
 
 import { BreadCrumb } from 'ui/views/_components/BreadCrumb';
-import { Button } from 'ui/views/_components/Button';
 import { MainLayout } from 'ui/views/_components/Layout';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 import { Spinner } from 'ui/views/_components/Spinner';
@@ -14,11 +13,16 @@ import { Spinner } from 'ui/views/_components/Spinner';
 import { DataFlowService } from 'core/services/DataFlow';
 import { UserContext } from '../_components/_context/UserContext';
 
+import { Chart } from 'primereact/chart';
+
 export const DataCustodianDashboards = withRouter(({ match, history }) => {
   const resources = useContext(ResourcesContext);
   const userData = useContext(UserContext);
   const [loading, setLoading] = useState(false); //TODO change to true & false when loaded
   const [breadCrumbItems, setBreadCrumbItems] = useState([]);
+
+  const [dashboardData, setDashboardData] = useState({});
+  const [dashboardOptions, setDashboardOptions] = useState({});
 
   const home = {
     icon: config.icons['home'],
@@ -30,6 +34,51 @@ export const DataCustodianDashboards = withRouter(({ match, history }) => {
     setBreadCrumbItems([{ label: resources.messages.dataCustodianDashboards }]);
   }, [history, match.params.dataFlowId, resources.messages]);
 
+  useEffect(() => {
+    setDashboardData({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          type: 'bar',
+          label: 'Dataset 1',
+          backgroundColor: '#66BB6A',
+          data: [50, 25, 12, 48, 90, 76, 42]
+        },
+        {
+          type: 'bar',
+          label: 'Dataset 2',
+          backgroundColor: '#FFCA28',
+          data: [21, 84, 24, 75, 37, 65, 34]
+        },
+        {
+          type: 'bar',
+          label: 'Dataset 3',
+          backgroundColor: '#42A5F5',
+          data: [41, 52, 24, 74, 23, 21, 32]
+        }
+      ]
+    });
+
+    setDashboardOptions({
+      tooltips: {
+        mode: 'index',
+        intersect: false
+      },
+      responsive: true,
+      scales: {
+        xAxes: [
+          {
+            stacked: true
+          }
+        ],
+        yAxes: [
+          {
+            stacked: true
+          }
+        ]
+      }
+    });
+  }, []);
   const layout = children => {
     return (
       <MainLayout>
@@ -43,5 +92,12 @@ export const DataCustodianDashboards = withRouter(({ match, history }) => {
     return layout(<Spinner />);
   }
 
-  return layout(<div className="rep-row"></div>);
+  return layout(
+    <div className="rep-row">
+      <React.Fragment>
+        <h1>Title Hardcoded</h1>
+        <Chart type="bar" data={dashboardData} options={dashboardOptions} width="50%" height="35%" />
+      </React.Fragment>
+    </div>
+  );
 });
