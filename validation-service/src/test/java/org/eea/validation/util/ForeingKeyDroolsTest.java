7dafb173c298820691c2f26255f4bb7e151f4a9d
Eduardo Ayala Collado <eduardo.ayala@altia.es>
2019-08-28 14:17:26 +0200

    [ADD]Add javadocs
    Eduardo Ayala Collado
    Altia Alicante

--- validation-service/src/test/java/org/eea/validation/util/ForeingKeyDroolsTest.java
+++ validation-service/src/test/java/org/eea/validation/util/ForeingKeyDroolsTest.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package org.eea.validation.util;
 
 import java.util.ArrayList;
@@ -10,40 +13,64 @@ import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
+/**
+ * The Class ForeingKeyDroolsTest.
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class ForeingKeyDroolsTest {
 
+  /**
+   * Inits the mocks.
+   */
   @Before
   public void initMocks() {
     MockitoAnnotations.initMocks(this);
   }
 
 
+  /**
+   * Checks if is query data WDF protect.
+   */
   @Test
   public void isQueryDataWDFProtect() {
     ForeingKeyDrools.isQueryDataWDFProtect("1234");
   }
 
+  /**
+   * Checks if is query data WDF protect part 1.
+   */
   @Test
   public void isQueryDataWDFProtectPart1() {
     ForeingKeyDrools.isQueryDataWDFProtect("AL202");
   }
 
+  /**
+   * Checks if is query data WDF protect part 2.
+   */
   @Test
   public void isQueryDataWDFProtectPart2() {
     ForeingKeyDrools.isQueryDataWDFProtect("ES614M0062204");
   }
 
+  /**
+   * Checks if is query data WDF protect part 3.
+   */
   @Test
   public void isQueryDataWDFProtectPart3() {
     ForeingKeyDrools.isQueryDataWDFProtect("IEWEBWC160_0000_0100");
   }
 
+  /**
+   * Checks if is query data WDF protect part 4.
+   */
   @Test
   public void isQueryDataWDFProtectPart4() {
     ForeingKeyDrools.isQueryDataWDFProtect("SE0441262000000324");
   }
 
+  /**
+   * Checks if is same record true.
+   */
   @Test
   public void isSameRecordTrue() {
     RecordValue recordValue = new RecordValue();
@@ -55,6 +82,9 @@ public class ForeingKeyDroolsTest {
     ForeingKeyDrools.isInSameRecord("value", recordValue, 0, "value");
   }
 
+  /**
+   * Checks if is same record false.
+   */
   @Test
   public void isSameRecordFalse() {
     RecordValue recordValue = new RecordValue();
