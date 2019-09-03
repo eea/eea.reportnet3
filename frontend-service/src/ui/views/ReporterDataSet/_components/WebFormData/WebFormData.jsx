5dbd9e04425bd7d871abfbb89d3a832ca9e4fb10
Miguel Azanza <mazanza@tracasa.es>
2019-09-03 14:24:29 +0200

    add headers webForms columns test

--- frontend-service/src/ui/views/ReporterDataSet/_components/WebFormData/WebFormData.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/WebFormData/WebFormData.jsx
@@ -1,22 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import styles from './WebFormData.module.css';
 
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { isEmpty, isUndefined } from 'lodash';
 
+import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
+
+import { InputText } from 'ui/views/_components/InputText';
+
 const WebFormData = ({ data }) => {
-  console.log(data);
+  const resources = useContext(ResourcesContext);
+
+  const [columns, setColumns] = useState([]);
+
+  console.log('Belgium dataset ', data);
+
+  useEffect(() => {
+    const headers = [
+      {
+        id: 'tableSchemaName',
+        header: resources.messages['origin']
+      },
+      {
+        id: 'levelError',
+        header: resources.messages['levelError']
+      },
+      {
+        id: 'message',
+        header: resources.messages['errorMessage']
+      },
+      {
+        id: 'entityType',
+        header: resources.messages['entityType']
+      },
+      {
+        id: 'entityType2',
+        header: resources.messages['entityType']
+      },
+      {
+        id: 'entityType3',
+        header: resources.messages['entityType']
+      },
+      {
+        id: 'entityType4',
+        header: resources.messages['entityType']
+      }
+    ];
+
+    let columnsArr = headers.map(col => (
+      <h2 key={col.id} field={col.id} header={col.header} className={styles.formColumn}>
+        {col.header}
+      </h2>
+    ));
+    columnsArr.push(<text key="recordId" field="recordId" header="" className={styles.VisibleHeader} />);
+    columnsArr.push(<text key="tableSchemaId" field="tableSchemaId" header="" className={styles.VisibleHeader} />);
+    setColumns(columnsArr);
+
+    // fetchData('', sortOrder, firstRow, numRows);
+  }, []);
+
+  const recordsArray = data.records.map(recordDTO => {
+    const fieldsArray = recordDTO.fields.map(fieldDTO => {
+      return <InputText value={fieldDTO.value} placeholder={fieldDTO.value} className={styles.inputField} />;
+    });
+    return fieldsArray;
+  });
 
   return (
     <div className={`${styles.newContainer} ${styles.section}`}>
       <Formik
-        initialValues={{ createSnapshotDescription: '' }}
+        initialValues={{ inputFields: '' }}
         render={({ errors, touched, isSubmitting }) => (
-          <Form className={styles.createForm}>
-            <div>
-              <Field type="text" name="test" placeholder={'test'} />
-            </div>
+          <Form>
+            <div className={styles.divWebForm}>{columns}</div>
           </Form>
         )}
       />
