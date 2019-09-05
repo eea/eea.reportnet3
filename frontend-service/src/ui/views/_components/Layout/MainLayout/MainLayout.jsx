ed88c6d0ba531950cf717ced546ab6142d662f4a
icojocaru <icojocaru-ext@tracasa.es>
2019-09-05 09:02:24 +0200

    update Beta Disclaimer text

--- frontend-service/src/ui/views/_components/Layout/MainLayout/MainLayout.jsx
+++ frontend-service/src/ui/views/_components/Layout/MainLayout/MainLayout.jsx
@@ -31,7 +31,7 @@ const MainLayout = ({ children }) => {
       <Navigation />
       <div className={styles.disclaimer}>
         <span className="p-messages-icon pi  pi-info-circle"></span>
-        This is test Data. Any change would not affect the real data. So feel free trying your stuff.
+        BETA VERSION: This is test Data. Any change would not affect the real data. So feel free trying your stuff.
       </div>
       <div className={styles.mainContent}>{children}</div>
       <Footer />
