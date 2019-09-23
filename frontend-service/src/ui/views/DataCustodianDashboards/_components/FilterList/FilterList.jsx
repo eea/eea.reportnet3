26a6084cf3b82004863219548b06731ff28c373e
Pablo Arrastia <parrastia-ext@outlook.es>
2019-09-23 12:53:21 +0200

    update use messages.json instead of hardcoded text

--- frontend-service/src/ui/views/DataCustodianDashboards/_components/FilterList/FilterList.jsx
+++ frontend-service/src/ui/views/DataCustodianDashboards/_components/FilterList/FilterList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { uniqBy } from 'lodash';
 
@@ -6,9 +6,11 @@ import styles from './FilterList.module.scss';
 
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import { CountriesListItem } from '../_components/CountriesListItem';
+import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 import { TableListItem } from '../_components/TableListItem';
 
 function FilterList({ originalData: { datasets, labels }, filterDispatch }) {
+  const resources = useContext(ResourcesContext);
   const createTableCheckBoxObject = dataset => {
     return { tableName: dataset.tableName, tableId: dataset.tableId };
   };
@@ -25,14 +27,14 @@ function FilterList({ originalData: { datasets, labels }, filterDispatch }) {
   return (
     <>
       <Accordion>
-        <AccordionTab header="Filter by Tables/Datasets">
+        <AccordionTab header={resources.messages['filterByTables']}>
           <ul className={styles.list}>
             {tableNamesIdsArray.map(item => (
               <TableListItem key={item.tableId} item={item} filterDispatch={filterDispatch} />
             ))}
           </ul>
         </AccordionTab>
-        <AccordionTab header="Filter by Countries">
+        <AccordionTab header={resources.messages['filterByCountries']}>
           <ul className={styles.list}>
             {labels.map(item => (
               <CountriesListItem key={item} item={item} filterDispatch={filterDispatch} />
