6dacdf5930a1b725f9c60c31a395630d18085f70
mazpilic <mazpilicueta-ext@tracasa.es>
2019-10-01 15:22:38 +0200

    update VisibilityMenu to add a listener to html element in orther to hide menu when click out of it, add styles too

--- frontend-service/src/ui/views/ReporterDataSet/_components/TabsSchema/_components/DataViewer/_components/VisibilityMenu/VisibilityMenu.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/TabsSchema/_components/DataViewer/_components/VisibilityMenu/VisibilityMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faColumns } from '@fortawesome/free-solid-svg-icons';
+import styles from './VisibilityMenu.module.css';
 
 class VisibilityMenu extends React.Component {
   constructor(props) {
@@ -9,10 +9,13 @@ class VisibilityMenu extends React.Component {
         display: 'none'
       }
     };
-    this.show = this.show.bind(this);
+    ['show'].map(item => {
+      this[item] = this[item].bind(this);
+    });
   }
+
   show(event) {
-    console.log(event);
+    const htmlElement = document.getElementsByTagName('html');
 
     this.setState(state => {
       return {
@@ -20,14 +23,20 @@ class VisibilityMenu extends React.Component {
         style: {
           ...state.style,
           display: state.style.display === 'none' ? 'block' : 'none'
-        }
+        },
+        listener:
+          state.style.display === 'none'
+            ? htmlElement[0].addEventListener('click', e => {
+                this.show(e);
+              })
+            : htmlElement.removeEventListener('click', this.state.listener, false)
       };
     });
   }
   render() {
     const { columns } = this.props;
     return (
-      <div style={this.state.style}>
+      <div className={styles.visibilityMenu} style={this.state.style}>
         <ul>
           {columns.map((column, i) => (
             <li key={i}>{column.label}</li>
