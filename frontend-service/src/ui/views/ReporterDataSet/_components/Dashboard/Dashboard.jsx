562b95f7a8d7e5f7d9acfcb76ffa1ea72619a347
Kiko <fjlabiano@itracasa.es>
2019-10-04 15:08:23 +0200

    add color-picker to dataset dashboard

--- frontend-service/src/ui/views/ReporterDataSet/_components/Dashboard/Dashboard.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/Dashboard/Dashboard.jsx
@@ -5,20 +5,37 @@ import isUndefined from 'lodash/isUndefined';
 
 import styles from './Dashboard.module.css';
 
+import { ColorPicker } from 'primereact/colorpicker';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 import { Spinner } from 'ui/views/_components/Spinner';
 import { DatasetService } from 'core/services/DataSet';
 
 import { Chart } from 'primereact/chart';
 
+const SEVERITY_CODE = {
+  CORRECT: 1,
+  WARNING: 2,
+  ERROR: 3
+};
+
 const Dashboard = withRouter(
   React.memo(({ refresh, match: { params: { datasetId } } }) => {
+    const [dashboardColors, setDashboardColors] = useState();
     const [dashboardData, setDashboardData] = useState({});
     const [dashboardOptions, setDashboardOptions] = useState({});
     const [dashboardTitle, setDashboardTitle] = useState('');
+    const [datasetData, setDatasetData] = useState();
     const [isLoading, setIsLoading] = useState(false);
 
     const resources = useContext(ResourcesContext);
+
+    useEffect(() => {
+      setDashboardColors({
+        CORRECT: '#004494',
+        WARNING: '#ffd617',
+        ERROR: '#DA2131'
+      });
+    }, []);
 
     useEffect(() => {
       if (refresh) {
@@ -29,31 +46,65 @@ const Dashboard = withRouter(
       };
     }, [refresh, datasetId]);
 
+    useEffect(() => {
+      if (!isUndefined(datasetData)) {
+        setDashboardData({
+          labels: datasetData.tables.map(table => table.tableSchemaName),
+          datasets: [
+            {
+              label: 'Correct',
+              backgroundColor: !isUndefined(dashboardColors) ? dashboardColors.CORRECT : '#004494',
+              data: datasetData.tableStatisticPercentages[0],
+              totalData: datasetData.tableStatisticValues
+            },
+            {
+              label: 'Warning',
+              backgroundColor: !isUndefined(dashboardColors) ? dashboardColors.WARNING : '#ffd617',
+              data: datasetData.tableStatisticPercentages[1],
+              totalData: datasetData.tableStatisticValues
+            },
+            {
+              label: 'Error',
+              backgroundColor: !isUndefined(dashboardColors) ? dashboardColors.ERROR : '#DA2131',
+              data: datasetData.tableStatisticPercentages[2],
+              totalData: datasetData.tableStatisticValues
+            }
+          ]
+        });
+      }
+    }, [dashboardColors]);
+
+    const onChangeColor = (color, type) => {
+      const inmDashboardColors = { ...dashboardColors };
+      inmDashboardColors[Object.keys(SEVERITY_CODE)[type - 1]] = `#${color}`;
+      setDashboardColors(inmDashboardColors);
+    };
+
     const onLoadStatistics = async () => {
       setIsLoading(true);
       const dataset = await DatasetService.errorStatisticsById(datasetId);
       const tableStatisticValues = dataset.tableStatisticValues;
       const tableNames = dataset.tables.map(table => table.tableSchemaName);
-
+      setDatasetData(dataset);
       setDashboardTitle(dataset.datasetSchemaName);
       setDashboardData({
         labels: tableNames,
         datasets: [
           {
             label: 'Correct',
-            backgroundColor: '#004494',
+            backgroundColor: !isUndefined(dashboardColors) ? dashboardColors.CORRECT : '#004494',
             data: dataset.tableStatisticPercentages[0],
             totalData: tableStatisticValues
           },
           {
             label: 'Warning',
-            backgroundColor: '#ffd617',
+            backgroundColor: !isUndefined(dashboardColors) ? dashboardColors.WARNING : '#ffd617',
             data: dataset.tableStatisticPercentages[1],
             totalData: tableStatisticValues
           },
           {
             label: 'Error',
-            backgroundColor: '#DA2131',
+            backgroundColor: !isUndefined(dashboardColors) ? dashboardColors.ERROR : '#DA2131',
             data: dataset.tableStatisticPercentages[2],
             totalData: tableStatisticValues
           }
@@ -118,6 +169,24 @@ const Dashboard = withRouter(
     return (
       <React.Fragment>
         <h1>{dashboardTitle}</h1>
+        <div>
+          {Object.keys(SEVERITY_CODE).map((type, i) => {
+            return (
+              <React.Fragment key={i}>
+                <span key={`label_${type}`}>{`  ${type.charAt(0).toUpperCase()}${type.slice(1).toLowerCase()}: `}</span>
+                <ColorPicker
+                  key={type}
+                  value={!isUndefined(dashboardColors) ? dashboardColors[type] : '#004494'}
+                  onChange={e => {
+                    e.preventDefault();
+                    onChangeColor(e.value, SEVERITY_CODE[type]);
+                  }}
+                />
+              </React.Fragment>
+            );
+          })}
+        </div>
+        <hr />
         {renderDashboard()}
       </React.Fragment>
     );
