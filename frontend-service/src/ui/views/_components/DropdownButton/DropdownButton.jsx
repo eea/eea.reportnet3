6bfd764c1c33f6c4d339602a7f7483c1518ff490
mazpilic <mazpilicueta-ext@tracasa.es>
2019-10-09 13:18:26 +0200

    update DropdownMenu to remove comment code

--- frontend-service/src/ui/views/_components/DropdownButton/DropdownButton.jsx
+++ frontend-service/src/ui/views/_components/DropdownButton/DropdownButton.jsx
@@ -14,23 +14,6 @@ const DropdownButton = ({ children, icon, model, hasWritePermissions }) => {
       <div className={styles.dropdown} onClick={e => menuRef.current.show(e)}>
         <Icon icon={icon} style={{ fontSize: '1.5rem' }} />
         {children}
-        {/* <div
-        className={`${styles.dropdown_content} p-menu p-menu-dynamic p-menu-overlay p-component p-menu-overlay-visible`}
-        style={{ display: isVisible ? 'block' : 'none' }}>
-        <ul className="p-menu-list p-reset">
-          {model.map(item => {
-            if (!item.show) return <></>;
-            return (
-              <li className="p-menuitem" role="menuitem" key={item.label}>
-                <div className="p-menuitem-link" onClick={item.menuItemFunction}>
-                  <span className={`p-menuitem-icon ${config.icons[item.icon]}`} />
-                  <span className="p-menuitem-text">{item.label}</span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div> */}
       </div>
       <DropDownMenu ref={menuRef} model={model} />
     </div>
