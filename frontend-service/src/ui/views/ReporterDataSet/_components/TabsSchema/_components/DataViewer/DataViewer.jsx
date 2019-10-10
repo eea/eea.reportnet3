92da2318bd91f23c5ad0de09adcf72ee32825285
Miguel Azanza <mazanza@tracasa.es>
2019-10-10 16:16:30 +0200

    fix header size by adding actions header

--- frontend-service/src/ui/views/ReporterDataSet/_components/TabsSchema/_components/DataViewer/DataViewer.jsx
+++ frontend-service/src/ui/views/ReporterDataSet/_components/TabsSchema/_components/DataViewer/DataViewer.jsx
@@ -179,6 +179,7 @@ const DataViewer = withRouter(
       });
       let editCol = (
         <Column
+          header={resources.messages['actions']}
           key="actions"
           body={row => actionTemplate(row)}
           sortable={false}
