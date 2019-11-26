1bdd3b7bfe7d69a394a5bc54ca48a46b7e9d5adc
Kiko <fjlabiano@itracasa.es>
2019-11-26 13:26:46 +0100

    update create dataflow

--- frontend-service/src/ui/views/_components/DataFlowColumn/DataFlowColumn.jsx
+++ frontend-service/src/ui/views/_components/DataFlowColumn/DataFlowColumn.jsx
@@ -1,47 +1,22 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { withRouter } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { AwesomeIcons } from 'conf/AwesomeIcons';
 
 import { isUndefined } from 'lodash';
 
 import styles from './DataFlowColumn.module.css';
 
-import { config } from 'conf';
-
 import { Button } from 'ui/views/_components/Button';
 import { ConfirmDialog } from 'ui/views/_components/ConfirmDialog';
 import { CreateDataflowForm } from './_components/CreateDataflowForm';
 import { Dialog } from 'ui/views/_components/Dialog';
-import { Icon } from 'ui/views/_components/Icon';
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
-import { UserContext } from 'ui/views/_components/_context/UserContext';
-import { UserService } from 'core/services/User';
 
 const DataflowColumn = withRouter(
-  ({
-    navTitle,
-    dataflowTitle,
-    components = [],
-    entity,
-    createDataflowButtonTitle,
-    subscribeButtonTitle,
-    history,
-    style,
-    match
-  }) => {
+  ({ components = [], createDataflowButtonTitle, isCustodian, navTitle, style, subscribeButtonTitle }) => {
     const resources = useContext(ResourcesContext);
     const [createDataflowDialogVisible, setCreateDataflowDialogVisible] = useState(false);
-    const [isCustodian, setIsCustodian] = useState(false);
     const [isFormReset, setIsFormReset] = useState(true);
     const [subscribeDialogVisible, setSubscribeDialogVisible] = useState(false);
-    const user = useContext(UserContext);
-
-    useEffect(() => {
-      if (!isUndefined(user.accessRole)) {
-        setIsCustodian(UserService.hasPermission(user, [config.permissions.CUSTODIAN]));
-      }
-    }, [user]);
 
     const setVisibleHandler = (fnUseState, visible) => {
       fnUseState(visible);
@@ -75,7 +50,7 @@ const DataflowColumn = withRouter(
           </div>
         )}
         <div className="navSection">
-          {isCustodian && components.includes('createDataflow') && (
+          {isCustodian && components.includes('createDataflow') ? (
             <Button
               className={`${styles.columnButton} p-button-primary`}
               icon="plus"
@@ -86,7 +61,7 @@ const DataflowColumn = withRouter(
               }}
               style={{ textAlign: 'left' }}
             />
-          )}
+          ) : null}
 
           <Button
             className={styles.columnButton}
