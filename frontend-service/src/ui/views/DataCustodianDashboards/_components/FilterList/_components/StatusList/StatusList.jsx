a411aef412c627339a1f94952c6735e1966ce0e5
icojocaru <icojocaru-ext@tracasa.es>
2019-11-27 14:52:04 +0100

    fix Bug on dashboard

--- frontend-service/src/ui/views/DataCustodianDashboards/_components/FilterList/_components/StatusList/StatusList.jsx
+++ frontend-service/src/ui/views/DataCustodianDashboards/_components/FilterList/_components/StatusList/StatusList.jsx
@@ -5,13 +5,14 @@ import colors from 'conf/colors.json';
 
 import { ResourcesContext } from 'ui/views/_components/_context/ResourcesContext';
 
-export const StatusList = ({ color, levelErrors, filterDispatch, statusFilters }) => {
+export const StatusList = ({ color, levelErrors, filterDispatch, statusFilters, datasetSchemaId }) => {
+  console.log('statusFilters', datasetSchemaId);
   const resources = useContext(ResourcesContext);
   let errorListFilters = levelErrors.map((errorLevel, i) => {
     return (
       <li key={i} className={styles.listItem}>
         <input
-          id={errorLevel.toString().toLowerCase()}
+          id={errorLevel.toString().toLowerCase() + datasetSchemaId}
           className={styles.checkbox}
           style={{ backgroundColor: colors[errorLevel.toString().toLowerCase()] }}
           type="checkbox"
@@ -23,7 +24,7 @@ export const StatusList = ({ color, levelErrors, filterDispatch, statusFilters }
             });
           }}
         />
-        <label htmlFor={errorLevel.toString().toLowerCase()} className={styles.labelItem}>
+        <label htmlFor={errorLevel.toString().toLowerCase() + datasetSchemaId} className={styles.labelItem}>
           {resources.messages[errorLevel.toString().toLowerCase()]}
         </label>
       </li>
