055cdc15649db1350e41d7dec6c23c3519dcb3e1
icojocaru <icojocaru-ext@tracasa.es>
2019-11-29 09:07:17 +0100

    fix Removed comment

--- frontend-service/src/ui/views/DocumentationDataSet/_components/Documents/Documents.jsx
+++ frontend-service/src/ui/views/DocumentationDataSet/_components/Documents/Documents.jsx
@@ -159,13 +159,12 @@ const Documents = ({
     const sizeTypes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    const decimals = i === 0 ? 0 : 2;
+    const decimals = i !== 0 ? 2 : 0;
 
     const bytesParsed = parseFloat(bytes / k ** i).toFixed(decimals);
 
     const result = { bytesParsed, sizeType: sizeTypes[i] };
 
-    // return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
     return result;
   };
 
