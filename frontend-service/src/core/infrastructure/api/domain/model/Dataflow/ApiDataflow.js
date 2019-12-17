39341521e05b699b2286ed81f8628c2aaf9eb69a
Pablo Arrastia <parrastia-ext@outlook.es>
2019-12-17 10:10:07 +0100

    update delete dataflow endpoint

--- frontend-service/src/core/infrastructure/api/domain/model/Dataflow/ApiDataflow.js
+++ frontend-service/src/core/infrastructure/api/domain/model/Dataflow/ApiDataflow.js
@@ -105,8 +105,7 @@ export const apiDataflow = {
   deleteById: async dataflowId => {
     const tokens = userStorage.get();
     const response = await HTTPRequester.delete({
-      url: getUrl(DataflowConfig.createDataflow),
-      data: { id: dataflowId },
+      url: getUrl(DataflowConfig.createDataflow, { dataflowId }),
       queryString: {},
       headers: {
         Authorization: `Bearer ${tokens.accessToken}`
