53f97a99e508ee4f109228e38a69e250c5ef532a
icojocaru <icojocaru-ext@tracasa.es>
2019-12-31 13:55:57 +0100

    update Refactored folder and file structure

--- frontend-service/src/ui/views/Dataflow/_components/RepresentativesList/RepresentativesList.jsx
+++ frontend-service/src/ui/views/Dataflow/_components/RepresentativesList/RepresentativesList.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useReducer } from 'react';
 
-import { isEmpty, isNull, isUndefined } from 'lodash';
+import { isEmpty, isNull } from 'lodash';
 import styles from './RepresentativesList.module.scss';
 
-import { reducer } from './_components/reducer.jsx';
+import { reducer } from './_functions/Reducers/representativeReducer.js';
 
 import { Button } from 'ui/views/_components/Button';
 import { Column } from 'primereact/column';
@@ -11,8 +11,16 @@ import { ConfirmDialog } from 'ui/views/_components/ConfirmDialog';
 import { DataTable } from 'ui/views/_components/DataTable';
 import { Dropdown } from 'ui/views/_components/Dropdown';
 
-import { RepresentativeService } from 'core/services/Representative';
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
+import {
+  getInitialData,
+  getAllDataProviders,
+  createUnusedOptionsList,
+  onAddProvider,
+  onKeyDown,
+  onDataProviderIdChange,
+  onDeleteConfirm
+} from './_functions/Utils/representativeUtils';
 
 const RepresentativesList = ({ dataflowId }) => {
   const resources = useContext(ResourcesContext);
@@ -174,164 +182,3 @@ const RepresentativesList = ({ dataflowId }) => {
 };
 
 export { RepresentativesList };
-
-const getInitialData = async (formDispatcher, dataflowId, formState) => {
-  await getProviderTypes(formDispatcher);
-
-  await getAllRepresentatives(dataflowId, formDispatcher);
-
-  if (!isEmpty(formState.representatives)) {
-    await getAllDataProviders(formState.selectedDataProviderGroup, formDispatcher);
-
-    createUnusedOptionsList(formDispatcher);
-  }
-};
-
-const getAllDataProviders = async (selectedDataProviderGroup, formDispatcher) => {
-  try {
-    const responseAllDataProviders = await RepresentativeService.allDataProviders(selectedDataProviderGroup);
-
-    formDispatcher({
-      type: 'GET_DATA_PROVIDERS_LIST_BY_GROUP_ID',
-      payload: responseAllDataProviders
-    });
-  } catch (error) {
-    console.log('error', error);
-  }
-};
-
-const onDeleteConfirm = async (formDispatcher, formState) => {
-  try {
-    await RepresentativeService.deleteById(formState.representativeIdToDelete);
-
-    formDispatcher({
-      type: 'DELETE_REPRESENTATIVE',
-      payload: formState.representativeIdToDelete
-    });
-  } catch (error) {
-    console.log('error on RepresentativeService.deleteById: ', error);
-  }
-};
-
-const createUnusedOptionsList = formDispatcher => {
-  formDispatcher({
-    type: 'CREATE_UNUSED_OPTIONS_LIST'
-  });
-};
-
-const getAllRepresentatives = async (dataflowId, formDispatcher) => {
-  try {
-    const responseAllRepresentatives = await RepresentativeService.allRepresentatives(dataflowId);
-
-    const representativesByCopy = JSON.parse(JSON.stringify(responseAllRepresentatives.representatives));
-
-    formDispatcher({
-      type: 'INITIAL_LOAD',
-      payload: { response: responseAllRepresentatives, representativesByCopy }
-    });
-  } catch (error) {
-    console.log('error on RepresentativeService.allRepresentatives', error);
-  }
-};
-
-const getProviderTypes = async formDispatcher => {
-  try {
-    const providerTypes = await RepresentativeService.getProviderTypes();
-
-    formDispatcher({
-      type: 'GET_PROVIDERS_TYPES_LIST',
-      payload: providerTypes
-    });
-  } catch (error) {
-    console.log('error on  RepresentativeService.getProviderTypes', error);
-  }
-};
-
-const addRepresentative = async (formDispatcher, representatives, dataflowId) => {
-  const newRepresentative = representatives.filter(representative => isNull(representative.representativeId));
-
-  if (!isEmpty(newRepresentative[0].providerAccount) && !isEmpty(newRepresentative[0].dataProviderId)) {
-    try {
-      await RepresentativeService.add(
-        dataflowId,
-        newRepresentative[0].providerAccount,
-        parseInt(newRepresentative[0].dataProviderId)
-      );
-
-      formDispatcher({
-        type: 'ADD_REPRESENTATIVE'
-      });
-    } catch (error) {
-      console.log('error on RepresentativeService.add', error);
-    }
-  }
-};
-
-const updateRepresentative = async (formDispatcher, formState, updatedRepresentative) => {
-  let isChangedAccount = false;
-
-  formState.initialRepresentatives.forEach(representative => {
-    if (
-      representative.representativeId === updatedRepresentative.representativeId &&
-      representative.providerAccount !== updatedRepresentative.providerAccount
-    ) {
-      isChangedAccount = true;
-    }
-  });
-
-  if (isChangedAccount) {
-    try {
-      await RepresentativeService.updateProviderAccount(
-        parseInt(updatedRepresentative.representativeId),
-        updatedRepresentative.providerAccount
-      );
-
-      formDispatcher({
-        type: 'UPDATE_ACCOUNT'
-      });
-    } catch (error) {
-      console.log('error on RepresentativeService.updateProviderAccount', error);
-
-      formDispatcher({
-        type: 'REPRESENTATIVE_HAS_ERROR',
-        payload: updatedRepresentative.representativeId
-      });
-    }
-  }
-};
-
-const updateProviderId = async (formDispatcher, representativeId, newDataProviderId) => {
-  try {
-    await RepresentativeService.updateDataProviderId(parseInt(representativeId), parseInt(newDataProviderId));
-
-    formDispatcher({
-      type: 'ON_PROVIDER_CHANGE',
-      payload: { dataProviderId: newDataProviderId, representativeId }
-    });
-  } catch (error) {
-    console.log('error on RepresentativeService.updateDataProviderId', error);
-  }
-};
-
-const onDataProviderIdChange = (formDispatcher, newDataProviderId, representative) => {
-  if (!isNull(representative.representativeId) && !isUndefined(representative.representativeId)) {
-    updateProviderId(formDispatcher, representative.representativeId, newDataProviderId);
-  } else {
-    formDispatcher({
-      type: 'ON_PROVIDER_CHANGE',
-      payload: { dataProviderId: newDataProviderId, representativeId: representative.representativeId }
-    });
-  }
-};
-
-const onAddProvider = (formDispatcher, formState, representative, dataflowId) => {
-  isNull(representative.representativeId)
-    ? addRepresentative(formDispatcher, formState.representatives, dataflowId)
-    : updateRepresentative(formDispatcher, formState, representative);
-};
-
-const onKeyDown = (event, formDispatcher, formState, representative, dataflowId) => {
-  if (event.key === 'Enter') {
-    onAddProvider(formDispatcher, formState, representative, dataflowId);
-  }
-};
