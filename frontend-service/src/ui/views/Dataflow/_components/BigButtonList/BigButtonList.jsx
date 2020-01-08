f3a09baa0720475ec307773694fed2c194daa932
Pablo Arrastia <parrastia-ext@outlook.es>
2020-01-08 15:21:04 +0100

    add BiButtonList component & hook

---
+++ frontend-service/src/ui/views/Dataflow/_components/BigButtonList/BigButtonList.jsx
@@ -0,0 +1,170 @@
+import React, { useContext, useState } from 'react';
+
+import styles from './BigButtonList.module.css';
+
+import { BigButton } from './_components/BigButton';
+import { Button } from 'ui/views/_components/Button';
+import { ConfirmDialog } from 'ui/views/_components/ConfirmDialog';
+import { Dialog } from 'ui/views/_components/Dialog';
+import { NewDatasetSchemaForm } from './_components/NewDatasetSchemaForm';
+
+import { DatasetService } from 'core/services/Dataset';
+
+import { LoadingContext } from 'ui/views/_functions/Contexts/LoadingContext';
+import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
+
+import { useBigButtonList } from './_functions/Hooks/useBigButtonList';
+
+export const BigButtonList = ({
+  handleRedirect,
+  dataflowId,
+  dataflowData,
+  designDatasetSchemas,
+  dataflowStatus,
+  isCustodian,
+  hasWritePermissions,
+  onUpdateData,
+  showReleaseSnapshotDialog,
+  updatedDatasetSchema,
+  onSaveName
+}) => {
+  const { showLoading, hideLoading } = useContext(LoadingContext);
+  const resources = useContext(ResourcesContext);
+
+  const [newDatasetDialog, setNewDatasetDialog] = useState(false);
+  const [isDuplicated, setIsDuplicated] = useState(false);
+  const [deleteDialogVisible, setDeleteDialogVisible] = useState(false);
+  const [deleteSchemaIndex, setDeleteSchemaIndex] = useState();
+  const [errorDialogVisible, setErrorDialogVisible] = useState(false);
+  const [isFormReset, setIsFormReset] = useState(true);
+
+  const getDeleteSchemaIndex = index => {
+    setDeleteSchemaIndex(index);
+    setDeleteDialogVisible(true);
+  };
+
+  const onDatasetSchemaNameError = () => {
+    setErrorDialogVisible(true);
+  };
+
+  const onDuplicateName = () => {
+    setIsDuplicated(true);
+  };
+
+  const showNewDatasetDialog = () => {
+    setNewDatasetDialog(true);
+    setIsFormReset(true);
+  };
+
+  const onHideErrorDialog = () => {
+    setErrorDialogVisible(false);
+    setIsDuplicated(false);
+  };
+
+  const onDeleteDatasetSchema = async index => {
+    setDeleteDialogVisible(false);
+    showLoading();
+    try {
+      const response = await DatasetService.deleteSchemaById(designDatasetSchemas[index].datasetId);
+      if (response >= 200 && response <= 299) {
+        onUpdateData();
+      }
+    } catch (error) {
+      console.error(error.response);
+    } finally {
+      hideLoading();
+    }
+  };
+
+  const onCreateDatasetSchema = () => {
+    setNewDatasetDialog(false);
+  };
+
+  const errorDialogFooter = (
+    <div className="ui-dialog-buttonpane p-clearfix">
+      <Button
+        label={resources.messages['ok']}
+        icon="check"
+        onClick={() => {
+          setErrorDialogVisible(false);
+          setIsDuplicated(false);
+        }}
+      />
+    </div>
+  );
+
+  return (
+    <>
+      <div className={styles.buttonsWrapper}>
+        <div className={styles.splitButtonWrapper}>
+          <div className={styles.datasetItem}>
+            {useBigButtonList({
+              dataflowData: dataflowData,
+              dataflowId: dataflowId,
+              dataflowStatus: dataflowStatus,
+              getDeleteSchemaIndex: getDeleteSchemaIndex,
+              handleRedirect: handleRedirect,
+              hasWritePermissions: hasWritePermissions,
+              isCustodian: isCustodian,
+              onDatasetSchemaNameError: onDatasetSchemaNameError,
+              onDuplicateName: onDuplicateName,
+              onSaveName: onSaveName,
+              showNewDatasetDialog: showNewDatasetDialog,
+              showReleaseSnapshotDialog: showReleaseSnapshotDialog,
+              updatedDatasetSchema: updatedDatasetSchema
+            }).map(button => (button.visibility ? <BigButton {...button} /> : <></>))}
+          </div>
+
+          <div className={`${styles.datasetItem}`}>
+            <BigButton layout="dataCollection" caption="Create data collection" />
+          </div>
+        </div>
+      </div>
+
+      <Dialog
+        header={resources.messages['newDatasetSchema']}
+        visible={newDatasetDialog}
+        className={styles.dialog}
+        dismissableMask={false}
+        onHide={() => {
+          setNewDatasetDialog(false);
+          setIsFormReset(false);
+        }}>
+        <NewDatasetSchemaForm
+          dataflowId={dataflowId}
+          datasetSchemaInfo={updatedDatasetSchema}
+          isFormReset={isFormReset}
+          onCreate={onCreateDatasetSchema}
+          onUpdateData={onUpdateData}
+          setNewDatasetDialog={setNewDatasetDialog}
+        />
+      </Dialog>
+
+      <Dialog
+        footer={errorDialogFooter}
+        header={resources.messages['error'].toUpperCase()}
+        onHide={onHideErrorDialog}
+        visible={isDuplicated}>
+        <div className="p-grid p-fluid">{resources.messages['duplicateSchemaError']}</div>
+      </Dialog>
+
+      <Dialog
+        footer={errorDialogFooter}
+        header={resources.messages['error'].toUpperCase()}
+        onHide={onHideErrorDialog}
+        visible={errorDialogVisible}>
+        <div className="p-grid p-fluid">{resources.messages['emptyDatasetSchema']}</div>
+      </Dialog>
+
+      <ConfirmDialog
+        header={resources.messages['delete'].toUpperCase()}
+        labelCancel={resources.messages['no']}
+        labelConfirm={resources.messages['yes']}
+        onConfirm={() => onDeleteDatasetSchema(deleteSchemaIndex)}
+        onHide={() => setDeleteDialogVisible(false)}
+        visible={deleteDialogVisible}>
+        {resources.messages['deleteDatasetSchema']}
+      </ConfirmDialog>
+    </>
+  );
+};
---
+++ frontend-service/src/ui/views/Dataflow/_components/BigButtonList/_functions/Hooks/useBigButtonList.js
@@ -0,0 +1,212 @@
+import { useContext } from 'react';
+
+import { isEmpty, isUndefined } from 'lodash';
+
+import { config } from 'conf';
+import { routes } from 'ui/routes';
+
+import { getUrl } from 'core/infrastructure/CoreUtils';
+
+import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
+
+const useBigButtonList = ({
+  dataflowData,
+  dataflowId,
+  dataflowStatus,
+  getDeleteSchemaIndex,
+  handleRedirect,
+  hasWritePermissions,
+  isCustodian,
+  onDatasetSchemaNameError,
+  onDuplicateName,
+  onSaveName,
+  showNewDatasetDialog,
+  showReleaseSnapshotDialog,
+  updatedDatasetSchema
+}) => {
+  const resources = useContext(ResourcesContext);
+
+  const buttonList = [
+    {
+      layout: 'newItem',
+      caption: resources.messages['newItem'],
+      model: [
+        {
+          label: resources.messages['createNewEmptyDatasetSchema'],
+          icon: 'add',
+          command: () => showNewDatasetDialog()
+        },
+        {
+          label: resources.messages['createNewDatasetFromTemplate'],
+          icon: 'add',
+          disabled: true
+        }
+      ],
+      visibility: isCustodian && dataflowStatus === config.dataflowStatus['DESIGN']
+    },
+    {
+      layout: 'documents',
+      caption: resources.messages['dataflowHelp'],
+      handleRedirect: () =>
+        handleRedirect(
+          getUrl(
+            routes.DOCUMENTS,
+            {
+              dataflowId: dataflowId
+            },
+            true
+          )
+        ),
+      onWheel: getUrl(
+        routes.DOCUMENTS,
+        {
+          dataflowId: dataflowId
+        },
+        true
+      ),
+      visibility: true
+    }
+  ];
+  const designDatasetModels = dataflowData.designDatasets.map(newDatasetSchema => ({
+    layout: 'designDatasetSchema',
+    caption: newDatasetSchema.datasetSchemaName,
+    dataflowStatus: dataflowStatus,
+    datasetSchemaInfo: updatedDatasetSchema,
+    handleRedirect: () => {
+      handleRedirect(
+        getUrl(
+          routes.DATASET_SCHEMA,
+          {
+            dataflowId: dataflowId,
+            datasetId: newDatasetSchema.datasetId
+          },
+          true
+        )
+      );
+    },
+    index: newDatasetSchema.index,
+    onDuplicateName: onDuplicateName,
+    onSaveError: onDatasetSchemaNameError,
+    onSaveName: onSaveName,
+    onWheel: getUrl(
+      routes.DATASET_SCHEMA,
+      {
+        dataflowId: dataflowId,
+        datasetId: newDatasetSchema.datasetId
+      },
+      true
+    ),
+    placeholder: resources.messages['datasetSchemaNamePlaceholder'],
+    model: [
+      {
+        label: resources.messages['openDataset'],
+        icon: 'openFolder',
+        command: () => {
+          handleRedirect(
+            getUrl(
+              routes.DATASET_SCHEMA,
+              {
+                dataflowId: dataflowId,
+                datasetId: newDatasetSchema.datasetId
+              },
+              true
+            )
+          );
+        }
+      },
+      {
+        label: resources.messages['rename'],
+        icon: 'pencil',
+        disabled: dataflowStatus !== config.dataflowStatus['DESIGN']
+      },
+      {
+        label: resources.messages['duplicate'],
+        icon: 'clone',
+        disabled: true
+      },
+      {
+        label: resources.messages['delete'],
+        icon: 'trash',
+        disabled: dataflowStatus !== config.dataflowStatus['DESIGN'],
+        command: () => getDeleteSchemaIndex(newDatasetSchema.index)
+      },
+      {
+        label: resources.messages['properties'],
+        icon: 'info',
+        disabled: true
+      }
+    ],
+    visibility: !isUndefined(dataflowData.designDatasets)
+  }));
+
+  const datasetModels = dataflowData.datasets.map(dataset => ({
+    layout: 'dataset',
+    caption: dataset.datasetSchemaName,
+    isReleased: dataset.isReleased,
+    handleRedirect: () => {
+      handleRedirect(
+        getUrl(
+          routes.DATASET,
+          {
+            dataflowId: dataflowId,
+            datasetId: dataset.datasetId
+          },
+          true
+        )
+      );
+    },
+    onWheel: getUrl(
+      routes.DATASET,
+      {
+        dataflowId: dataflowId,
+        datasetId: dataset.datasetId
+      },
+      true
+    ),
+    model: hasWritePermissions
+      ? [
+          {
+            label: resources.messages['releaseDataCollection'],
+            icon: 'cloudUpload',
+            command: () => showReleaseSnapshotDialog(dataset.datasetId),
+            disabled: false
+          }
+        ]
+      : [
+          {
+            label: resources.messages['properties'],
+            icon: 'info',
+            disabled: true
+          }
+        ],
+    visibility: !isEmpty(dataflowData.datasets)
+  }));
+
+  const dashboardModels = [
+    {
+      layout: 'dashboard',
+      caption: resources.messages['dashboards'],
+      handleRedirect: () =>
+        handleRedirect(
+          getUrl(
+            routes.DASHBOARDS,
+            {
+              dataflowId: dataflowId
+            },
+            true
+          )
+        ),
+      onWheel: getUrl(
+        routes.DASHBOARDS,
+        {
+          dataflowId: dataflowId
+        },
+        true
+      ),
+      visibility: isCustodian && !isEmpty(dataflowData.datasets)
+    }
+  ];
+
+  return [...buttonList, ...designDatasetModels, ...datasetModels, ...dashboardModels];
+};
+export { useBigButtonList };
