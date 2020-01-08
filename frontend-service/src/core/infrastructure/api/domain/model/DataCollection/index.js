8bb6d1d66978aee6b5b51b935d489cf84649b8ac
Pablo Arrastia <parrastia-ext@outlook.es>
2020-01-08 15:19:40 +0100

    fix datacollection service issue

---
+++ frontend-service/src/core/infrastructure/api/domain/model/DataCollection/index.js
@@ -0,0 +1,1 @@
+export { apiDataCollection } from './ApiDataCollection';
--- frontend-service/src/core/services/DataCollection/index.js
+++ frontend-service/src/core/services/DataCollection/index.js
@@ -1,4 +1,5 @@
 import { Create } from './Create';
+import { dataCollectionRepository } from 'core/domain/model/DataCollection/DataCollectionRepository';
 
 export const DataCollectionService = {
   create: Create({ dataCollectionRepository })
