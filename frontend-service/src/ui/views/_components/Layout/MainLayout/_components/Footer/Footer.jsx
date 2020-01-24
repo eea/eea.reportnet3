68fd2f2c0149c0cea48cf02f1ecdfc453be3f6e0
mazpilic <mazpilicueta-ext@tracasa.es>
2020-01-24 09:29:58 +0100

    update footer to eliminate background color

--- frontend-service/src/ui/views/_components/Layout/MainLayout/_components/Footer/Footer.jsx
+++ frontend-service/src/ui/views/_components/Layout/MainLayout/_components/Footer/Footer.jsx
@@ -1,30 +1,28 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 
 import styles from './Footer.module.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { AwesomeIcons } from 'conf/AwesomeIcons';
 
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
 
 export const Footer = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
   const resources = useContext(ResourcesContext);
+  const footerLinks = [
+    { text: 'copyrightAbout' },
+    { text: 'copyrightLanguage' },
+    { text: 'copyrightResources' },
+    { text: 'copyrightCookies' },
+    { text: 'copyrightPrivacy' },
+    { text: 'copyrightLegal' },
+    { text: 'copyrightContact' }
+  ];
   return (
     <React.Fragment>
-      <div className={styles.slideUpFooter} onClick={() => setIsExpanded(!isExpanded)}>
-        <span className={styles.slideUpFooterSpan}>
-          <FontAwesomeIcon icon={AwesomeIcons('angleUp')} className={styles.slideUpFooterButton} />
-        </span>
-      </div>
-      <footer className={`${isExpanded ? styles.FooterExpanded : styles.FooterCollapsed} ${styles.Footer}`}>
-        <a href=".">{resources.messages['copyrightAbout']}</a>
-        <a href=".">{resources.messages['copyrightLanguage']}</a>
-        <a href=".">{resources.messages['copyrightResources']}</a>
-        <a href=".">{resources.messages['copyrightCookies']}</a>
-        <a href=".">{resources.messages['copyrightPrivacy']}</a>
-        <a href=".">{resources.messages['copyrightLegal']}</a>
-        <a href=".">{resources.messages['copyrightContact']}</a>
+      <footer className={`${styles.FooterExpanded} ${styles.Footer}`}>
+        {footerLinks.map((footerLink, i) => (
+          <>
+            <a href=".">{resources.messages[footerLink.text]}</a> {i < footerLinks ? '·' : ''}
+          </>
+        ))}
       </footer>
     </React.Fragment>
   );
