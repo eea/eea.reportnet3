11fee0abab7a557a61a0030e2eeb6039066593e5
Kiko <fjlabiano@itracasa.es>
2020-01-27 16:43:15 +0100

    fixing issues codelist manager

--- frontend-service/src/ui/views/_components/CodelistsManager/_components/Category/_components/Codelist/_functions/Utils/CodelistUtils.js
+++ frontend-service/src/ui/views/_components/CodelistsManager/_components/Category/_components/Codelist/_functions/Utils/CodelistUtils.js
@@ -2,6 +2,15 @@ const getItem = (items, selectedItem) => {
   return items.filter(item => item.id === selectedItem.id)[0];
 };
 
+const getItemByIndex = (items, itemId) => {
+  return items
+    .map(e => {
+      return e.id;
+    })
+    .indexOf(itemId);
+};
+
 export const CodelistUtils = {
-  getItem
+  getItem,
+  getItemByIndex
 };
