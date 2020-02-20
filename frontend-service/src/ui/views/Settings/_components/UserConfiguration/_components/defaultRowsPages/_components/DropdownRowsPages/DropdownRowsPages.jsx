b5e73ff15b5105f0eff6e96e677fea3c4d074d0a
Maxi Ragusa <mragusa-ext@tracasa.es>
2020-02-20 09:07:22 +0100

    manage dropDownRows

--- frontend-service/src/ui/views/Settings/_components/UserConfiguration/_components/defaultRowsPages/_components/DropdownRowsPages/DropdownRowsPages.jsx
+++ frontend-service/src/ui/views/Settings/_components/UserConfiguration/_components/defaultRowsPages/_components/DropdownRowsPages/DropdownRowsPages.jsx
@@ -5,39 +5,18 @@ export class DropdownRowsPages extends Component {
   constructor() {
     super();
     this.state = {
-      city: null,
-      car: null,
-      car2: 'BMW'
+      row: null
     };
 
     this.onCityChange = this.onCityChange.bind(this);
   }
 
   onCityChange(e) {
-    this.setState({ city: e.value });
-  }
-  carTemplate(option) {
-    if (!option.value) {
-      return option.label;
-    } else {
-      var logoPath = 'showcase/resources/demo/images/car/' + option.label + '.png';
-
-      return (
-        <div className="p-clearfix">
-          <img
-            alt={option.label}
-            src={logoPath}
-            style={{ display: 'inline-block', margin: '5px 0 0 5px' }}
-            width="24"
-          />
-          <span style={{ float: 'right', margin: '.5em .25em 0 0' }}>{option.label}</span>
-        </div>
-      );
-    }
+    this.setState({ row: e.value });
   }
 
   render() {
-    const cities = [{ row: 5 }, { row: 10 }, { row: 20 }, { row: 100 }];
+    const rows = [{ row: 5 }, { row: 10 }, { row: 20 }, { row: 100 }];
 
     return (
       <div>
@@ -47,8 +26,8 @@ export class DropdownRowsPages extends Component {
 
         <div className="content-section implementation">
           <Dropdown
-            value={this.state.city}
-            options={cities}
+            value={this.state.row}
+            options={rows}
             onChange={this.onCityChange}
             placeholder={this.props.placeholder}
             optionLabel="row"
