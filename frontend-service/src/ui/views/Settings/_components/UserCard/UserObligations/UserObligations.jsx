f04477003757ff5399c6b7fd625d6b0e8d2bbe1b
Miriam <mgaviria-ext@tracasa.es>
2020-02-20 09:11:15 +0100

    ADD treeview (not finish)

--- frontend-service/src/ui/views/Settings/_components/UserCard/UserObligations/UserObligations.jsx
+++ frontend-service/src/ui/views/Settings/_components/UserCard/UserObligations/UserObligations.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 import styles from './UserObligations.module.scss';
 
+import { Button } from 'ui/views/_components/Button';
+
+import { TreeViewExpandableItem } from 'ui/views/_components/TreeView/_components/TreeViewExpandableItem';
+
+import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
+
 const UserObligations = () => {
+  const resources = useContext(ResourcesContext);
+
+  const [isUserObligationsCollapse, setIsUserObligationsCollapse] = useState(true);
+
+  console.log(`isUserObligationsCollapse ${isUserObligationsCollapse}`);
+
+  const dataflow = 'dataflow';
+
   return (
     <div className={styles.userObligationsContainer}>
-      <div className={styles.userObligationsTitle}>UserObligations</div>
+      <div className={styles.userObligationsTitle}>Obligations</div>
+
+      {/* <div>
+        <span>Dataflow 1</span>
+        <Button
+          className={`p-button-secondary-transparent ${styles.orderIcon}`}
+          icon={isUserObligationsCollapse === true ? 'angleRight' : 'angleDown'}
+          onClick={() => setIsUserObligationsCollapse(!isUserObligationsCollapse)}
+          tooltip={
+            isUserObligationsCollapse === true ? resources.messages['expandAll'] : resources.messages['collapseAll']
+          }
+          tooltipOptions={{ position: 'bottom' }}
+        />
+      </div> */}
+
+      <TreeViewExpandableItem
+        className={styles.obligationsExpandable}
+        expanded={false}
+        items={[{ label: dataflow }, { label: dataflow }]}
+        children={['dataflow1 ', 'dataflow2 ']}
+      />
     </div>
   );
 };
