6fe5a573580eae29b5bf4e0e3634eb2ad7998a45
Miriam <mgaviria-ext@tracasa.es>
2020-02-20 13:56:04 +0100

    ADD treeview with children
    ADD treeview with children in UserObligations

--- frontend-service/src/ui/views/Settings/_components/UserCard/UserObligations/UserObligations.jsx
+++ frontend-service/src/ui/views/Settings/_components/UserCard/UserObligations/UserObligations.jsx
@@ -1,45 +1,50 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import styles from './UserObligations.module.scss';
 
-import { Button } from 'ui/views/_components/Button';
-
 import { TreeViewExpandableItem } from 'ui/views/_components/TreeView/_components/TreeViewExpandableItem';
 
-import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
-
 const UserObligations = () => {
-  const resources = useContext(ResourcesContext);
+  const [dataflowData, setDataflowData] = useState([]);
 
-  const [isUserObligationsCollapse, setIsUserObligationsCollapse] = useState(true);
+  useEffect(() => {
+    onLoadDataflowData();
+  }, []);
 
-  console.log(`isUserObligationsCollapse ${isUserObligationsCollapse}`);
+  const onLoadDataflowData = () => {
+    const arrayDataflow = [
+      {
+        dataflow: 'dataflow1',
+        dataset: ['dataset1 (Reporting)', 'dataset1 (Schema)']
+      },
+      {
+        dataflow: 'dataflow2',
+        dataset: ['dataset2 (Reporting)', 'dataset2 (Schema)']
+      }
+    ];
+    setDataflowData(arrayDataflow);
+  };
 
-  const dataflow = 'dataflow';
+  const renderChildrenArray = () => {
+    return dataflowData.map(item => (
+      <TreeViewExpandableItem
+        className={styles.obligationsExpandable}
+        expanded={false}
+        items={[{ label: item.dataflow }]}
+        children={['dataflow1  ']}>
+        {item.dataset.map(dataset => (
+          <ul>
+            <li className={styles.children}>{dataset}</li>
+          </ul>
+        ))}
+      </TreeViewExpandableItem>
+    ));
+  };
 
   return (
     <div className={styles.userObligationsContainer}>
       <div className={styles.userObligationsTitle}>Obligations</div>
-
-      {/* <div>
-        <span>Dataflow 1</span>
-        <Button
-          className={`p-button-secondary-transparent ${styles.orderIcon}`}
-          icon={isUserObligationsCollapse === true ? 'angleRight' : 'angleDown'}
-          onClick={() => setIsUserObligationsCollapse(!isUserObligationsCollapse)}
-          tooltip={
-            isUserObligationsCollapse === true ? resources.messages['expandAll'] : resources.messages['collapseAll']
-          }
-          tooltipOptions={{ position: 'bottom' }}
-        />
-      </div> */}
-
-      <TreeViewExpandableItem
-        className={styles.obligationsExpandable}
-        expanded={false}
-        items={[{ label: dataflow }, { label: dataflow }]}
-        children={['dataflow1 ', 'dataflow2 ']}
-      />
+      {renderChildrenArray()}
     </div>
   );
 };
