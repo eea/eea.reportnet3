2e21f9b44acb9d3214d9e6f07076874487e72f8f
Miriam <mgaviria-ext@tracasa.es>
2020-02-21 08:51:58 +0100

    ADD dropdown in UserDesignOptions

--- frontend-service/src/ui/views/Settings/_components/UserDesignOptions/UserDesignOptions.jsx
+++ frontend-service/src/ui/views/Settings/_components/UserDesignOptions/UserDesignOptions.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { Dropdown } from 'ui/views/_components/Dropdown';
+import { UserContext } from 'ui/views/_functions/Contexts/UserContext';
+import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
 
 import styles from './UserDesignOptions.module.scss';
 
 const UserDesignOptions = () => {
+  const resources = useContext(ResourcesContext);
+  const userContext = useContext(UserContext);
+
   return (
     <div className={styles.userDesignContainer}>
-      <div className={styles.userLogoBoxContainer}></div>
-
-      <div className={styles.userName}>UserDesignOptions</div>
-
-      <div className={styles.userMail}>UserDesignOptions</div>
+      <h3 className={styles.userThemeTitle}>{resources.messages['defaultVisualTheme']}</h3>
+      <Dropdown
+        className={styles.dropdownFieldType}
+        options={[
+          { label: 'light', value: 'light' },
+          { label: 'dark', value: 'dark' }
+        ]}
+        onChange={e => {}}
+        placeholder={resources.messages['manageRolesDialogDropdownPlaceholder']}
+        value="light"
+      />
     </div>
   );
 };
