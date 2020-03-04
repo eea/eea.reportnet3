17d93e9c86fcd89025ff50bc16b8374b059ddd47
Eduardo Ayala Collado <eduardo.ayala@altia.es>
2020-03-04 11:55:32 +0100

    [FIX]fix new data with codelist
    Eduardo Ayala Collado
    Altia Alicante

--- common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/schemas/FieldSchemaVO.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/schemas/FieldSchemaVO.java
@@ -29,8 +29,8 @@ public class FieldSchemaVO {
   /** The type. */
   private DataType type;
 
-  /** The id code list. */
-  private Long idCodeList;
+  /** The code list items. */
+  private String[] codeListItems;
 
   /** The required. */
   private Boolean required;
@@ -42,7 +42,7 @@ public class FieldSchemaVO {
    */
   @Override
   public int hashCode() {
-    return Objects.hash(id, description, idRecord, name, type, idCodeList, required);
+    return Objects.hash(id, description, idRecord, name, type, codeListItems, required);
   }
 
   /**
@@ -62,7 +62,7 @@ public class FieldSchemaVO {
     FieldSchemaVO other = (FieldSchemaVO) obj;
     return Objects.equals(id, other.id) && Objects.equals(idRecord, other.idRecord)
         && Objects.equals(description, other.description) && Objects.equals(name, other.name)
-        && Objects.equals(type, other.type) && Objects.equals(idCodeList, other.idCodeList)
+        && Objects.equals(type, other.type) && Objects.equals(codeListItems, other.codeListItems)
         && Objects.equals(required, other.required);
   }
 }
