9a20fe6f24603439903d20cfe0c9221417131394
ruben.lozano <ruben.lozano@altia.es>
2020-03-17 10:42:20 +0100

    FIX new event

---
+++ document-container-service/src/main/java/org/eea/document/io/notification/events/UpdateDocumentCompletedEvent.java
@@ -0,0 +1,52 @@
+package org.eea.document.io.notification.events;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.eea.exception.EEAException;
+import org.eea.interfaces.controller.dataflow.DataFlowController.DataFlowControllerZuul;
+import org.eea.kafka.domain.EventType;
+import org.eea.kafka.domain.NotificationVO;
+import org.eea.notification.event.NotificableEventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * The Class UpdateDocumentCompletedEvent.
+ */
+@Component
+public class UpdateDocumentCompletedEvent implements NotificableEventHandler {
+
+  /** The dataflow controller zuul. */
+  @Autowired
+  private DataFlowControllerZuul dataflowControllerZuul;
+
+  /**
+   * Gets the event type.
+   *
+   * @return the event type
+   */
+  @Override
+  public EventType getEventType() {
+    return EventType.UPDATED_DOCUMENT_COMPLETED_EVENT;
+  }
+
+  /**
+   * Gets the map.
+   *
+   * @param notificationVO the notification VO
+   * @return the map
+   * @throws EEAException the EEA exception
+   */
+  @Override
+  public Map<String, Object> getMap(NotificationVO notificationVO) throws EEAException {
+    Map<String, Object> notification = new HashMap<>();
+    notification.put("user", notificationVO.getUser());
+    notification.put("dataflowId", notificationVO.getDataflowId());
+    notification.put("dataflowName",
+        notificationVO.getDataflowName() != null ? notificationVO.getDataflowName()
+            : dataflowControllerZuul.findById(notificationVO.getDataflowId()).getName());
+    notification.put("fileName", notificationVO.getFileName());
+    return notification;
+  }
+}
