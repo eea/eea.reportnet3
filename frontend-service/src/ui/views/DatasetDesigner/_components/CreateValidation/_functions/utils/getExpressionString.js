10a55714279fee90248b5cd5857a8e25cf5ef195
azpilm <mazpilicueta-ext@tracasa.es>
2020-03-20 14:24:17 +0100

    fix field appears in formula textarea when rule is empty

--- frontend-service/src/ui/views/DatasetDesigner/_components/CreateValidation/_functions/utils/getExpressionString.js
+++ frontend-service/src/ui/views/DatasetDesigner/_components/CreateValidation/_functions/utils/getExpressionString.js
@@ -1,8 +1,9 @@
 import isNil from 'lodash/isNil';
+import isEmpty from 'lodash/isEmpty';
 
 export const getExpressionString = (expressions, field) => {
   let expressionString = '';
-  if (!isNil(field) && expressions.length > 0) {
+  if (!isNil(field) && expressions.length > 0 && !isEmpty(expressions[0].operatorType)) {
     const { label: fieldLabel } = field;
     expressions.forEach((expression, i) => {
       const { union: unionValue, operatorValue: operator, expressionValue, expressions } = expression;
