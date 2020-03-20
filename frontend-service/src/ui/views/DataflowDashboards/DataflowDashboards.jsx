d687a14fd2c473ce232837746b12252be751bb06
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-03-20 13:15:33 +0100

    update Some minor refactor

--- frontend-service/src/ui/views/DataflowDashboards/DataflowDashboards.jsx
+++ frontend-service/src/ui/views/DataflowDashboards/DataflowDashboards.jsx
@@ -22,156 +22,165 @@ import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext'
 
 import { getUrl } from 'core/infrastructure/CoreUtils';
 
-export const DataflowDashboards = withRouter(({ match, history }) => {
-  const breadCrumbContext = useContext(BreadCrumbContext);
-  const leftSideBarContext = useContext(LeftSideBarContext);
-  const resources = useContext(ResourcesContext);
+export const DataflowDashboards = withRouter(
+  ({
+    match: {
+      params: { dataflowId }
+    },
+    history
+  }) => {
+    const breadCrumbContext = useContext(BreadCrumbContext);
+    const leftSideBarContext = useContext(LeftSideBarContext);
+    const resources = useContext(ResourcesContext);
 
-  const [dashboardInitialValues, setDashboardInitialValues] = useState({});
-  const [dataflowName, setDataflowName] = useState('');
-  const [dataSchema, setDataSchema] = useState();
+    const [dashboardInitialValues, setDashboardInitialValues] = useState({});
+    const [dataflowName, setDataflowName] = useState('');
+    const [dataSchema, setDataSchema] = useState();
 
-  useEffect(() => {
-    breadCrumbContext.add([
-      {
-        label: resources.messages['dataflows'],
-        icon: 'home',
-        href: routes.DATAFLOWS,
-        command: () => history.push(getUrl(routes.DATAFLOWS))
-      },
-      {
-        label: resources.messages['dataflow'],
-        icon: 'archive',
-        href: getUrl(
-          routes.DATAFLOW,
-          {
-            dataflowId: match.params.dataflowId
-          },
-          true
-        ),
-        command: () =>
-          history.push(
-            getUrl(
-              routes.DATAFLOW,
-              {
-                dataflowId: match.params.dataflowId
-              },
-              true
+    useEffect(() => {
+      breadCrumbContext.add([
+        {
+          label: resources.messages['dataflows'],
+          icon: 'home',
+          href: routes.DATAFLOWS,
+          command: () => history.push(getUrl(routes.DATAFLOWS))
+        },
+        {
+          label: resources.messages['dataflow'],
+          icon: 'archive',
+          href: getUrl(
+            routes.DATAFLOW,
+            {
+              dataflowId
+            },
+            true
+          ),
+          command: () =>
+            history.push(
+              getUrl(
+                routes.DATAFLOW,
+                {
+                  dataflowId
+                },
+                true
+              )
             )
-          )
-      },
-      {
-        label: resources.messages.dashboards,
-        icon: 'barChart'
+        },
+        {
+          label: resources.messages['dashboards'],
+          icon: 'barChart'
+        }
+      ]);
+      leftSideBarContext.removeModels();
+    }, []);
+
+    useEffect(() => {
+      try {
+        getDataflowName();
+        onLoadDataSchemas();
+      } catch (error) {
+        console.error(error.response);
       }
-    ]);
-    leftSideBarContext.removeModels();
-  }, []);
+    }, []);
 
-  useEffect(() => {
-    try {
-      getDataflowName();
-      onLoadDataSchemas();
-    } catch (error) {
-      console.error(error.response);
-    }
-  }, []);
+    const getDataflowName = async () => {
+      const dataflowData = await DataflowService.dataflowDetails(dataflowId);
+      setDataflowName(dataflowData.name);
+    };
 
-  const getDataflowName = async () => {
-    const dataflowData = await DataflowService.dataflowDetails(match.params.dataflowId);
-    setDataflowName(dataflowData.name);
-  };
+    const onLoadDataSchemas = async () => {
+      try {
+        const dataflow = await DataflowService.reporting(dataflowId);
+        setDataSchema(dataflow.designDatasets);
+        setDashboardInitialValues(
+          dataflow.designDatasets.forEach(schema => {
+            dashboardInitialValues[schema.datasetSchemaId] = true;
+          })
+        );
+      } catch (error) {
+        if (error.response.status === 401 || error.response.status === 403) {
+          history.push(getUrl(routes.DATAFLOWS));
+        }
+      } finally {
+      }
+    };
 
-  const onLoadDataSchemas = async () => {
-    try {
-      const dataflow = await DataflowService.reporting(match.params.dataflowId);
-      setDataSchema(dataflow.designDatasets);
-      setDashboardInitialValues(
-        dataflow.designDatasets.forEach(schema => {
-          dashboardInitialValues[schema.datasetSchemaId] = true;
+    const chartReducer = (state, { type, payload }) => {
+      switch (type) {
+        case 'TOGGLE_SCHEMA_CHART':
+          return {
+            ...state,
+            [payload]: !state[payload]
+          };
+
+        default:
+          return {
+            ...state
+          };
+      }
+    };
+
+    const [chartState, chartDispatch] = useReducer(chartReducer, dashboardInitialValues);
+
+    const onLoadButtons = !isUndefined(dataSchema)
+      ? dataSchema.map(schema => {
+          return (
+            <Button
+              className={`p-button-rounded ${
+                chartState[schema.datasetSchemaId] ? 'p-button-primary' : 'p-button-secondary'
+              } ${styles.dashboardsButton}`}
+              key={schema.datasetSchemaId}
+              label={schema.datasetSchemaName}
+              icon={chartState[schema.datasetSchemaId] ? 'eye' : 'eye-slash'}
+              onClick={() => chartDispatch({ type: 'TOGGLE_SCHEMA_CHART', payload: schema.datasetSchemaId })}
+            />
+          );
         })
+      : null;
+
+    const onLoadCharts = !isUndefined(dataSchema)
+      ? dataSchema.map(schema => {
+          return (
+            <DatasetValidationDashboard
+              key={schema.datasetSchemaId}
+              datasetSchemaId={schema.datasetSchemaId}
+              isVisible={chartState[schema.datasetSchemaId]}
+              datasetSchemaName={schema.datasetSchemaName}
+            />
+          );
+        })
+      : null;
+
+    const layout = children => {
+      return (
+        <MainLayout>
+          <div className="rep-container">{children}</div>
+        </MainLayout>
       );
-    } catch (error) {
-      if (error.response.status === 401 || error.response.status === 403) {
-        history.push(getUrl(routes.DATAFLOWS));
-      }
-    } finally {
-    }
-  };
+    };
 
-  const chartReducer = (state, { type, payload }) => {
-    switch (type) {
-      case 'TOGGLE_SCHEMA_CHART':
-        return {
-          ...state,
-          [payload]: !state[payload]
-        };
+    return layout(
+      <>
+        <Title title={resources.messages['dashboards']} subtitle={dataflowName} icon="barChart" iconSize="4.5rem" />
+        <div className={styles.validationChartWrap}>
+          <h2 className={styles.dashboardType}>{resources.messages['validationDashboards']}</h2>
 
-      default:
-        return {
-          ...state
-        };
-    }
-  };
+          <Toolbar className={styles.chartToolbar}>
+            <div className="p-toolbar-group-left">{onLoadButtons}</div>
+          </Toolbar>
 
-  const [chartState, chartDispatch] = useReducer(chartReducer, dashboardInitialValues);
+          {!Object.values(chartState).includes(true) && (
+            <div className={styles.informationText}>{resources.messages['noDashboardSelected']}</div>
+          )}
 
-  const onLoadButtons = !isUndefined(dataSchema)
-    ? dataSchema.map(schema => {
-        return (
-          <Button
-            className={`p-button-rounded ${
-              chartState[schema.datasetSchemaId] ? 'p-button-primary' : 'p-button-secondary'
-            } ${styles.dashboardsButton}`}
-            key={schema.datasetSchemaId}
-            label={schema.datasetSchemaName}
-            icon={chartState[schema.datasetSchemaId] ? 'eye' : 'eye-slash'}
-            onClick={() => chartDispatch({ type: 'TOGGLE_SCHEMA_CHART', payload: schema.datasetSchemaId })}
-          />
-        );
-      })
-    : null;
+          {onLoadCharts}
+        </div>
 
-  const onLoadCharts = !isUndefined(dataSchema)
-    ? dataSchema.map(schema => {
-        return (
-          <DatasetValidationDashboard
-            key={schema.datasetSchemaId}
-            datasetSchemaId={schema.datasetSchemaId}
-            isVisible={chartState[schema.datasetSchemaId]}
-            datasetSchemaName={schema.datasetSchemaName}
-          />
-        );
-      })
-    : null;
-
-  const layout = children => {
-    return (
-      <MainLayout>
-        <div className="rep-container">{children}</div>
-      </MainLayout>
+        <div className={styles.releasedChartWrap}>
+          <h2 className={styles.dashboardType}>{resources.messages['releaseDashboard']}</h2>
+          <ReleasedDatasetsDashboard dataflowId={dataflowId} />
+        </div>
+      </>
     );
-  };
-
-  return layout(
-    <>
-      <Title title={resources.messages['dashboards']} subtitle={dataflowName} icon="barChart" iconSize="4.5rem" />
-      <div className={styles.validationChartWrap}>
-        <h2 className={styles.dashboardType}>{resources.messages['validationDashboards']}</h2>
-        <Toolbar className={styles.chartToolbar}>
-          <div className="p-toolbar-group-left">{onLoadButtons}</div>
-        </Toolbar>
-        {Object.values(chartState).includes(true) ? (
-          <></>
-        ) : (
-          <div className={styles.informationText}>{resources.messages['noDashboardSelected']}</div>
-        )}
-        {onLoadCharts}
-      </div>
-      <div className={styles.releasedChartWrap}>
-        <h2 className={styles.dashboardType}>{resources.messages['releaseDashboard']}</h2>
-        <ReleasedDatasetsDashboard dataflowId={match.params.dataflowId} />
-      </div>
-    </>
-  );
-});
+  }
+);
