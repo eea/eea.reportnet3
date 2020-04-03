33c5183ed8968eac2c9df239729373d2b362c22d
azpilm <mazpilicueta-ext@tracasa.es>
2020-04-03 15:13:01 +0200

    update getExpression in DTO parsing to prepare for future operator type additions

--- frontend-service/src/core/infrastructure/domain/model/Validation/Utils/getExpression.js
+++ frontend-service/src/core/infrastructure/domain/model/Validation/Utils/getExpression.js
@@ -1,28 +1,33 @@
 import { config } from 'conf';
 
+const getOperatorEquivalence = (operatorType, operatorValue) => {
+  const {
+    validations: { operatorEquivalences }
+  } = config;
+  if (operatorEquivalences[operatorType]) {
+    return operatorEquivalences[operatorType][operatorValue];
+  }
+  return operatorEquivalences.default[operatorValue];
+};
+
 export const getExpression = expression => {
+  const { operatorType, operatorValue, expressionValue } = expression;
+  const {
+    validations: { nonNumericOperators }
+  } = config;
   if (expression.operatorType == 'LEN') {
     return {
-      operator: config.validations.operatorEquivalences[expression.operatorValue],
+      operator: getOperatorEquivalence(operatorType, operatorValue),
       arg1: {
         operator: 'LEN',
         arg1: 'VALUE'
       },
-      arg2: parseInt(expression.expressionValue)
-    };
-  }
-  if (expression.operatorType == 'string') {
-    return {
-      arg1: 'VALUE',
-      operator: config.validations.stringOperatorsEquivalences[expression.operatorValue],
-      arg2: expression.expressionValue
+      arg2: Number(expressionValue)
     };
   }
   return {
     arg1: 'VALUE',
-    operator: config.validations.operatorEquivalences[expression.operatorValue],
-    arg2: !config.validations.nonNumericOperators.includes(expression.operatorType)
-      ? parseInt(expression.expressionValue)
-      : expression.expressionValue
+    operator: getOperatorEquivalence(operatorType, operatorValue),
+    arg2: !nonNumericOperators.includes(operatorType) ? Number(expressionValue) : expressionValue
   };
 };
