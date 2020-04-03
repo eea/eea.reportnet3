17e6c7d6329db6256397144719b9f2a761402348
vicente.cano <vicente.cano@altia.es>
2020-04-03 12:10:34 +0200

    FIX commit sql

--- database/src/main/resources/db/migration/V11__Alter_FK_Dataset.sql
+++ database/src/main/resources/db/migration/V11__Alter_FK_Dataset.sql
@@ -1,20 +1,12 @@
 
--- DROP AND ADD AGAIN THE DIFFERENT FOREIGN KEYS OF THE RELATIONS TO DATAFLOW. TO ADD DELETE CASCADE WHEN A DATAFLOW IS DELETED
+-- DROP AND ADD AGAIN THE DIFFERENT FOREIGN KEYS OF DATASET METABASE. TO ADD DELETE CASCADE WHEN A DESIGN DATASET IS DELETED
 
-ALTER TABLE public."document" DROP CONSTRAINT IF EXISTS document_dataflow_fkey;
-ALTER TABLE public."document" ADD CONSTRAINT document_dataflow_fkey FOREIGN KEY (dataflow_id) REFERENCES dataflow(id) ON DELETE CASCADE;
+ALTER TABLE public.partition_dataset DROP CONSTRAINT IF EXISTS partition_dataset_dataset_fkey;
+ALTER TABLE public.partition_dataset ADD CONSTRAINT partition_dataset_dataset_fkey FOREIGN KEY (id_dataset) REFERENCES dataset(id) ON DELETE CASCADE;
 
-ALTER TABLE public.contributor DROP CONSTRAINT IF EXISTS dataflow_contributor_fkey;
-ALTER TABLE public.contributor ADD CONSTRAINT dataflow_contributor_fkey FOREIGN KEY (dataflow_id) REFERENCES dataflow(id) ON DELETE CASCADE;
+ALTER TABLE public.design_dataset DROP CONSTRAINT IF EXISTS dataset_design_fkey;
+ALTER TABLE public.design_dataset ADD CONSTRAINT dataset_design_fkey FOREIGN KEY (id) REFERENCES dataset(id) ON DELETE CASCADE;
 
-ALTER TABLE public.weblink DROP CONSTRAINT IF EXISTS weblink_dataflow_fkey;
-ALTER TABLE public.weblink ADD CONSTRAINT weblink_dataflow_fkey FOREIGN KEY (dataflow_id) REFERENCES dataflow(id) ON DELETE CASCADE;
 
-ALTER TABLE public.representative DROP CONSTRAINT IF EXISTS  dataflow_id;
-ALTER TABLE public.representative DROP CONSTRAINT IF EXISTS  dataflow_fk;
-ALTER TABLE public.representative ADD CONSTRAINT dataflow_fk FOREIGN KEY (dataflow_id) REFERENCES dataflow(id) ON DELETE CASCADE;
-
-ALTER TABLE public.dataflow_user_request DROP CONSTRAINT IF EXISTS user_request_dataflow_pkey;
-ALTER TABLE public.dataflow_user_request ADD CONSTRAINT user_request_dataflow_pkey FOREIGN KEY (dataflow_id) REFERENCES dataflow(id) ON DELETE CASCADE;
 
 commit;
