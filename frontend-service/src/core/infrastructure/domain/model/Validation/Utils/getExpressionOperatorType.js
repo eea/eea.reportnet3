ad172026234220dcd40546a818203a54f84fb672
azpilm <mazpilicueta-ext@tracasa.es>
2020-04-04 21:07:50 +0200

    add match operator to getOperatorType

--- frontend-service/src/core/infrastructure/domain/model/Validation/Utils/getExpressionOperatorType.js
+++ frontend-service/src/core/infrastructure/domain/model/Validation/Utils/getExpressionOperatorType.js
@@ -1,7 +1,7 @@
 import { config } from 'conf';
 
 export const getExpressionOperatorType = operator => {
-  if (!['SEQ', 'SEQIC'].includes(operator)) {
+  if (!['SEQ', 'SEQIC', 'MATCH'].includes(operator)) {
     return config.validations.operatorTypes.number.option.value;
   } else {
     return config.validations.operatorTypes.string.option.value;
