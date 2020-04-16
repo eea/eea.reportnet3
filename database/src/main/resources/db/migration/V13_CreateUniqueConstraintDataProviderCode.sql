64d2cc3549213d0c25554efb2f9b4972ba1f5527
Eduardo Ayala Collado <eduardo.ayala@altia.es>
2020-04-16 11:26:14 +0200

    [ADD]add database
    Eduardo Ayala Collado
    Altia Alicante

--- database/src/main/resources/db/migration/V13_CreateUniqueConstraintDataProviderCode.sql
+++ database/src/main/resources/db/migration/V13_CreateUniqueConstraintDataProviderCode.sql
@@ -1,1 +1,2 @@
-ALTER TABLE public.data_provider ADD CONSTRAINT data_provider_un UNIQUE (code);
+ALTER TABLE public.data_provider DROP CONSTRAINT IF EXISTS unique_data_provider;
+ALTER TABLE public.data_provider ADD CONSTRAINT unique_data_provider UNIQUE ("type",code);
