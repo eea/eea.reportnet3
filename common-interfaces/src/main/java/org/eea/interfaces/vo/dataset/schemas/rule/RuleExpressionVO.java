4ad053f0fffcc8c38f094355dec967df62d6f1f6
José Ángel <joseangel.belda@altia.es>
2020-05-13 08:36:22 +0200

    UPDATE comment code in progress

--- common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/schemas/rule/RuleExpressionVO.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/schemas/rule/RuleExpressionVO.java
@@ -517,34 +517,38 @@ public class RuleExpressionVO implements Serializable {
   }
 
   public boolean isDataTypeCompatible(DataType dataType) {
-    switch (dataType) {
-      // DataType: Boolean
-      case BOOLEAN:
-        return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("Boolean", this));
-      // DataType: String
-      case TEXT:
-      case LONG_TEXT:
-      case CODELIST:
-      case LINK:
-      case LINK_DATA:
-      case URL:
-      case PHONE:
-      case EMAIL:
-        return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("String", this));
-      // DataType: Number
-      case NUMBER_INTEGER:
-      case NUMBER_DECIMAL:
-      case COORDINATE_LAT:
-      case COORDINATE_LONG:
-        return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("Number", this));
-      // DataType: Date
-      case DATE:
-        return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("Date", this));
-      // DataType: Unsupported
-      default:
-        LOG_ERROR.error("Unsupported DataType: {}", dataType);
-        return false;
-    }
+
+    // TODO.
+    return true;
+
+    // switch (dataType) {
+    // // DataType: Boolean
+    // case BOOLEAN:
+    // return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("Boolean", this));
+    // // DataType: String
+    // case TEXT:
+    // case LONG_TEXT:
+    // case CODELIST:
+    // case LINK:
+    // case LINK_DATA:
+    // case URL:
+    // case PHONE:
+    // case EMAIL:
+    // return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("String", this));
+    // // DataType: Number
+    // case NUMBER_INTEGER:
+    // case NUMBER_DECIMAL:
+    // case COORDINATE_LAT:
+    // case COORDINATE_LONG:
+    // return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("Number", this));
+    // // DataType: Date
+    // case DATE:
+    // return Boolean.TRUE.equals(isDataTypeCompatibleRecursive("Date", this));
+    // // DataType: Unsupported
+    // default:
+    // LOG_ERROR.error("Unsupported DataType: {}", dataType);
+    // return false;
+    // }
   }
 
   private Boolean isDataTypeCompatibleRecursive(String type, RuleExpressionVO ruleExpressionVO) {
