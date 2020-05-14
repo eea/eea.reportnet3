bfe339fc14fae37c8c9f375819fc2cc55977cd02
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-05-14 14:16:03 +0200

    fix Bug on unnecesary page reload
    Signed-off-by: Igor Cojocaru <icojocaru-ext@tracasa.es>

--- frontend-service/src/ui/views/Dataflow/Dataflow.jsx
+++ frontend-service/src/ui/views/Dataflow/Dataflow.jsx
@@ -234,7 +234,7 @@ const Dataflow = withRouter(({ history, match }) => {
     setIsPageLoading(true);
     onLoadReportingDataflow();
     onLoadSchemasValidations();
-  }, [dataflowId, dataflowState.isDataUpdated, representativeId]);
+  }, [dataflowId, dataflowState.isDataUpdated]);
 
   const handleRedirect = target => history.push(target);
 
