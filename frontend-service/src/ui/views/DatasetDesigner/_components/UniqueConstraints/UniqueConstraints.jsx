aa149751e2873aef9508a819d7f4da9884ddaaa7
Pablo Arrastia <parrastia-ext@tracasa.es>
2020-05-21 14:41:08 +0200

    fix unique constraint filters bug

--- frontend-service/src/ui/views/DatasetDesigner/_components/UniqueConstraints/UniqueConstraints.jsx
+++ frontend-service/src/ui/views/DatasetDesigner/_components/UniqueConstraints/UniqueConstraints.jsx
@@ -103,7 +103,7 @@ export const UniqueConstraints = ({ datasetSchemaId, tableData }) => {
 
   const renderColumns = constraints => {
     const fieldColumns = Object.keys(constraints[0])
-      .filter(key => !key.includes('Id'))
+      .filter(key => !key.includes('Id') && !key.includes('filter'))
       .map(field => (
         <Column
           body={field === 'fieldData' ? renderFieldBody : null}
@@ -126,7 +126,12 @@ export const UniqueConstraints = ({ datasetSchemaId, tableData }) => {
     <Fragment>{resources.messages['noConstraints']}</Fragment>
   ) : (
     <Fragment>
-      <Filters data={constraintsState.data} getFilteredData={onLoadFilteredData} selectOptions={['tableSchemaName']} />
+      <Filters
+        data={constraintsState.data}
+        getFilteredData={onLoadFilteredData}
+        inputOptions={['filterFieldsNames']}
+        selectOptions={['tableSchemaName']}
+      />
 
       {!isEmpty(constraintsState.filteredData) ? (
         <DataTable
