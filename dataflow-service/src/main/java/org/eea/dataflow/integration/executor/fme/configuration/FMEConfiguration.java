50547d598163e63444eb33b140ab7d8e509b2c93
msevera <mario.severa@altia.es>
2020-06-09 11:54:46 +0200

    Merge remote-tracking branch 'origin/sandbox' into REP-706-us-stp6-dsm-dsc-tab-07-import-data

--- dataflow-service/src/main/java/org/eea/dataflow/integration/executor/fme/configuration/FMEConfiguration.java
+++ dataflow-service/src/main/java/org/eea/dataflow/integration/executor/fme/configuration/FMEConfiguration.java
@@ -1,5 +1,6 @@
 package org.eea.dataflow.integration.executor.fme.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +10,15 @@ import feign.auth.BasicAuthRequestInterceptor;
 @EnableFeignClients("org.eea.dataflow.integration.fme.repository")
 public class FMEConfiguration {
 
+  @Value("${integration.fme.user}")
+  private String fmeUser;
+
+  @Value("${integration.fme.password}")
+  private String fmePassword;
+
   @Bean
   public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-    return new BasicAuthRequestInterceptor("Reportnet3", "Reportnet3_2020!");
+    return new BasicAuthRequestInterceptor(fmeUser, fmePassword);
   }
 
 }
--- dataflow-service/src/main/java/org/eea/dataflow/integration/executor/fme/repository/FMEFeignRepository.java
+++ dataflow-service/src/main/java/org/eea/dataflow/integration/executor/fme/repository/FMEFeignRepository.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "FMEInterface", url = "https://fme.discomap.eea.europa.eu/fmerest/v3")
+@FeignClient(name = "FMEInterface", url = "integration.fme.url")
 public interface FMEFeignRepository {
 
-  // Repository: ReportNetTesting
+
   @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
   FMEStatus find();
 
