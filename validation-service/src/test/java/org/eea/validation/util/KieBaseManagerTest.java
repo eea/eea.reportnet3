7818ca5ab27631de8121aa2c2323fdfe6d35550a
Bea <beatrizsevilla@altia.es>
2020-06-16 13:52:07 +0200

    ADD junits validation utils

---
+++ validation-service/src/test/java/org/eea/validation/util/KieBaseManagerTest.java
@@ -0,0 +1,348 @@
+package org.eea.validation.util;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.times;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.eea.exception.EEAException;
+import org.eea.interfaces.controller.dataset.DatasetMetabaseController;
+import org.eea.interfaces.vo.dataset.DataSetMetabaseVO;
+import org.eea.interfaces.vo.dataset.enums.DataType;
+import org.eea.interfaces.vo.dataset.enums.EntityTypeEnum;
+import org.eea.kafka.utils.KafkaSenderUtils;
+import org.eea.validation.persistence.repository.RulesRepository;
+import org.eea.validation.persistence.repository.SchemasRepository;
+import org.eea.validation.persistence.schemas.DataSetSchema;
+import org.eea.validation.persistence.schemas.FieldSchema;
+import org.eea.validation.persistence.schemas.RecordSchema;
+import org.eea.validation.persistence.schemas.ReferencedFieldSchema;
+import org.eea.validation.persistence.schemas.TableSchema;
+import org.eea.validation.persistence.schemas.rule.Rule;
+import org.eea.validation.persistence.schemas.rule.RulesSchema;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class KieBaseManagerTest {
+
+  @InjectMocks
+  private KieBaseManager kieBaseManager;
+
+  @Mock
+  private RulesRepository rulesRepository;
+
+  @Mock
+  private SchemasRepository schemasRepository;
+
+  @Mock
+  private DatasetMetabaseController datasetMetabaseController;
+
+  @Mock
+  private KafkaSenderUtils kafkaSenderUtils;
+
+  private DataSetSchema datasetSchema;
+  private ObjectId id;
+  private FieldSchema fieldSchema;
+
+  @Before
+  public void initMocks() {
+    MockitoAnnotations.initMocks(this);
+    id = new ObjectId();
+    datasetSchema = new DataSetSchema();
+    List<TableSchema> tableSchemas = new ArrayList<>();
+    List<FieldSchema> fieldSchemas = new ArrayList<>();
+    TableSchema tableSchema = new TableSchema();
+    RecordSchema recordSchema = new RecordSchema();
+    fieldSchema = new FieldSchema();
+    ReferencedFieldSchema referencedFieldSchema = new ReferencedFieldSchema();
+    referencedFieldSchema.setIdPk(id);
+    fieldSchema.setIdFieldSchema(id);
+    fieldSchema.setReferencedField(referencedFieldSchema);
+    fieldSchema.setPkMustBeUsed(Boolean.TRUE);
+    fieldSchemas.add(fieldSchema);
+    recordSchema.setFieldSchema(fieldSchemas);
+    recordSchema.setIdRecordSchema(id);
+    tableSchema.setRecordSchema(recordSchema);
+    tableSchema.setIdTableSchema(id);
+    tableSchemas.add(tableSchema);
+    datasetSchema.setTableSchemas(tableSchemas);
+  }
+
+  @Test
+  public void testReloadRulesDataset() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.DATASET);
+    rule.setReferenceId(new ObjectId());
+    rule.setRuleId(new ObjectId());
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    schemaRules.setRules(rules);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(datasetMetabaseController.findDatasetMetabaseById(Mockito.any()))
+        .thenReturn(new DataSetMetabaseVO());
+    assertNotNull(kieBaseManager.reloadRules(1L, new ObjectId().toString()));
+  }
+
+  @Test
+  public void testReloadRulesTable() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.TABLE);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    schemaRules.setRules(rules);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesRecord() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.RECORD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    schemaRules.setRules(rules);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesFieldInteger() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    schemaRules.setRules(rules);
+    fieldSchema.setType(DataType.NUMBER_INTEGER);
+    Document fieldDocument = new Document();
+    fieldDocument.put("typeData", DataType.NUMBER_INTEGER);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(fieldDocument);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesFieldDecimal() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    schemaRules.setRules(rules);
+    fieldSchema.setType(DataType.NUMBER_DECIMAL);
+    Document fieldDocument = new Document();
+    fieldDocument.put("typeData", DataType.NUMBER_DECIMAL);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(fieldDocument);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesFieldDate() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    schemaRules.setRules(rules);
+    fieldSchema.setType(DataType.DATE);
+    Document fieldDocument = new Document();
+    fieldDocument.put("typeData", DataType.DATE);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(fieldDocument);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesFieldBoolean() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    schemaRules.setRules(rules);
+    fieldSchema.setType(DataType.BOOLEAN);
+    Document fieldDocument = new Document();
+    fieldDocument.put("typeData", DataType.BOOLEAN);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(fieldDocument);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesFieldCoordinateLat() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    schemaRules.setRules(rules);
+    fieldSchema.setType(DataType.COORDINATE_LAT);
+    Document fieldDocument = new Document();
+    fieldDocument.put("typeData", DataType.COORDINATE_LAT);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(fieldDocument);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+  @Test
+  public void testReloadRulesFieldCoordinateLong() throws FileNotFoundException {
+    RulesSchema schemaRules = new RulesSchema();
+    List<Rule> rules = new ArrayList<>();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    Rule rule = new Rule();
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    rules.add(rule);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    schemaRules.setRules(rules);
+    fieldSchema.setType(DataType.COORDINATE_LONG);
+    Document fieldDocument = new Document();
+    fieldDocument.put("typeData", DataType.COORDINATE_LONG);
+    Mockito.when(rulesRepository.getActiveAndVerifiedRules(Mockito.any())).thenReturn(schemaRules);
+    Mockito.when(schemasRepository.findByIdDataSetSchema(Mockito.any())).thenReturn(datasetSchema);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(fieldDocument);
+    assertNotNull(kieBaseManager.reloadRules(1L, id.toString()));
+  }
+
+
+
+  @Test
+  public void testTextRuleCorrectTable() throws EEAException {
+    Rule rule = new Rule();
+    List<String> thenCondition = new ArrayList<>();
+    thenCondition.add("");
+    thenCondition.add("");
+    rule.setType(EntityTypeEnum.TABLE);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    kieBaseManager.textRuleCorrect(id.toString(), rule);
+    Mockito.verify(kafkaSenderUtils, times(1)).releaseNotificableKafkaEvent(Mockito.any(),
+        Mockito.any(), Mockito.any());
+  }
+
+  @Test
+  public void testTextRuleCorrectRecord() throws EEAException {
+    Rule rule = new Rule();
+    List<String> thenCondition = new ArrayList<>();
+    List<Document> fields = new ArrayList<>();
+    Document document = new Document();
+    Document field = new Document();
+    field.put("_id", new ObjectId());
+    field.put("typeData", DataType.NUMBER_INTEGER);
+    fields.add(field);
+    document.put("fieldSchemas", fields);
+    thenCondition.add("");
+    thenCondition.add("");
+    rule.setType(EntityTypeEnum.RECORD);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setThenCondition(thenCondition);
+    Mockito.when(schemasRepository.findRecordSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(document);
+    kieBaseManager.textRuleCorrect(id.toString(), rule);
+    Mockito.verify(kafkaSenderUtils, times(1)).releaseNotificableKafkaEvent(Mockito.any(),
+        Mockito.any(), Mockito.any());
+  }
+
+  @Test
+  public void testTextRuleCorrectField() throws EEAException {
+    Rule rule = new Rule();
+    List<String> thenCondition = new ArrayList<>();
+    Document document = new Document();
+    document.put("typeData", DataType.NUMBER_DECIMAL);
+    thenCondition.add("");
+    thenCondition.add("");
+    rule.setType(EntityTypeEnum.FIELD);
+    rule.setReferenceId(id);
+    rule.setRuleId(id);
+    rule.setWhenCondition("this.fieldNumberEquals(\"5\", 5)");
+    rule.setThenCondition(thenCondition);
+    Mockito.when(schemasRepository.findFieldSchema(Mockito.any(), Mockito.any()))
+        .thenReturn(document);
+    kieBaseManager.textRuleCorrect(id.toString(), rule);
+    Mockito.verify(kafkaSenderUtils, times(1)).releaseNotificableKafkaEvent(Mockito.any(),
+        Mockito.any(), Mockito.any());
+  }
+
+
+}
