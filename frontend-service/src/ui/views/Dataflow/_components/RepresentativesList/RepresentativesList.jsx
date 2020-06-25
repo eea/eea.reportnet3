e8e017432d4b08c489f81696403f73ebc225b41e
Igor Cojocaru <icojocaru-ext@tracasa.es>
2020-06-25 15:07:43 +0200

    fix Max-width on dropdown
    Signed-off-by: Igor Cojocaru <icojocaru-ext@tracasa.es>

--- frontend-service/src/ui/views/Dataflow/_components/RepresentativesList/RepresentativesList.jsx
+++ frontend-service/src/ui/views/Dataflow/_components/RepresentativesList/RepresentativesList.jsx
@@ -205,13 +205,12 @@ const RepresentativesList = ({
           <Dropdown
             disabled={formState.representatives.length > 1}
             name="dataProvidersDropdown"
+            onChange={event => formDispatcher({ type: 'SELECT_PROVIDERS_TYPE', payload: event.target.value })}
             optionLabel="label"
-            placeholder={resources.messages['manageRolesDialogDropdownPlaceholder']}
-            value={formState.selectedDataProviderGroup}
             options={formState.dataProvidersTypesList}
-            onChange={e => {
-              return formDispatcher({ type: 'SELECT_PROVIDERS_TYPE', payload: e.target.value });
-            }}
+            placeholder={resources.messages['manageRolesDialogDropdownPlaceholder']}
+            style={{ maxWidth: '16rem' }}
+            value={formState.selectedDataProviderGroup}
           />
         </div>
       </div>
