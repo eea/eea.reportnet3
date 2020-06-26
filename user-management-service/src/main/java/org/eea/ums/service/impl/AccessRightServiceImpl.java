c28bccf966492a2850d09850078ec595fcdc2fc2
Eduardo Ayala Collado <eduardo.ayala@altia.es>
2020-06-26 13:58:19 +0200

    [ADD]
    add a part of editor
    
    Eduardo Ayala Collado
    Altia Alicante

--- user-management-service/src/main/java/org/eea/ums/service/impl/AccessRightServiceImpl.java
+++ user-management-service/src/main/java/org/eea/ums/service/impl/AccessRightServiceImpl.java
@@ -1,8 +1,15 @@
 package org.eea.ums.service.impl;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import org.eea.interfaces.controller.dataflow.DataFlowController.DataFlowControllerZuul;
+import org.eea.interfaces.vo.dataflow.DataFlowVO;
 import org.eea.interfaces.vo.dataflow.RepresentativeVO;
+import org.eea.interfaces.vo.dataflow.enums.TypeStatusEnum;
+import org.eea.interfaces.vo.dataset.DesignDatasetVO;
+import org.eea.interfaces.vo.ums.ResourceAssignationVO;
+import org.eea.interfaces.vo.ums.enums.ResourceGroupEnum;
 import org.eea.ums.service.AccessRightService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,9 +36,47 @@ public class AccessRightServiceImpl implements AccessRightService {
 
   @Override
   public void deleteRoleUser(RepresentativeVO representativeVO, Long dataflowId) {
-    dataflowControlleZuul.findById(dataflowId);
+    DataFlowVO dataflow = dataflowControlleZuul.findById(dataflowId);
+
+    ResourceGroupEnum resourceGroupEnum = null;
+
+    switch (representativeVO.getRole()) {
+      case "EDITOR":
+        resourceGroupEnum = Boolean.TRUE.equals(representativeVO.getPermission())
+            ? resourceGroupEnum.DATASCHEMA_EDITOR_WRITE
+            : resourceGroupEnum.DATASCHEMA_EDITOR_READ;
+        break;
+      case "REPORTER_PARTITIONED":
+        break;
+      case "REPORTER":
+        /*
+         * resourceGroupEnum = Boolean.TRUE.equals(representativeVO.getPermission()) ?
+         * resourceGroupEnum.datas : resourceGroupEnum.DATASCHEMA_EDITOR_READ;
+         */
+        break;
+    }
+
+    if (TypeStatusEnum.DESIGN.equals(dataflow.getStatus())) {
+      for (DesignDatasetVO designDatasetVO : dataflow.getDesignDatasets()) {
+        // quitar resource
+        List<ResourceAssignationVO> resourcesProviders = new ArrayList<>();
+        ResourceAssignationVO resourceDP = fillResourceAssignation(designDatasetVO.getId(),
+            representativeVO.getAccount(), resourceGroupEnum);
+        resourcesProviders.add(resourceDP);
+      }
+      // enviar a bea resourcesProviders;
+    }
   }
 
+  private ResourceAssignationVO fillResourceAssignation(Long id, String email,
+      ResourceGroupEnum group) {
 
+    ResourceAssignationVO resource = new ResourceAssignationVO();
+    resource.setResourceId(id);
+    resource.setEmail(email);
+    resource.setResourceGroup(group);
+
+    return resource;
+  }
 
 }
