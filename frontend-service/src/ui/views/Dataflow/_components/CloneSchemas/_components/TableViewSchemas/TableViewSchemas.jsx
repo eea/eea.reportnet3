d3103a0d93c5e7c177f9a4388552711db44588ab
Miriam Gaviria <mgaviria-ext@tracasa.es>
2020-06-26 12:29:32 +0200

    Delete - history and withRouter

--- frontend-service/src/ui/views/Dataflow/_components/CloneSchemas/_components/TableViewSchemas/TableViewSchemas.jsx
+++ frontend-service/src/ui/views/Dataflow/_components/CloneSchemas/_components/TableViewSchemas/TableViewSchemas.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { withRouter } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import isEmpty from 'lodash/isEmpty';
@@ -17,108 +16,103 @@ import { DataTable } from 'ui/views/_components/DataTable';
 
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
 
-export const TableViewSchemas = withRouter(
-  ({ data, history, onChangePagination, onSelectDataflow, pagination, selectedDataflowId }) => {
-    const resources = useContext(ResourcesContext);
+export const TableViewSchemas = ({ data, onChangePagination, onSelectDataflow, pagination, selectedDataflowId }) => {
+  const resources = useContext(ResourcesContext);
 
-    const headerTableTemplate = field => {
-      if (field === 'expirationDate') return resources.messages['nextReportDue'];
-      else if (field === 'obligation') return resources.messages['obligationTitle'];
-      else if (field === 'legalInstruments') return resources.messages['legalInstrument'];
-      else return resources.messages[field];
-    };
+  const fieldTables = {
+    expirationDate: resources.messages['nextReportDue'],
+    obligation: resources.messages['obligationTitle'],
+    legalInstruments: resources.messages['legalInstrument']
+  };
 
-    const onLoadCheckButton = row => {
-      return (
-        <div className={styles.checkColum}>
-          <Checkbox
-            id={`${row.id}_checkbox`}
-            isChecked={selectedDataflowId === row.id}
-            onChange={() => onSelectDataflow(row)}
-            role="checkbox"
-          />
-        </div>
-      );
-    };
+  const headerTableTemplate = field => fieldTables[field] || resources.messages[field];
 
-    const onLoadLegalInstrumentTemplate = row => (
-      <div className={styles.titleColum}>{row.obligation.legalInstruments.alias}</div>
-    );
-
-    const onLoadObligationTemplate = row => <div className={styles.titleColum}>{row.obligation.title}</div>;
-
-    const onLoadPagination = event => onChangePagination({ first: event.first, rows: event.rows, page: event.page });
-
-    const onLoadTitleTemplate = row => (
-      <div className={styles.titleColum}>
-        {row.name}
-        <FontAwesomeIcon
-          className={styles.linkIcon}
-          icon={AwesomeIcons('externalLink')}
-          onMouseDown={() => window.open(getUrl(routes.DATAFLOW, { dataflowId: row.id }, true))}
+  const onLoadCheckButton = row => {
+    return (
+      <div className={styles.checkColum}>
+        <Checkbox
+          id={`${row.id}_checkbox`}
+          isChecked={selectedDataflowId === row.id}
+          onChange={() => onSelectDataflow(row)}
+          role="checkbox"
         />
       </div>
     );
+  };
 
-    const renderCheckColum = <Column key="checkId" body={row => onLoadCheckButton(row)} />;
+  const onLoadLegalInstrumentTemplate = row => (
+    <div className={styles.titleColum}>{row.obligation.legalInstruments.alias}</div>
+  );
 
-    const renderColumns = dataflows => {
-      const fieldColumns = Object.keys(dataflows[0])
-        .filter(
-          key =>
-            key.includes('name') ||
-            key.includes('expirationDate') ||
-            key.includes('description') ||
-            key.includes('status') ||
-            key.includes('userRole') ||
-            key.includes('obligation')
-        )
-        .map(field => {
-          let template = null;
-          if (field === 'name') template = onLoadTitleTemplate;
-          if (field === 'obligation') template = onLoadObligationTemplate;
+  const onLoadObligationTemplate = row => <div className={styles.titleColum}>{row.obligation.title}</div>;
 
-          return (
-            <Column body={template} field={field} header={headerTableTemplate(field)} key={field} sortable={true} />
-          );
-        });
+  const onLoadPagination = event => onChangePagination({ first: event.first, rows: event.rows, page: event.page });
 
-      const legalFieldColumn = Object.values(dataflows[0]).filter(key => typeof key === 'object');
+  const onLoadTitleTemplate = row => (
+    <div className={styles.titleColum}>
+      {row.name}
+      <FontAwesomeIcon
+        className={styles.linkIcon}
+        icon={AwesomeIcons('externalLink')}
+        onMouseDown={() => window.open(getUrl(routes.DATAFLOW, { dataflowId: row.id }, true))}
+      />
+    </div>
+  );
 
-      const legalInstrument = Object.keys(legalFieldColumn[0])
-        .filter(key => key.includes('legalInstruments'))
-        .map(field => {
-          let template = null;
-          if (field === 'legalInstruments') template = onLoadLegalInstrumentTemplate;
+  const renderCheckColum = <Column key="checkId" body={row => onLoadCheckButton(row)} />;
 
-          return (
-            <Column body={template} field={field} header={headerTableTemplate(field)} key={field} sortable={true} />
-          );
-        });
+  const renderColumns = dataflows => {
+    const fieldColumns = Object.keys(dataflows[0])
+      .filter(
+        key =>
+          key.includes('name') ||
+          key.includes('expirationDate') ||
+          key.includes('description') ||
+          key.includes('status') ||
+          key.includes('userRole') ||
+          key.includes('obligation')
+      )
+      .map(field => {
+        let template = null;
+        if (field === 'name') template = onLoadTitleTemplate;
+        if (field === 'obligation') template = onLoadObligationTemplate;
 
-      fieldColumns.push(legalInstrument);
-      fieldColumns.unshift(renderCheckColum);
+        return <Column body={template} field={field} header={headerTableTemplate(field)} key={field} sortable={true} />;
+      });
 
-      return fieldColumns;
-    };
+    const legalFieldColumn = Object.values(dataflows[0]).filter(key => typeof key === 'object');
 
-    const paginatorRightText = `${resources.messages['totalDataflows']}: ${data.length}`;
+    const legalInstrument = Object.keys(legalFieldColumn[0])
+      .filter(key => key.includes('legalInstruments'))
+      .map(field => {
+        let template = null;
+        if (field === 'legalInstruments') template = onLoadLegalInstrumentTemplate;
 
-    return isEmpty(data) ? (
-      <h3 className={styles.noObligations}>{resources.messages['noObligationsWithSelectedParameters']}</h3>
-    ) : (
-      <DataTable
-        autoLayout={true}
-        first={pagination.first}
-        getPageChange={onLoadPagination}
-        paginator={true}
-        paginatorRight={paginatorRightText}
-        rows={pagination.rows}
-        rowsPerPageOptions={[5, 10, 15]}
-        totalRecords={data.length}
-        value={data}>
-        {renderColumns(data)}
-      </DataTable>
-    );
-  }
-);
+        return <Column body={template} field={field} header={headerTableTemplate(field)} key={field} sortable={true} />;
+      });
+
+    fieldColumns.push(legalInstrument);
+    fieldColumns.unshift(renderCheckColum);
+
+    return fieldColumns;
+  };
+
+  const paginatorRightText = `${resources.messages['totalDataflows']}: ${data.length}`;
+
+  return isEmpty(data) ? (
+    <h3 className={styles.noObligations}>{resources.messages['noObligationsWithSelectedParameters']}</h3>
+  ) : (
+    <DataTable
+      autoLayout={true}
+      first={pagination.first}
+      getPageChange={onLoadPagination}
+      paginator={true}
+      paginatorRight={paginatorRightText}
+      rows={pagination.rows}
+      rowsPerPageOptions={[5, 10, 15]}
+      totalRecords={data.length}
+      value={data}>
+      {renderColumns(data)}
+    </DataTable>
+  );
+};
