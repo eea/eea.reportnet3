09ca263e80ae7b578918c919d67983225d0c01e4
Miguel Azanza <mazanza@tracasa.es>
2020-07-01 14:39:29 +0200

    fix export Delete

--- frontend-service/src/core/services/Contributor/Delete.js
+++ frontend-service/src/core/services/Contributor/Delete.js
@@ -1,2 +1,2 @@
-export const DeleteContributor = ({ contributorRepository }) => async (account, dataflowId, dataProviderId) =>
+export const Delete = ({ contributorRepository }) => async (account, dataflowId, dataProviderId) =>
   contributorRepository.delete(account, dataflowId, dataProviderId);
