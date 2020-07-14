aff96db34b26398e84b4a7a5631b4c98824ee128
msevera <mario.severa@altia.es>
2020-07-14 09:35:45 +0200

    fix Controller and add consul KV

--- common-interfaces/src/main/java/org/eea/interfaces/controller/dataflow/integration/fme/FMEController.java
+++ common-interfaces/src/main/java/org/eea/interfaces/controller/dataflow/integration/fme/FMEController.java
@@ -1,6 +1,7 @@
 package org.eea.interfaces.controller.dataflow.integration.fme;
 
 import org.eea.interfaces.vo.integration.fme.FMECollectionVO;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
  * The Interface FMEController.
  */
 public interface FMEController {
+
+
+  /**
+   * The Interface FMEControllerZuul.
+   */
+  @FeignClient(value = "dataflow", contextId = "fme", path = "/fme")
+  interface FMEControllerZuul extends FMEController {
+
+  }
 
   /**
    * Find repositories.
@@ -22,6 +32,7 @@ public interface FMEController {
   /**
    * Find items.
    *
+   * @param repository the repository
    * @return the collection VO
    */
   @GetMapping(value = "/findItems", produces = MediaType.APPLICATION_JSON_VALUE)
