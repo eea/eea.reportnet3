6017bbe8f5b5dc784d1db05de53d3e4d795b516b
vicente.cano <vicente.cano@altia.es>
2020-07-23 09:56:45 +0200

    FIX notification

--- dataflow-service/src/main/java/org/eea/dataflow/io/notification/events/ExternalExportEUDatasetCompletedEvent.java
+++ dataflow-service/src/main/java/org/eea/dataflow/io/notification/events/ExternalExportEUDatasetCompletedEvent.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.eea.dataflow.service.DataflowService;
 import org.eea.exception.EEAException;
+import org.eea.interfaces.controller.dataset.DatasetMetabaseController.DataSetMetabaseControllerZuul;
 import org.eea.kafka.domain.EventType;
 import org.eea.kafka.domain.NotificationVO;
 import org.eea.notification.event.NotificableEventHandler;
@@ -20,6 +21,10 @@ public class ExternalExportEUDatasetCompletedEvent implements NotificableEventHa
   /** The dataflow service. */
   @Autowired
   private DataflowService dataflowService;
+
+  /** The dataset metabase controller zuul. */
+  @Autowired
+  private DataSetMetabaseControllerZuul datasetMetabaseControllerZuul;
 
 
   /**
@@ -43,13 +48,18 @@ public class ExternalExportEUDatasetCompletedEvent implements NotificableEventHa
   public Map<String, Object> getMap(NotificationVO notificationVO) throws EEAException {
 
     Long dataflowId = notificationVO.getDataflowId();
+    Long datasetId = notificationVO.getDatasetId();
     String dataflowName = dataflowService.getById(dataflowId).getName();
-
+    String datasetName = "";
+    if (datasetId != null) {
+      datasetName =
+          datasetMetabaseControllerZuul.findDatasetMetabaseById(datasetId).getDataSetName();
+    }
     Map<String, Object> notification = new HashMap<>();
     notification.put("user", notificationVO.getUser());
     notification.put("dataflowId", dataflowId);
     notification.put("datasetId", notificationVO.getDatasetId());
-    notification.put("datasetName", notificationVO.getDatasetName());
+    notification.put("datasetName", datasetName);
     notification.put("dataflowName", dataflowName);
     notification.put("fileName", notificationVO.getFileName());
 
