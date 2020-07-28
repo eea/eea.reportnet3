c0fcdac1c358ec61262eb8729872ab42aad80519
Bea <beatrizsevilla@altia.es>
2020-07-28 14:02:18 +0200

    FIX SONNAR ISSUES

--- common-interfaces/src/main/java/org/eea/interfaces/vo/dataflow/enums/FMEItemEnum.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/dataflow/enums/FMEItemEnum.java
@@ -1,16 +1,24 @@
 package org.eea.interfaces.vo.dataflow.enums;
 
+/**
+ * The Enum FMEItemEnum.
+ */
 public enum FMEItemEnum {
 
 
+  /** The unknown. */
   UNKNOWN("UNKNOWN"),
 
+  /** The workspace. */
   WORKSPACE("WORKSPACE"),
 
+  /** The customformat. */
   CUSTOMFORMAT("CUSTOMFORMAT"),
 
+  /** The customtransform. */
   CUSTOMTRANSFORM("CUSTOMTRANSFORM"),
 
+  /** The template. */
   TEMPLATE("TEMPLATE");
 
   /** The value. */
--- common-utitlities/src/main/java/org/eea/kafka/io/KafkaReceiver.java
+++ common-utitlities/src/main/java/org/eea/kafka/io/KafkaReceiver.java
@@ -24,6 +24,7 @@ public abstract class KafkaReceiver {
    * Listen message.
    *
    * @param message the message
+   * @throws EEAException the EEA exception
    */
 
   public abstract void listenMessage(final Message<EEAEventVO> message) throws EEAException;
