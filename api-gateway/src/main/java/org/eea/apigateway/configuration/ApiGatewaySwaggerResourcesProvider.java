d2cb229d174fa84c84194af65bfddaa285a0dd19
jlanton <ext.jose.luis.anton@altia.es>
2020-08-11 12:42:27 +0200

    ADDED Api-Gateway configuration for swagger

---
+++ api-gateway/src/main/java/org/eea/apigateway/configuration/ApiGatewaySwaggerResourcesProvider.java
@@ -0,0 +1,96 @@
+package org.eea.apigateway.configuration;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+/**
+ * The type Api gateway  swagger resources provider.
+ */
+@Component
+@Primary
+public class ApiGatewaySwaggerResourcesProvider implements SwaggerResourcesProvider {
+
+
+  @Autowired
+  private RouteLocator routeLocator;
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  @Value("${spring.profiles.active:local}")
+  private String activeProfile;
+
+  @Override
+  public List<SwaggerResource> get() {
+    List<SwaggerResource> resources = new ArrayList<>();
+
+    //Add the default swagger resource that correspond to the gateway's own swagger doc
+    resources.add(swaggerResource("default", "/v2/api-docs"));
+
+    //Add the registered microservices swagger docs as additional swagger resources. Get all the routes registered in the ApiGateway configuration
+    List<Route> routes = routeLocator.getRoutes();
+    //Given that it is possible that a microservice has several Rest interfaces exposed under the same endpoint it's necessary to avoid duplicated configurations
+    Set<String> registeredResources = new HashSet<>();
+    routes.forEach(route -> {
+      //if the microservice (route) has not been already added to the Swagger resources
+      if (!registeredResources.contains(route.getLocation())) {
+        //Get information from consul for that service: Schema, Port, Ip...
+        Optional<URI> uri = getServiceUrl(route.getLocation());
+        if (uri
+            .isPresent()) {//it could happen that there is a route without a deployed microservice, this could happen for brand new microservices
+
+          //finally adding the Swagger Resource as pair "Name","Origin of JSON Swagger Rest Document".
+          // In this case, the microservices rest api swagger documentation will come from the rest of the microservices
+          URI location = uri.get();
+          StringBuilder endpoint = new StringBuilder(location.getScheme()).append("://")
+              .append(getServiceHost(route.getLocation(), location))
+              .append("/v2/api-docs");
+          resources
+              .add(swaggerResource(route.getLocation(),
+                  endpoint.toString()));
+          registeredResources.add(route.getLocation());
+        }
+      }
+    });
+
+    return resources;
+  }
+
+
+  private String getServiceHost(String service, URI uri) {
+
+    //if no profile has been set it is assume that it's running in local, otherwise it is assume it is running in kubernetes and it will be used its name translation (k8s dns)
+    return "local".equals(activeProfile) ? "localhost:" + uri.getPort() : service;
+  }
+
+
+  private Optional<URI> getServiceUrl(String service) {
+    Optional<URI> uri = discoveryClient.getInstances(service)
+        .stream()
+        .findFirst()
+        .map(si -> si.getUri());
+
+    return uri;
+  }
+
+  private SwaggerResource swaggerResource(String name, String location) {
+    SwaggerResource swaggerResource = new SwaggerResource();
+    swaggerResource.setName(name);
+    swaggerResource.setLocation(location);
+    swaggerResource.setSwaggerVersion("2.0");
+    return swaggerResource;
+  }
+}
+
