15614d1740833007ecf5837efa5f9e2ce1157272
igor-co <60940858+igor-co@users.noreply.github.com>
2020-08-18 13:04:49 +0200

    add Notification on start

--- frontend-service/src/ui/views/Dataflow/_components/SnapshotsDialog/SnapshotsDialog.jsx
+++ frontend-service/src/ui/views/Dataflow/_components/SnapshotsDialog/SnapshotsDialog.jsx
@@ -118,6 +118,7 @@ export const SnapshotsDialog = ({ dataflowId, datasetId, datasetName, isSnapshot
         }}
         style={{ width: '30vw' }}
         visible={isSnapshotDialogVisible}>
+          
         <li className={styles.createAndReleaseItem}>
           <div className={styles.itemInner}>
             <div className={styles.itemData}>
