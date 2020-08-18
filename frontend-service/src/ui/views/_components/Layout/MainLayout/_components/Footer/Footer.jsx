7dbbc2a9c52d8a2a3511e42b0eaff1772795d707
azpilm <mazpilicueta-ext@tracasa.es>
2020-08-18 14:59:54 +0200

    update footer to add a blueBox in the left side

--- frontend-service/src/ui/views/_components/Layout/MainLayout/_components/Footer/Footer.jsx
+++ frontend-service/src/ui/views/_components/Layout/MainLayout/_components/Footer/Footer.jsx
@@ -7,38 +7,44 @@ import { routes } from 'ui/routes';
 
 import eeaLogo from 'assets/images/pdflogo-web.png';
 import logo from 'assets/images/logo.png';
+
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
+import { LeftSideBarContext } from 'ui/views/_functions/Contexts/LeftSideBarContext';
 
 import { getUrl } from 'core/infrastructure/CoreUtils';
 
 export const Footer = withRouter(({ history, leftMargin }) => {
   const resources = useContext(ResourcesContext);
+  const leftSideBarContext = useContext(LeftSideBarContext);
+
   return (
     <Fragment>
       <div className={styles.Footer}>
-        <div className="rep-container">
-          <div className={styles.footerContent}>
-            <div className={styles.reportnetLogo}>
-              <a href="https://www.eea.europa.eu/" className={styles.title} title={resources.messages['eea']}>
-                <img height="50px" src={eeaLogo} alt={resources.messages['eea']} className={styles.appLogo} />
-              </a>
-            </div>
-            <div className={styles.reportnetLogo}>
-              <a
-                href={getUrl(routes.DATAFLOWS)}
-                className={styles.title}
-                title={resources.messages['titleHeader']}
-                onClick={e => {
-                  e.preventDefault();
-                  history.push(getUrl(routes.DATAFLOWS));
-                }}>
-                <img height="50px" src={logo} alt="Reportnet" className={styles.appLogo} />
-                <h1 className={styles.appTitle}>{resources.messages['titleHeader']}</h1>
-              </a>
-            </div>
-            <div className={styles.helpDesk}>
-              <a href="mailto:helpdesk@eionet.europa.eu">helpdesk@eionet.europa.eu</a>
-            </div>
+        <div
+          className={styles.blueBox}
+          style={leftSideBarContext.isLeftSideBarOpened ? { width: '200px' } : { width: '50px' }}></div>
+
+        <div className={styles.footerContent}>
+          <div className={styles.reportnetLogo}>
+            <a href="https://www.eea.europa.eu/" className={styles.title} title={resources.messages['eea']}>
+              <img height="50px" src={eeaLogo} alt={resources.messages['eea']} className={styles.appLogo} />
+            </a>
+          </div>
+          <div className={styles.reportnetLogo}>
+            <a
+              href={getUrl(routes.DATAFLOWS)}
+              className={styles.title}
+              title={resources.messages['titleHeader']}
+              onClick={e => {
+                e.preventDefault();
+                history.push(getUrl(routes.DATAFLOWS));
+              }}>
+              <img height="50px" src={logo} alt="Reportnet" className={styles.appLogo} />
+              <h1 className={styles.appTitle}>{resources.messages['titleHeader']}</h1>
+            </a>
+          </div>
+          <div className={styles.helpDesk}>
+            <a href="mailto:helpdesk@eionet.europa.eu">helpdesk@eionet.europa.eu</a>
           </div>
         </div>
       </div>
