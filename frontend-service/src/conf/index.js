37181e0db5b4b8a5bdb6431e1135a482915852cd
azpilm <mazpilicueta-ext@tracasa.es>
2020-08-25 10:28:49 +0200

    add footer config to conf index

--- frontend-service/src/conf/index.js
+++ frontend-service/src/conf/index.js
@@ -7,6 +7,7 @@ import permissions from './permissions';
 import primeIcons from './prime.icons';
 import validations from './validation.config.json';
 import publicFrontpage from './publicFrontpage.json';
+import footer from './footer.config.json';
 
 const config = {};
 const { images } = avatarImages;
@@ -18,13 +19,14 @@ const { languages } = languageCode;
 config.avatars = images;
 config.countries = countries;
 config.exportTypes = exportTypes;
+config.footer = footer;
 config.icons = icons;
 config.languages = languages;
 config.MAX_FILE_EXTENSION_LENGTH = 10;
 config.MAX_FILE_SIZE = 100000000;
 config.notifications = notifications;
 config.permissions = permissions;
-config.validations = validations;
 config.publicFrontpage = publicFrontpage;
+config.validations = validations;
 
 export { config };
