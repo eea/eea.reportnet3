e8022efdcf1651bd73e615d1e2c505441ecbfbf5
igor-co <60940858+igor-co@users.noreply.github.com>
2020-09-18 13:50:22 +0200

    fix Show SQL sentence editor on edit press

--- frontend-service/src/ui/views/DatasetDesigner/_components/Validations/_functions/reducers/CreateValidationReducer.js
+++ frontend-service/src/ui/views/DatasetDesigner/_components/Validations/_functions/reducers/CreateValidationReducer.js
@@ -1,3 +1,5 @@
+import isNil from 'lodash/isNil';
+
 export const createValidationReducerInitState = {
   candidateRule: {
     active: true,
@@ -333,24 +335,28 @@ export const createValidationReducer = (state, { type, payload }) => {
     case 'POPULATE_CREATE_FORM':
       const rowOptions = {};
 
-      if (payload.expressionsIf && payload.expressionsIf.length > 0) {
-        rowOptions.expressionType = 'ifThenClause';
-        rowOptions.expressionsIf = payload.expressionsIf;
-        rowOptions.allExpressionsIf = payload.allExpressionsIf;
-        rowOptions.expressionsThen = payload.expressionsThen;
-        rowOptions.allExpressionsThen = payload.allExpressionsThen;
-      }
+      if (isNil(payload.sqlSentence)) {
+        if (payload.expressionsIf && payload.expressionsIf.length > 0) {
+          rowOptions.expressionType = 'ifThenClause';
+          rowOptions.expressionsIf = payload.expressionsIf;
+          rowOptions.allExpressionsIf = payload.allExpressionsIf;
+          rowOptions.expressionsThen = payload.expressionsThen;
+          rowOptions.allExpressionsThen = payload.allExpressionsThen;
+        }
 
-      if (payload.entityType === 'DATASET') {   
-        rowOptions.expressionType = 'fieldRelations';
-      }
+        if (payload.entityType === 'DATASET') {
+          rowOptions.expressionType = 'fieldRelations';
+        }
 
-      if (payload.entityType === 'RECORD' && payload.expressions.length > 0) {
-        rowOptions.expressionType = 'fieldComparison';
-      }
+        if (payload.entityType === 'RECORD' && payload.expressions.length > 0) {
+          rowOptions.expressionType = 'fieldComparison';
+        }
 
-      if (payload.entityType === 'FIELD' && payload.expressions.length > 0) {
-        rowOptions.expressionType = 'fieldTab';
+        if (payload.entityType === 'FIELD' && payload.expressions.length > 0) {
+          rowOptions.expressionType = 'fieldTab';
+        }
+      } else {
+        rowOptions.expressionType = 'SQLsentence';
       }
 
       return {
