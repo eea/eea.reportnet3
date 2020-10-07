4317734017ad37819bde6567098589c63ec0b47f
Miriam Gaviria <mgaviria-ext@tracasa.es>
2020-10-07 16:19:58 +0200

    Fix - eventlistener to prevent download file

--- frontend-service/public/js/index.js
+++ frontend-service/public/js/index.js
@@ -6,21 +6,3 @@ if (isIE || isEdge) {
   var notice = document.getElementById('browserNotice');
   notice.style.display = 'block';
 }
-
-window.addEventListener(
-  'dragover',
-  function (e) {
-    // e.dataTransfer.effectAllowed = 'none';
-    e.preventDefault();
-    e = e || event;
-  },
-  false
-);
-window.addEventListener(
-  'drop',
-  function (e) {
-    e = e || event;
-    e.preventDefault();
-  },
-  false
-);
