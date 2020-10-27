379bc70395993159e96a9a54ef8db3cb4b9c675e
Bea <beatrizsevilla@altia.es>
2020-10-27 13:46:17 +0100

    update vo

--- dataset-service/src/main/java/org/eea/dataset/persistence/metabase/domain/DataSetMetabase.java
+++ dataset-service/src/main/java/org/eea/dataset/persistence/metabase/domain/DataSetMetabase.java
@@ -9,6 +9,8 @@ import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,6 +19,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import org.eea.interfaces.vo.dataset.enums.DatasetStatusEnum;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -63,7 +66,8 @@ public class DataSetMetabase {
 
   /** The status. */
   @Column(name = "STATUS")
-  private String status;
+  @Enumerated(EnumType.STRING)
+  private DatasetStatusEnum status;
 
   /** The dataset schema. */
   @Column(name = "DATASET_SCHEMA")
