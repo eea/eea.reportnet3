c8a2284ae9bf8bf59f28acd4f96c68acbeb05bc7
Arrastia <arrastia@pm.me>
2020-11-02 18:02:27 +0100

    update PaMs webformView functionality

--- frontend-service/src/ui/views/Webforms/_components/Article13/Article13.jsx
+++ frontend-service/src/ui/views/Webforms/_components/Article13/Article13.jsx
@@ -17,15 +17,20 @@ import { ActionsColumn } from 'ui/views/_components/ActionsColumn';
 import { Button } from 'ui/views/_components/Button';
 import { Column } from 'primereact/column';
 import { DataTable } from 'ui/views/_components/DataTable';
-import { TableManagement } from './_components/TableManagement';
 import { InputSwitch } from 'ui/views/_components/InputSwitch';
 import { Spinner } from 'ui/views/_components/Spinner';
+import { TableManagement } from './_components/TableManagement';
 import { Toolbar } from 'ui/views/_components/Toolbar';
+import { WebformView } from './_components/WebformView';
 
 import { NotificationContext } from 'ui/views/_functions/Contexts/NotificationContext';
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
 
+import { Article15Utils } from '../Article15/_functions/Utils/Article15Utils';
+
 export const Article13 = ({ dataflowId, datasetId, isReporting = false, state }) => {
+  const { datasetSchema } = state;
+
   const notificationContext = useContext(NotificationContext);
   const resources = useContext(ResourcesContext);
 
@@ -43,11 +48,70 @@ export const Article13 = ({ dataflowId, datasetId, isReporting = false, state })
   };
 
   const [article13State, article13Dispatch] = useReducer(article13Reducer, {
+    data: [],
     isWebformView: false,
     tableList: { group: [], single: [] }
   });
 
+  useEffect(() => {
+    initialLoad();
+  }, []);
+
   const { isWebformView, tableList } = article13State;
+
+  const initialLoad = () => {
+    const allTables = tables.map(table => table.name);
+    const parsedData = onLoadData();
+
+    article13Dispatch({ type: 'INITIAL_LOAD', payload: { data: parsedData } });
+  };
+
+  const onLoadData = () => {
+    if (!isEmpty(datasetSchema)) return onParseWebformData(tables, datasetSchema.tables);
+  };
+
+  const onParseWebformData = (allTables, schemaTables) => {
+    const data = Article15Utils.mergeArrays(allTables, schemaTables, 'name', 'tableSchemaName');
+    data.map(table => {
+      if (table.records) {
+        table.records[0].fields = table.records[0].fields.map(field => {
+          const { fieldId, recordId, type } = field;
+
+          return { fieldSchema: fieldId, fieldType: type, recordSchemaId: recordId, ...field };
+        });
+      }
+    });
+
+    for (let index = 0; index < data.length; index++) {
+      const table = data[index];
+
+      if (table.records) {
+        const { elements, records } = table;
+
+        const result = [];
+        for (let index = 0; index < elements.length; index++) {
+          if (elements[index].type === 'FIELD') {
+            result.push({
+              ...elements[index],
+              ...records[0].fields.find(element => element['name'] === elements[index]['name']),
+              type: elements[index].type
+            });
+          } else if (elements[index].type === 'TABLE') {
+            const filteredTable = datasetSchema.tables.filter(table => table.tableSchemaName === elements[index].name);
+            const parsedTable = onParseWebformData([elements[index]], filteredTable);
+
+            result.push({ ...elements[index], ...parsedTable[0], type: elements[index].type });
+          } else if (elements[index].type === 'LABEL') {
+            result.push({ ...elements[index] });
+          }
+        }
+
+        table.elements = result;
+      }
+    }
+
+    return data;
+  };
 
   const onToggleView = view => article13Dispatch({ type: 'ON_TOGGLE_VIEW', payload: { view } });
 
@@ -94,7 +158,17 @@ export const Article13 = ({ dataflowId, datasetId, isReporting = false, state })
         ))}
       </ul>
 
-      {isWebformView ? null : <TableManagement />}
+      {isWebformView ? (
+        <WebformView
+          data={article13State.data}
+          dataflowId={dataflowId}
+          datasetId={datasetId}
+          state={state}
+          tables={tables}
+        />
+      ) : (
+        <TableManagement />
+      )}
     </Fragment>
   );
 };
