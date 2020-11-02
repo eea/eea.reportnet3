812229269820b8fdbd69c1ae95936d5c45f2509c
igor-co <60940858+igor-co@users.noreply.github.com>
2020-11-02 18:08:35 +0100

    Add Table to webforms

--- frontend-service/src/ui/views/Webforms/Webforms.jsx
+++ frontend-service/src/ui/views/Webforms/Webforms.jsx
@@ -3,7 +3,6 @@ import { Article13 } from './_components/Article13';
 import { Article15 } from './_components/Article15';
 
 const Webforms = ({ webformType, dataflowId, datasetId, state }) => {
-  console.log('webformType', webformType);
   switch (webformType) {
     case 'MMR-ART13':
       return <Article13 dataflowId={dataflowId} datasetId={datasetId} state={state} />;
--- frontend-service/src/ui/views/Webforms/_components/Article13/_components/TableManagement/TableManagement.jsx
+++ frontend-service/src/ui/views/Webforms/_components/Article13/_components/TableManagement/TableManagement.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useCallback, useContext, useEffect, useReducer, useRef } from 'react';
+import React, { Fragment, useCallback, useContext, useEffect, useReducer, useRef, useState } from 'react';
 
 import ReactTooltip from 'react-tooltip';
 
@@ -13,6 +13,7 @@ import { AwesomeIcons } from 'conf/AwesomeIcons';
 import { ActionsColumn } from 'ui/views/_components/ActionsColumn';
 import { Button } from 'ui/views/_components/Button';
 import { Column } from 'primereact/column';
+import { ConfirmDialog } from 'ui/views/_components/ConfirmDialog';
 import { DataTable } from 'ui/views/_components/DataTable';
 import { InputSwitch } from 'ui/views/_components/InputSwitch';
 import { Spinner } from 'ui/views/_components/Spinner';
@@ -22,9 +23,19 @@ import { NotificationContext } from 'ui/views/_functions/Contexts/NotificationCo
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
 
 export const TableManagement = () => {
+  const [isConfirmDeleteVisible, setIsConfirmDeleteVisible] = useState(false);
+  const [PaMNumberToDelete, setPaMNumberToDelete] = useState(null);
+  const [tableToCreate, setTableToCreate] = useState(null);
+
+  const resources = useContext(ResourcesContext);
+
+  useEffect(() => {
+    console.log('tableToCreate', tableToCreate);
+  }, [tableToCreate]);
+
   const data = [
     {
-      PaMNumber: 0,
+      PaMNumber: 1,
       nameOfPolicy: 'test',
       singleOrGroup: 'single',
       table1: 'aaaa',
@@ -32,7 +43,7 @@ export const TableManagement = () => {
       table3: 'cccc'
     },
     {
-      PaMNumber: 0,
+      PaMNumber: 2,
       nameOfPolicy: 'test',
       singleOrGroup: 'group',
       table1: 'dddd',
@@ -41,6 +52,99 @@ export const TableManagement = () => {
     }
   ];
 
+  const deletePamBodyTemplate = rowData => {
+    return (
+      <React.Fragment>
+        <span>{rowData.PaMNumber}</span>
+        <Button
+          icon={'trash'}
+          className="p-button-danger"
+          onClick={() => {
+            setPaMNumberToDelete(rowData.PaMNumber);
+            setIsConfirmDeleteVisible(true);
+          }}
+        />
+      </React.Fragment>
+    );
+  };
+
+  const addTable1 = rowData => {
+    return (
+      <React.Fragment>
+        <Button
+          label={'Create Table'}
+          icon={'add'}
+          onClick={() => {
+            setTableToCreate(rowData.table1);
+          }}
+        />
+      </React.Fragment>
+    );
+  };
+
+  const addTable2 = rowData => {
+    return (
+      <React.Fragment>
+        <Button
+          label={'Create Table'}
+          icon={'add'}
+          onClick={() => {
+            setTableToCreate(rowData.table2);
+          }}
+        />
+      </React.Fragment>
+    );
+  };
+
+  const addTable3 = rowData => {
+    return (
+      <React.Fragment>
+        <Button
+          label={'Create Table'}
+          icon={'add'}
+          onClick={() => {
+            setTableToCreate(rowData.table3);
+          }}
+        />
+      </React.Fragment>
+    );
+  };
+
+  const confirmDelete = PaMNumberToDelete => {
+    console.log('PaMNumberToDelete', PaMNumberToDelete);
+  };
+
+  const tableColumns = [
+    { body: deletePamBodyTemplate, field: 'PaMNumber', header: 'PaMnumber' },
+    { field: 'nameOfPolicy', header: 'Name of policy or measure' },
+    { field: 'singleOrGroup', header: 'PaM or group of PaMs' },
+    {
+      body: addTable1,
+      field: 'table1',
+      header:
+        'Table 1: Sectors and gases for reporting on policies and measures and groups of measures, and type of policy instrument'
+    },
+    {
+      body: addTable2,
+      field: 'table2',
+      header:
+        'Table 2: Available results of ex-ante and ex-post assessments of the effects of individual or groups of policies and measures on mitigation of climate change'
+    },
+    {
+      body: addTable3,
+      field: 'table3',
+      header:
+        'Table 3: Available projected and realised costs and benefits of individual or groups of policies and measures on mitigation of climate change'
+    }
+  ];
+
+  const renderTableColumns = tableColumns.map(col => {
+    return isNil(col.body) ? (
+      <Column key={col.field} field={col.field} header={col.header} />
+    ) : (
+      <Column key={col.field} field={col.field} header={col.header} body={col.body} />
+    );
+  });
   return (
     <Fragment>
       <DataTable
@@ -49,8 +153,6 @@ export const TableManagement = () => {
         // value={constraintsState.filteredData}
         value={data}>
         {data.map(datos => {
-          console.log('datos', datos);
-
           Object.keys(datos).map(d => {
             return (
               <Column
@@ -62,6 +164,26 @@ export const TableManagement = () => {
           });
         })}
       </DataTable>
+
+      <DataTable autoLayout={true} value={data}>
+        {renderTableColumns}
+      </DataTable>
+
+      {isConfirmDeleteVisible && (
+        <ConfirmDialog
+          classNameConfirm={'p-button-danger'}
+          header={resources.messages['deleteTabHeader']}
+          labelCancel={resources.messages['no']}
+          labelConfirm={resources.messages['yes']}
+          onConfirm={() => {
+            confirmDelete(PaMNumberToDelete);
+            setIsConfirmDeleteVisible(false);
+          }}
+          onHide={() => setIsConfirmDeleteVisible(false)}
+          visible={isConfirmDeleteVisible}>
+          ARE YOU SURE YOU WANT TO DELETE
+        </ConfirmDialog>
+      )}
     </Fragment>
   );
 };
