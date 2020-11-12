f9711dbe16152b26a12495a6459295674961eadc
azpilm <mazpilicueta-ext@tracasa.es>
2020-11-12 15:56:31 +0100

    update feedback to scroll to last element when create new one

--- frontend-service/src/ui/views/Feedback/_components/ListMessages/ListMessages.jsx
+++ frontend-service/src/ui/views/Feedback/_components/ListMessages/ListMessages.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useReducer, useRef, useState } from 'react';
 import isEmpty from 'lodash/isEmpty';
 import isNil from 'lodash/isNil';
+import last from 'lodash/last';
 
 import styles from './ListMessages.module.scss';
 
@@ -60,9 +61,13 @@ export const ListMessages = ({
   useEffect(() => {
     dispatchListMessages({ type: 'SET_IS_LOADING', payload: false });
     if (newMessageAdded) {
-      messagesWrapperRef.current.scrollTop = messagesWrapperRef.current.scrollHeight;
+      const messages = document.querySelectorAll('.rep-feedback-message');
+      if (!isEmpty(messages)) {
+        const lastMessage = last(messages);
+        messagesWrapperRef.current.scrollTop = lastMessage.offsetTop;
+      }
     }
-  }, [messages]);
+  }, [messages, listContent]);
 
   const onScroll = e => {
     if (!isNil(e)) {
@@ -96,6 +101,7 @@ export const ListMessages = ({
       <div className={styles.scrollMessagesWrapper}>
         {messages.map((message, i) => (
           <Message
+            key={i}
             message={message}
             hasSeparator={
               i === separatorIndex && ((isCustodian && message.direction) || (!isCustodian && !message.direction))
