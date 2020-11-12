2d4d0d149869c8a33d9460d8b257886e04775441
kikolabiano <flabiano@hotmail.com>
2020-11-12 10:31:58 +0100

    update srid name in map

--- frontend-service/src/ui/views/_components/DataViewer/_components/DataForm/DataForm.jsx
+++ frontend-service/src/ui/views/_components/DataViewer/_components/DataForm/DataForm.jsx
@@ -93,7 +93,7 @@ const DataForm = ({
                   fieldValue={
                     isNil(field.fieldData[column.field])
                       ? column.type === 'POINT'
-                        ? `{"type": "Feature", "geometry": {"type":"Point","coordinates":[55.6811608,12.5844761]}, "properties": {"rsid": "EPSG:4326"}}`
+                        ? `{"type": "Feature", "geometry": {"type":"Point","coordinates":[55.6811608,12.5844761]}, "properties": {"srid": "EPSG:4326"}}`
                         : ''
                       : field.fieldData[column.field]
                   }
