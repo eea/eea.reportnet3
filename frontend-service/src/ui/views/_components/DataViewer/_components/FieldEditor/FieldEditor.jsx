cf288a7b17180c346836ac8ec184d054d9d7d5bb
kikolabiano <flabiano@hotmail.com>
2020-11-12 16:27:23 +0100

    fix caret inputTextArea to End

--- frontend-service/src/ui/views/_components/DataViewer/_components/FieldEditor/FieldEditor.jsx
+++ frontend-service/src/ui/views/_components/DataViewer/_components/FieldEditor/FieldEditor.jsx
@@ -199,6 +199,8 @@ const FieldEditor = ({
           <InputTextarea
             collapsedHeight={75}
             onBlur={e => onEditorSubmitValue(cells, e.target.value, record)}
+            maxLength={textCharacters}
+            moveCaretToEnd={true}
             onChange={e => onEditorValueChange(cells, e.target.value)}
             onFocus={e => {
               e.preventDefault();
@@ -206,7 +208,6 @@ const FieldEditor = ({
             }}
             onKeyDown={e => onEditorKeyChange(cells, e, record, false, '', type)}
             value={RecordUtils.getCellValue(cells, cells.field)}
-            maxLength={textCharacters}
           />
         );
       case 'RICH_TEXT':
