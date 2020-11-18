8e729f810eacf30fc0fb42ba4961d8e84554147b
vicente.cano <vicente.cano@altia.es>
2020-11-18 11:24:26 +0100

    update previous sql scripts to flyway

--- database/src/main/resources/db/migration/V24__Delete_column_blocked_snapshot.sql
+++ database/src/main/resources/db/migration/V24__Delete_column_blocked_snapshot.sql
@@ -1,3 +1,10 @@
-ALTER TABLE public."snapshot" DROP COLUMN "blocked";
-
+DO $$
+BEGIN
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='snapshot' and column_name='blocked')
+  THEN
+      ALTER TABLE "public"."snapshot" DROP COLUMN "blocked";
+  END IF;
+END $$;
 COMMIT;
