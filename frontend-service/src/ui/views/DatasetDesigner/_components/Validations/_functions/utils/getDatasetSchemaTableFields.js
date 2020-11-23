aa22a6190943e4d9d38591ff5ee0d37dd4f2a602
azpilm <mazpilicueta-ext@tracasa.es>
2020-11-23 14:01:22 +0100

    update validation bannedfields and change by bannedFieldNames and bannedTypes

--- frontend-service/src/ui/views/DatasetDesigner/_components/Validations/_functions/utils/getDatasetSchemaTableFields.js
+++ frontend-service/src/ui/views/DatasetDesigner/_components/Validations/_functions/utils/getDatasetSchemaTableFields.js
@@ -8,7 +8,7 @@ export const getDatasetSchemaTableFields = (candidateTable, tables) => {
 
   if (!isNil(selectedTable) && !isEmpty(selectedTable) && !isNil(selectedTable.records)) {
     return selectedTable.records[0].fields
-      .filter(field => !config.validations.bannedFields.includes(field.type.toLowerCase()))
+      .filter(field => !config.validations.bannedTypes.includes(field.type.toLowerCase()))
       .map(field => ({
         label: field.name,
         code: field.fieldId
