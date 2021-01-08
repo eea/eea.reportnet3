bd129bef8b2bd1f7d2c2dea8f51c38f21ef7fc42
jlanton <ext.jose.luis.anton@altia.es>
2021-01-08 07:39:33 +0100

    FIXED FieldExtendedRepositoryImpl bean definition

--- dataset-service/src/main/java/org/eea/dataset/persistence/data/repository/FieldExtendedRepositoryImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/persistence/data/repository/FieldExtendedRepositoryImpl.java
@@ -15,60 +15,88 @@ import org.eea.interfaces.vo.dataset.enums.DataType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 
 /**
  * The Class FieldExtendedRepositoryImpl.
  */
+@Repository
 public class FieldExtendedRepositoryImpl implements FieldExtendedRepository {
 
 
-  /** The Constant LOG. */
+  /**
+   * The Constant LOG.
+   */
   private static final Logger LOG = LoggerFactory.getLogger(FieldExtendedRepositoryImpl.class);
 
-  /** The entity manager. */
+  /**
+   * The entity manager.
+   */
   @PersistenceContext
   private EntityManager entityManager;
 
-  /** The field no validation mapper. */
+  /**
+   * The field no validation mapper.
+   */
   @Autowired
   private FieldNoValidationMapper fieldNoValidationMapper;
 
-  /** The field repository. */
+  /**
+   * The field repository.
+   */
   @Autowired
   private FieldRepository fieldRepository;
 
-  /** The Constant SORT_NUMBER_QUERY. */
+  /**
+   * The Constant SORT_NUMBER_QUERY.
+   */
   private static final String SORT_NUMBER_QUERY =
       ",CAST(fv.value as java.math.BigDecimal) as orden ";
 
-  /** The Constant SORT_DATE_QUERY. */
+  /**
+   * The Constant SORT_DATE_QUERY.
+   */
   private static final String SORT_DATE_QUERY = ",CAST(fv.value as java.sql.Date) as orden ";
 
-  /** The Constant SORT_STRING_QUERY. */
+  /**
+   * The Constant SORT_STRING_QUERY.
+   */
   private static final String SORT_STRING_QUERY = ",CAST(fv.value as java.lang.String) as orden ";
 
-  /** The Constant QUERY_1. */
+  /**
+   * The Constant QUERY_1.
+   */
   private static final String QUERY_1 = "SELECT DISTINCT fv as fieldValue, tag as label ";
 
-  /** The Constant QUERY_2_WITHOUT_CONDITIONAL. */
+  /**
+   * The Constant QUERY_2_WITHOUT_CONDITIONAL.
+   */
   private static final String QUERY_2_WITHOUT_CONDITIONAL =
       "FROM FieldValue fv, FieldValue tag  WHERE fv.idFieldSchema = :fieldSchemaId ";
 
-  /** The Constant QUERY_2_WITH_CONDITIONAL. */
+  /**
+   * The Constant QUERY_2_WITH_CONDITIONAL.
+   */
   private static final String QUERY_2_WITH_CONDITIONAL =
       "FROM FieldValue fv, FieldValue tag, FieldValue cond  WHERE fv.idFieldSchema = :fieldSchemaId ";
 
-  /** The Constant QUERY_3. */
+  /**
+   * The Constant QUERY_3.
+   */
   private static final String QUERY_3 =
       "AND tag.idFieldSchema = :labelId AND fv.record.id = tag.record.id " + " AND fv.value <> '' "
           + " AND (:searchText IS NULL or fv.value like CONCAT('%',:searchText,'%') or tag.value like CONCAT('%',:searchText,'%') ) ";
 
-  /** The Constant QUERY_3_CONDITIONAL. */
+  /**
+   * The Constant QUERY_3_CONDITIONAL.
+   */
   private static final String QUERY_3_CONDITIONAL =
       "AND (cond.idFieldSchema = :conditionalId AND cond.value = :conditionalValue AND cond.record.id = fv.record.id or :conditionalId IS NULL) ";
 
-  /** The Constant QUERY_ORDER. */
+  /**
+   * The Constant QUERY_ORDER.
+   */
   private static final String QUERY_ORDER = "ORDER BY orden";
 
 
@@ -81,8 +109,10 @@ public class FieldExtendedRepositoryImpl implements FieldExtendedRepository {
    * @param conditionalSchemaId the conditional schema id
    * @param conditionalValue the conditional value
    * @param resultsNumber the results number
+   *
    * @return the list
    */
+  @Override
   public List<FieldVO> findByIdFieldSchemaWithTagOrdered(String idPk, String labelSchemaId,
       String searchValue, String conditionalSchemaId, String conditionalValue,
       Integer resultsNumber) {
@@ -107,7 +137,6 @@ public class FieldExtendedRepositoryImpl implements FieldExtendedRepository {
       queryBuilder.append(QUERY_2_WITHOUT_CONDITIONAL).append(QUERY_3);
     }
     queryBuilder.append(QUERY_ORDER);
-
 
     Query query = entityManager.createQuery(queryBuilder.toString());
     query.setParameter("fieldSchemaId", idPk);
