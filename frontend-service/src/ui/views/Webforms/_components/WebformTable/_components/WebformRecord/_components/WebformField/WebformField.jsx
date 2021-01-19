63b43902c2dec857ec453646cb0e472cabf9428e
igor-co <icojocaru-ext@tracasa.es>
2021-01-19 13:25:57 +0100

    Fix itemTemplate is waiting for function and recieve boolean instead

--- frontend-service/src/ui/views/Webforms/_components/WebformTable/_components/WebformRecord/_components/WebformField/WebformField.jsx
+++ frontend-service/src/ui/views/Webforms/_components/WebformTable/_components/WebformRecord/_components/WebformField/WebformField.jsx
@@ -315,7 +315,7 @@ export const WebformField = ({
             appendTo={document.body}
             maxSelectedLabels={10}
             id={field.fieldId}
-            itemTemplate={TextUtils.areEquals(field.name, 'ListOfSinglePams') && renderSinglePamsTemplate}
+            itemTemplate={TextUtils.areEquals(field.name, 'ListOfSinglePams') ? renderSinglePamsTemplate : () => {}}
             onChange={event => {
               onFillField(field, option, event.target.value);
               if (isNil(field.recordId)) onSaveField(option, event.target.value);
