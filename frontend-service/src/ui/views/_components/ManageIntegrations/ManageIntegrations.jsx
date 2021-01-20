680c945683607c918979a85b58bc276883470b72
Miriam Gaviria <mgaviria-ext@tracasa.es>
2021-01-20 12:42:15 +0100

    Update - StraightToFME tooltip text

--- frontend-service/src/ui/views/_components/ManageIntegrations/ManageIntegrations.jsx
+++ frontend-service/src/ui/views/_components/ManageIntegrations/ManageIntegrations.jsx
@@ -330,7 +330,7 @@ export const ManageIntegrations = ({
           <Button
             className={`${styles.infoButton} p-button-rounded p-button-secondary-transparent`}
             icon="infoCircle"
-            tooltip={resources.messages['strictModeTooltip']}
+            tooltip={resources.messages['StraightToFMETooltip']}
             tooltipOptions={{ position: 'top' }}
           />
         </label>
