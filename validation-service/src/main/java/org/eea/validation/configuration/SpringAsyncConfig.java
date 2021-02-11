287919d8af5e49fac8d0f67d4fc4c401d152ca6c
jlanton <ext.jose.luis.anton@altia.es>
2021-02-11 07:39:33 +0100

    UPDATE using DelegatingSecurityContextAsyncTaskExecutor on async executions

--- validation-service/src/main/java/org/eea/validation/configuration/SpringAsyncConfig.java
+++ validation-service/src/main/java/org/eea/validation/configuration/SpringAsyncConfig.java
@@ -1,7 +1,10 @@
 package org.eea.validation.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 /**
  * The Class SpringAsyncConfig.
@@ -10,4 +13,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class SpringAsyncConfig {
 
+  @Bean
+  public DelegatingSecurityContextAsyncTaskExecutor taskExecutor(ThreadPoolTaskExecutor delegate) {
+    return new DelegatingSecurityContextAsyncTaskExecutor(delegate);
+  }
 }
