a6d45a192bfb9ad9fc75a1ef149c5eb62fba7408
José Ángel <angel.belda@altia.es>
2021-02-22 12:03:26 +0100

    ADD Javadoc

--- common-utitlities/src/main/java/org/eea/thread/EEADelegatingSecurityContextExecutorService.java
+++ common-utitlities/src/main/java/org/eea/thread/EEADelegatingSecurityContextExecutorService.java
@@ -3,15 +3,27 @@ package org.eea.thread;
 import java.util.concurrent.ExecutorService;
 import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 
+/**
+ * The Class EEADelegatingSecurityContextExecutorService.
+ */
 public class EEADelegatingSecurityContextExecutorService
     extends DelegatingSecurityContextExecutorService {
 
+  /**
+   * Instantiates a new EEA delegating security context executor service.
+   *
+   * @param delegate the delegate
+   */
   public EEADelegatingSecurityContextExecutorService(ExecutorService delegate) {
     super(delegate);
   }
 
+  /**
+   * Gets the delegate executor service.
+   *
+   * @return the delegate executor service
+   */
   public ExecutorService getDelegateExecutorService() {
     return (ExecutorService) getDelegateExecutor();
   }
-
 }
