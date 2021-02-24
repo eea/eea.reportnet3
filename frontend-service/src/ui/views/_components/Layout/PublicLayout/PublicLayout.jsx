428f1ba3972392350ef787a33a6d2431d86168a5
azpilm <mazpilicueta-ext@tracasa.es>
2021-02-24 17:29:58 +0100

    update public layout style to set min height

--- frontend-service/src/ui/views/_components/Layout/PublicLayout/PublicLayout.jsx
+++ frontend-service/src/ui/views/_components/Layout/PublicLayout/PublicLayout.jsx
@@ -1,4 +1,6 @@
-import React, { Fragment, useContext, useEffect } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
+
+import { config } from 'conf';
 
 import styles from './PublicLayout.module.scss';
 
@@ -11,14 +13,23 @@ import { ThemeContext } from 'ui/views/_functions/Contexts/ThemeContext';
 export const PublicLayout = ({ children }) => {
   const themeContext = useContext(ThemeContext);
 
+  const mainContentStyle = {
+    height: `auto`,
+    minHeight: `${window.innerHeight - config.theme.baseHeaderHeight}px`,
+    marginTop: 0
+  };
+
   useEffect(() => {
     themeContext.onToggleTheme('light');
   }, []);
+
   return (
     <Fragment>
       <div className={styles.mainContainer}>
         <Header isPublic={true} />
-        {children}
+        <div className={styles.mainContent} style={mainContentStyle}>
+          {children}
+        </div>
         <Footer />
         <EuFooter />
       </div>
