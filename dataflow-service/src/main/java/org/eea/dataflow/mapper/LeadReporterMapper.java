856356fba348f6b4314d982c2fab61794696a2f5
ruben.lozano <ruben.lozano@altia.es>
2021-02-25 10:25:28 +0100

    FIX sonar and lead reporter mapper

--- dataflow-service/src/main/java/org/eea/dataflow/mapper/LeadReporterMapper.java
+++ dataflow-service/src/main/java/org/eea/dataflow/mapper/LeadReporterMapper.java
@@ -4,11 +4,32 @@ import org.eea.dataflow.persistence.domain.LeadReporter;
 import org.eea.interfaces.vo.dataflow.LeadReporterVO;
 import org.eea.mapper.IMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * The Interface LeadReporterMapper.
  */
 @Mapper(componentModel = "spring")
 public interface LeadReporterMapper extends IMapper<LeadReporter, LeadReporterVO> {
+
+  /**
+   * Entity to class.
+   *
+   * @param entity the entity
+   * @return the representative VO
+   */
+  @Override
+  @Mapping(source = "representative.id", target = "representativeId")
+  LeadReporterVO entityToClass(LeadReporter entity);
+
+  /**
+   * Class to entity.
+   *
+   * @param model the model
+   * @return the representative
+   */
+  @Override
+  @Mapping(source = "representativeId", target = "representative.id")
+  LeadReporter classToEntity(LeadReporterVO model);
 
 }
