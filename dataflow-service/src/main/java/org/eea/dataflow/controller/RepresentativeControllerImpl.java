0792a3cdd5b893626c7ea30fa75bc1bd26056937
ruben.lozano <ruben.lozano@altia.es>
2021-02-25 08:28:10 +0100

    FIX lock

--- dataflow-service/src/main/java/org/eea/dataflow/controller/RepresentativeControllerImpl.java
+++ dataflow-service/src/main/java/org/eea/dataflow/controller/RepresentativeControllerImpl.java
@@ -351,7 +351,7 @@ public class RepresentativeControllerImpl implements RepresentativeController {
   @Override
   @HystrixCommand
   @PostMapping("/{representativeId}/leadReporter")
-  @LockMethod(removeWhenFinish = false)
+  @LockMethod
   @PreAuthorize("hasAnyRole('DATA_CUSTODIAN','DATA_STEWARD')")
   @ApiOperation(value = "Create one Lead reporter", response = Long.class)
   public Long createLeadReporter(
