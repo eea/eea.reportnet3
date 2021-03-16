4129c19a7b6c6493dc4e20fdb21ffe3b549ffb02
igor-co <icojocaru-ext@tracasa.es>
2021-03-16 13:44:51 +0100

    Update make cards responsive

--- frontend-service/src/ui/views/PublicCountries/PublicCountries.jsx
+++ frontend-service/src/ui/views/PublicCountries/PublicCountries.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import europeanFlag from 'assets/images/logos/europeanFlag.png';
 import { config } from 'conf';
 import { routes } from 'ui/routes';
 
@@ -18,6 +17,7 @@ import { useBreadCrumbs } from 'ui/views/_functions/Hooks/useBreadCrumbs';
 
 import { CurrentPage } from 'ui/views/_functions/Utils';
 import { getUrl } from 'core/infrastructure/CoreUtils';
+import { EuroFlag } from './components/EuroFlag';
 
 export const PublicCountries = withRouter(({ history }) => {
   const resources = useContext(ResourcesContext);
@@ -35,62 +35,42 @@ export const PublicCountries = withRouter(({ history }) => {
     }
   }, [themeContext.headerCollapse]);
 
+  const associatesCountriesCodes = config.countriesByGroup.associates.map(country => country.code);
+
+  const renderCard = isEurope => country => {
+    const countryCode = country.code;
+
+    return (
+      <div
+        href={getUrl(routes.COUNTRY)}
+        key={country.code}
+        onClick={e => {
+          e.preventDefault();
+          history.push(getUrl(routes.PUBLIC_COUNTRY_INFORMATION, { countryCode }, true));
+        }}
+        className={styles.wrapper}>
+        <ReactCountryFlag aria-label={country.name} className={styles.flag} countryCode={country.code} svg />
+
+        <div className={styles.titleWrap}>
+          <h3>{country.name}</h3>
+        </div>
+
+        <div className={styles.euFlagWrapper}>
+          {isEurope && !associatesCountriesCodes.includes(countryCode) && <EuroFlag className={styles.euFlag} />}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <PublicLayout>
       <div className={styles.content} style={contentStyles}>
         <div className={`rep-container ${styles.repContainer}`}>
           <h1 className={styles.title}>{resources.messages['countriesPageTitle']}</h1>
           <h2>{resources.messages['eeaCountries']}</h2>
-          <div className={styles.countriesWrapper}>
-            {config.countriesByGroup.eeaCountries.map(country => {
-              const countryCode = country.code;
-              return (
-                <div
-                  href={getUrl(routes.COUNTRY)}
-                  key={country.code}
-                  onClick={e => {
-                    e.preventDefault();
-                    history.push(getUrl(routes.PUBLIC_COUNTRY_INFORMATION, { countryCode }, true));
-                  }}>
-                  <>
-                    <img alt={country.name} className={styles.country} src={europeanFlag}></img>
-                    <h3>{country.name}</h3>
-                    <ReactCountryFlag
-                      aria-label={country.name}
-                      className={styles.flag}
-                      countryCode={country.code}
-                      svg
-                    />
-                  </>
-                </div>
-              );
-            })}
-          </div>
+          <div className={styles.countriesWrapper}>{config.countriesByGroup.eeaCountries.map(renderCard(true))}</div>
           <h2>{resources.messages['otherCountries']}</h2>
-          <div className={styles.countriesWrapper}>
-            {config.countriesByGroup.otherCountries.map(country => {
-              const countryCode = country.code;
-              return (
-                <div
-                  key={country.code}
-                  href={getUrl(routes.COUNTRY)}
-                  onClick={e => {
-                    e.preventDefault();
-                    history.push(getUrl(routes.PUBLIC_COUNTRY_INFORMATION, { countryCode }, true));
-                  }}>
-                  <>
-                    <h3>{country.name}</h3>
-                    <ReactCountryFlag
-                      aria-label={country.name}
-                      className={styles.flag}
-                      countryCode={country.code}
-                      svg
-                    />
-                  </>
-                </div>
-              );
-            })}
-          </div>
+          <div className={styles.countriesWrapper}>{config.countriesByGroup.otherCountries.map(renderCard(false))}</div>
         </div>
       </div>
     </PublicLayout>
