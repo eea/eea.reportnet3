e3135089cdcd4f0015fc7d421e659fc9a5d156b8
igor-co <icojocaru-ext@tracasa.es>
2021-04-12 13:05:41 +0200

    Update - removed React import

--- frontend-service/src/ui/views/_components/Filters/Filters.jsx
+++ frontend-service/src/ui/views/_components/Filters/Filters.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useEffect, useReducer, useRef } from 'react';
+import { Fragment, useContext, useEffect, useReducer, useRef } from 'react';
 
 import cloneDeep from 'lodash/cloneDeep';
 import isEmpty from 'lodash/isEmpty';
