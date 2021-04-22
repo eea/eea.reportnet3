623917c4611570fec1607b43c79274d862df9bb2
Bea <beatrizsevilla@altia.es>
2021-04-22 10:01:58 +0200

    update Polygon message

--- common-utitlities/src/main/java/org/eea/utils/LiteralConstants.java
+++ common-utitlities/src/main/java/org/eea/utils/LiteralConstants.java
@@ -153,6 +153,13 @@ public final class LiteralConstants {
   public static final String RELEASEMESSAGE =
       "This automatic notification informs that %s has successfully released in %s on %s (CET).";
 
+  public static final String GEOMETRYERROR =
+      "The value does not follow the expected syntax for a valid ";
+
+  /** The Constant POLYGONERROR. */
+  public static final String POLYGONERROR =
+      "Polygon geometry is not valid - polygon ring not closed";
+
   /**
    * Instantiates a new literal constants.
    */
