01b474f6572dd7881ea8666f62ca6a0f1e0b21ed
azpilm <mazpilicueta-ext@tracasa.es>
2021-04-23 08:06:59 +0200

    update inputText to change fonts and css to scss

--- frontend-service/src/ui/views/_components/InputText/InputText.jsx
+++ frontend-service/src/ui/views/_components/InputText/InputText.jsx
@@ -1,44 +1,8 @@
-// import React, { forwardRef } from 'react';
-// import { InputText as PrimeInputText } from 'primereact/inputtext';
-
-// export const InputText = forwardRef((props, _) => {
-//   const {
-//     autoFocus,
-//     className,
-//     disabled = false,
-//     inputRef,
-//     onBlur,
-//     onChange,
-//     onFocus,
-//     onInput,
-//     onKeyDown,
-//     placeholder,
-//     type,
-//     value
-//   } = props;
-//   return (
-//     <PrimeInputText
-//       autoFocus={autoFocus}
-//       className={className}
-//       disabled={disabled}
-//       onBlur={onBlur}
-//       onChange={onChange}
-//       onFocus={onFocus}
-//       onInput={onInput}
-//       onKeyDown={onKeyDown}
-//       placeholder={placeholder}
-//       type={type}
-//       ref={inputRef}
-//       value={value}
-//     />
-//   );
-// });
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import './InputText.css';
+import './InputText.scss';
 
 import KeyFilter from 'ui/views/_functions/PrimeReact/KeyFilter';
 import DomHandler from 'ui/views/_functions/PrimeReact/DomHandler';
