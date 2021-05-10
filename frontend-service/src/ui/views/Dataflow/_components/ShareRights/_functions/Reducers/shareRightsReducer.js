4505a9462a93996faa41b2a7937c7c67dee5239a
igor-co <icojocaru-ext@tracasa.es>
2021-05-10 16:09:04 +0200

    Update alphabetic order

--- frontend-service/src/ui/views/Dataflow/_components/ShareRights/_functions/Reducers/shareRightsReducer.js
+++ frontend-service/src/ui/views/Dataflow/_components/ShareRights/_functions/Reducers/shareRightsReducer.js
@@ -3,6 +3,9 @@ export const shareRightsReducer = (state, { type, payload }) => {
     case 'GET_USER_RIGHT_LIST':
       return { ...state, userRightList: payload.userRightList, clonedUserRightList: payload.clonedUserRightList };
 
+    case 'ON_CLOSE_MANAGEMENT_DIALOG':
+      return { ...state, userRight: { account: '', isNew: true, role: '' }, isEditing: false };
+
     case 'ON_DATA_CHANGE':
       return { ...state, isDataUpdated: payload.isDataUpdated };
 
@@ -13,12 +16,11 @@ export const shareRightsReducer = (state, { type, payload }) => {
         userRightToDelete: payload.userRightToDelete
       };
 
-    case 'ON_CLOSE_MANAGEMENT_DIALOG':
-      return {
-        ...state,
-        userRight: { account: '', isNew: true, role: '' },
-        isEditing: false
-      };
+    case 'ON_EDIT_USER_RIGHT':
+      return { ...state, isEditing: payload.isEditing, userRight: payload.userRight };
+
+    case 'ON_ROLE_CHANGE':
+      return { ...state, userRight: { ...state.userRight, role: payload.role } };
 
     case 'ON_SET_ACCOUNT':
       return {
@@ -28,20 +30,14 @@ export const shareRightsReducer = (state, { type, payload }) => {
         accountNotFound: payload.accountNotFound
       };
 
-    case 'ON_ROLE_CHANGE':
-      return { ...state, userRight: { ...state.userRight, role: payload.role } };
-
-    case 'SET_IS_LOADING':
-      return { ...state, isLoading: payload.isLoading };
-
     case 'SET_ACCOUNT_HAS_ERROR':
       return { ...state, accountHasError: payload.accountHasError };
 
-    case 'ON_EDIT_USER_RIGHT':
-      return { ...state, isEditing: payload.isEditing, userRight: payload.userRight };
-
     case 'SET_ACCOUNT_NOT_FOUND':
       return { ...state, accountNotFound: payload.accountNotFound, accountHasError: payload.accountHasError };
+
+    case 'SET_IS_LOADING':
+      return { ...state, isLoading: payload.isLoading };
 
     case 'SET_IS_VISIBLE_DELETE_CONFIRM_DIALOG':
       return { ...state, isDeleteDialogVisible: payload.isDeleteDialogVisible };
