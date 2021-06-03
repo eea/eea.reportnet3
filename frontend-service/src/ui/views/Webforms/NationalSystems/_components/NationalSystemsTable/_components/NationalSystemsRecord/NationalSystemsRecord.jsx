e676ffee70ba1d07408b3d5087c14bfde2f9848a
Miriam Gaviria <mgaviria-ext@tracasa.es>
2021-06-03 14:17:46 +0200

    Add - dataflowId and dataProviderId in national systems download file

--- frontend-service/src/ui/views/Webforms/NationalSystems/_components/NationalSystemsTable/_components/NationalSystemsRecord/NationalSystemsRecord.jsx
+++ frontend-service/src/ui/views/Webforms/NationalSystems/_components/NationalSystemsTable/_components/NationalSystemsRecord/NationalSystemsRecord.jsx
@@ -4,7 +4,7 @@ import styles from './NationalSystemsRecord.module.scss';
 
 import { NationalSystemsField } from './_components/NationalSystemsField';
 
-export const NationalSystemsRecord = ({ datasetId, getTableErrors, record }) => (
+export const NationalSystemsRecord = ({ dataProviderId, dataflowId, datasetId, getTableErrors, record }) => (
   <div className={styles.record}>
     {record.elements.map((element, index) => {
       const { name, title, titleSource, tooltipSource } = element;
@@ -12,6 +12,8 @@ export const NationalSystemsRecord = ({ datasetId, getTableErrors, record }) => 
       return (
         <Fragment key={index}>
           <NationalSystemsField
+            dataProviderId={dataProviderId}
+            dataflowId={dataflowId}
             datasetId={datasetId}
             getTableErrors={getTableErrors}
             nationalField={name}
