ae7808ff03e09691dc0c0dce7488accb3b6d6da3
Arrastia <arrastia@pm.me>
2021-06-07 16:25:32 +0200

    update edit/add reference modal

--- frontend-service/src/ui/views/Dataflows/_components/ManageReferenceDataflow/ManageReferenceDataflow.jsx
+++ frontend-service/src/ui/views/Dataflows/_components/ManageReferenceDataflow/ManageReferenceDataflow.jsx
@@ -2,10 +2,15 @@ import React, { Fragment, useContext, useRef, useState } from 'react';
 
 import isEmpty from 'lodash/isEmpty';
 
+import { config } from 'conf';
+import { routes } from 'ui/routes';
+
 import styles from './ManageReferenceDataflow.module.scss';
 
 import { Button } from 'ui/views/_components/Button';
+import { ConfirmDialog } from 'ui/views/_components/ConfirmDialog';
 import { Dialog } from 'ui/views/_components/Dialog';
+import { ErrorMessage } from 'ui/views/_components/ErrorMessage';
 import { InputText } from 'ui/views/_components/InputText';
 import { InputTextarea } from 'ui/views/_components/InputTextarea';
 
@@ -16,11 +21,9 @@ import { NotificationContext } from 'ui/views/_functions/Contexts/NotificationCo
 import { ResourcesContext } from 'ui/views/_functions/Contexts/ResourcesContext';
 
 import { useInputTextFocus } from 'ui/views/_functions/Hooks/useInputTextFocus';
-import { ConfirmDialog } from 'ui/views/_components/ConfirmDialog/ConfirmDialog';
-import { routes } from 'ui/routes';
+
 import { getUrl } from 'core/infrastructure/CoreUtils';
 import { TextUtils } from 'ui/views/_functions/Utils';
-import { config } from 'conf';
 
 export const ManageReferenceDataflow = ({
   dataflowId,
@@ -32,6 +35,7 @@ export const ManageReferenceDataflow = ({
   onManage
 }) => {
   const dialogName = isEditing ? 'isEditDialogVisible' : 'isReferencedDataflowDialogVisible';
+  const INPUT_MAX_LENGTH = 255;
   const isDesign = TextUtils.areEquals(metadata?.status, config.dataflowStatus.DESIGN);
 
   const { hideLoading, showLoading } = useContext(LoadingContext);
@@ -40,7 +44,10 @@ export const ManageReferenceDataflow = ({
 
   const [deleteInput, setDeleteInput] = useState('');
   const [description, setDescription] = useState(isEditing ? metadata.description : '');
-  const [hasErrors, setHasErrors] = useState({ description: false, name: false });
+  const [errors, setErrors] = useState({
+    description: { hasErrors: false, message: '' },
+    name: { hasErrors: false, message: '' }
+  });
   const [isDeleteDialogVisible, setIsDeleteDialogVisible] = useState(false);
   const [isSending, setIsSending] = useState(false);
   const [name, setName] = useState(isEditing ? metadata.name : '');
@@ -50,6 +57,29 @@ export const ManageReferenceDataflow = ({
 
   useInputTextFocus(isVisible, inputRef);
   useInputTextFocus(isDeleteDialogVisible, deleteInputRef);
+
+  const checkErrors = () => {
+    let hasErrors = false;
+    if (description.length > INPUT_MAX_LENGTH) {
+      handleErrors({
+        field: 'description',
+        hasErrors: true,
+        message: resources.messages['dataflowDescriptionValidationMax']
+      });
+      hasErrors = true;
+    }
+
+    if (name.length > INPUT_MAX_LENGTH) {
+      handleErrors({ field: 'name', hasErrors: true, message: resources.messages['dataflowNameValidationMax'] });
+      hasErrors = true;
+    }
+
+    return hasErrors;
+  };
+
+  const handleErrors = ({ field, hasErrors, message }) => {
+    setErrors(prevState => ({ ...prevState, [field]: { message, hasErrors } }));
+  };
 
   const onDeleteDataflow = async () => {
     setIsDeleteDialogVisible(false);
@@ -68,8 +98,10 @@ export const ManageReferenceDataflow = ({
   };
 
   const onManageReferenceDataflow = async () => {
-    setIsSending(true);
+    if (checkErrors()) return;
+
     try {
+      setIsSending(true);
       if (isEditing) {
         const { status } = await ReferenceDataflowService.edit(dataflowId, description, name);
 
@@ -80,7 +112,16 @@ export const ManageReferenceDataflow = ({
         if (status >= 200 && status <= 299) onManage();
       }
     } catch (error) {
-      console.log('error :>> ', error);
+      if (TextUtils.areEquals(error?.response?.data, 'Dataflow name already exists')) {
+        handleErrors({ field: 'name', hasErrors: true, message: resources.messages['dataflowNameExists'] });
+        notificationContext.add({ type: 'DATAFLOW_NAME_EXISTS' });
+      } else {
+        const notification = isEditing
+          ? { type: 'DATAFLOW_UPDATING_ERROR', content: { dataflowId, dataflowName: name } }
+          : { type: 'DATAFLOW_CREATION_ERROR', content: { dataflowName: name } };
+
+        notificationContext.add(notification);
+      }
     } finally {
       setIsSending(false);
     }
@@ -122,24 +163,26 @@ export const ManageReferenceDataflow = ({
         header={'Reference dataflow'}
         onHide={() => manageDialogs(dialogName, false)}
         visible={isVisible}>
-        <div className={`formField ${hasErrors.name ? 'error' : ''}`}>
+        <div className={`formField ${errors.name.hasErrors ? 'error' : ''}`}>
           <InputText
             onChange={event => setName(event.target.value)}
-            onFocus={() => setHasErrors({ ...hasErrors, name: false })}
+            onFocus={() => handleErrors({ field: 'name', hasErrors: false, message: '' })}
             placeholder={resources.messages['createDataflowName']}
             ref={inputRef}
             value={name}
           />
+          {!isEmpty(errors.name.message) && <ErrorMessage message={errors.name.message} />}
         </div>
-        <div className={`formField ${hasErrors.description ? 'error' : ''}`}>
+        <div className={`formField ${errors.description.hasErrors ? 'error' : ''}`}>
           <InputTextarea
             className={styles.inputTextArea}
             onChange={event => setDescription(event.target.value)}
-            onFocus={() => setHasErrors({ ...hasErrors, description: false })}
+            onFocus={() => handleErrors({ field: 'description', hasErrors: false, message: '' })}
             placeholder={resources.messages['createDataflowDescription']}
-            rows={100}
+            rows={10}
             value={description}
           />
+          {!isEmpty(errors.description.message) && <ErrorMessage message={errors.description.message} />}
         </div>
       </Dialog>
 
