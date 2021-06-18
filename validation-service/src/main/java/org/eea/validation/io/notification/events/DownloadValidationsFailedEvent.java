31752d14a8a20df113a984747d695a0adf78c7cf
Cristian Martinez <cmartmac@gmail.com>
2021-06-18 12:00:46 +0200

    Created DownloadValidationsCompletedEvent and FailedEvent classes

---
+++ validation-service/src/main/java/org/eea/validation/io/notification/events/DownloadValidationsFailedEvent.java
@@ -0,0 +1,50 @@
+package org.eea.validation.io.notification.events;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.eea.exception.EEAException;
+import org.eea.interfaces.controller.dataset.DatasetMetabaseController;
+import org.eea.kafka.domain.EventType;
+import org.eea.kafka.domain.NotificationVO;
+import org.eea.notification.event.NotificableEventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DownloadValidationsFailedEvent implements NotificableEventHandler 
+{
+
+
+  /**
+   * Gets the event type.
+   *
+   * @return the event type
+   */
+  @Override
+  public EventType getEventType() {
+    return EventType.DOWNLOAD_VALIDATIONS_FAILED_EVENT;
+  }
+
+  /**
+   * Gets the map.
+   *
+   * @param notificationVO the notification VO
+   * @return the map
+   * @throws EEAException the EEA exception
+   */
+  @Override
+  public Map<String, Object> getMap(NotificationVO notificationVO) throws EEAException 
+  {
+    Long datasetId = notificationVO.getDatasetId();
+    String nameFile = notificationVO.getFileName();
+
+
+    Map<String, Object> notification = new HashMap<>();
+    notification.put("user", notificationVO.getUser());
+    notification.put("datasetId", datasetId);
+    notification.put("nameFile", nameFile);
+    return notification;
+  }
+
+
+}
