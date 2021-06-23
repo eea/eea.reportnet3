d16a699d6642435cc5598ec48fd2c6cdc4e591fa
Miriam Gaviria <mgaviria-ext@tracasa.es>
2021-06-23 13:43:33 +0200

    Fix - accessibility issues - user configuration

--- frontend-service/src/ui/views/_components/TitleWithItem/TitleWithItem.jsx
+++ frontend-service/src/ui/views/_components/TitleWithItem/TitleWithItem.jsx
@@ -27,7 +27,12 @@ const TitleWithItem = memo(
       <div className={styles.rowContainer}>
         <div className={styles.titleWrap}>
           <div className={styles.iconWrap}>
-            <FontAwesomeIcon className={styles.icon} icon={AwesomeIcons(icon)} style={{ fontSize: iconSize }} />
+            <FontAwesomeIcon
+              className={styles.icon}
+              icon={AwesomeIcons(icon)}
+              role="presentation"
+              style={{ fontSize: iconSize }}
+            />
           </div>
           <div className={styles.textWrap}>
             <span className={styles.title}>
