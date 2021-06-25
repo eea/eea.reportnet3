fe843f1eb4cd4e8059e4f4fbf9d31c90eba4e87d
ruben.lozano <ruben.lozano@altia.es>
2021-06-25 13:05:08 +0200

    Jdoc

--- common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/enums/FileTypeEnum.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/enums/FileTypeEnum.java
@@ -3,17 +3,25 @@ package org.eea.interfaces.vo.dataset.enums;
 import java.util.Arrays;
 
 
+/**
+ * The Enum FileTypeEnum.
+ */
 public enum FileTypeEnum {
 
 
+  /** The csv. */
   CSV("csv"),
 
+  /** The xml. */
   XML("xml"),
 
+  /** The xls. */
   XLS("xls"),
 
+  /** The xlsx. */
   XLSX("xlsx"),
 
+  /** The validations. */
   VALIDATIONS("validations");
 
   /** The value. */
@@ -38,6 +46,12 @@ public enum FileTypeEnum {
   }
 
 
+  /**
+   * Gets the enum.
+   *
+   * @param value the value
+   * @return the enum
+   */
   public static FileTypeEnum getEnum(String value) {
     return Arrays.stream(FileTypeEnum.values()).filter(m -> m.value.equals(value)).findAny()
         .orElse(null);
