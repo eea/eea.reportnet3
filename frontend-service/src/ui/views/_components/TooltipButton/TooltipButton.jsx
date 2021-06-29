5159e0f2abb525c3f12915489f2263aed2988bfb
igor-co <icojocaru-ext@tracasa.es>
2021-06-29 13:06:37 +0200

    Add tooltip button component

---
+++ frontend-service/src/ui/views/_components/TooltipButton/TooltipButton.jsx
@@ -0,0 +1,23 @@
+import styles from './TooltipButton.module.scss';
+import ReactTooltip from 'react-tooltip';
+import { Button } from 'ui/views/_components/Button';
+
+export const TooltipButton = ({ message, uniqueIdentifier, getContent = null }) => {
+  return (
+    <>
+      <span data-for={`infoCircleButton_${uniqueIdentifier}`} data-tip>
+        <Button
+          className={`${styles.tooltipButton} p-button-rounded p-button-secondary-transparent`}
+          icon="infoCircle"
+        />
+      </span>
+      <ReactTooltip
+        effect="solid"
+        getContent={() => (getContent ? getContent() : message)}
+        html={true}
+        id={`infoCircleButton_${uniqueIdentifier}`}
+        place="top"
+      />
+    </>
+  );
+};
