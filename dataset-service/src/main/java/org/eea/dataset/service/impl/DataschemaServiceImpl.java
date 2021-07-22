2dfd07ea4ae61e614c47af1df3596c1689186bdd
ruben.lozano <ruben.lozano@altia.es>
2021-07-22 10:49:31 +0200

    FIX validate schema

--- dataset-service/src/main/java/org/eea/dataset/service/impl/DataschemaServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/DataschemaServiceImpl.java
@@ -1185,9 +1185,7 @@ public class DataschemaServiceImpl implements DatasetSchemaService {
     Boolean isValid = true;
     DataSetSchemaVO schema = getDataSchemaById(datasetSchemaId);
     for (ValidationSchemaCommand command : validationCommands) {
-      if (Boolean.FALSE.equals(command.execute(schema, dataflowType))) {
-        isValid = false;
-      }
+      isValid = Boolean.TRUE.equals(isValid) ? command.execute(schema, dataflowType) : isValid;
     }
 
     return isValid;
@@ -3365,4 +3363,5 @@ public class DataschemaServiceImpl implements DatasetSchemaService {
       }
     }
   }
+
 }
