0553dccdb3f04fa15f2613933c3c60746f96a318
Miriam Gaviria <mgaviria-ext@tracasa.es>
2021-07-27 13:28:32 +0200

    Update - get representativesUsersList endpoint

--- frontend-service/src/core/infrastructure/domain/model/Dataflow/ApiDataflowRepository.js
+++ frontend-service/src/core/infrastructure/domain/model/Dataflow/ApiDataflowRepository.js
@@ -356,41 +356,41 @@ const getAllDataflowsUserList = async () => {
 };
 
 const getRepresentativesUsersList = async dataflowId => {
-  const response = {
-    data: [
-      {
-        email: 'beaprovider@reportnet.net',
-        roles: ['LEAD_REPORTER'],
-        country: 'ES'
-      },
-      {
-        email: 'beaprovider2@reportnet.net',
-        roles: ['REPORTER_READ', 'REPORTER_READ'],
-        country: 'ES'
-      },
-      {
-        email: 'nationalspain@reportnet.net',
-        roles: ['NATIONAL_COORDINATOR'],
-        country: 'ES'
-      },
-      {
-        email: 'bse.ies.so@gmail.com',
-        roles: ['DATA_CUSTODIAN'],
-        country: null
-      },
-      {
-        email: 'beaprovider2@reportnet.net',
-        roles: ['DATA_CUSTODIAN'],
-        country: null
-      },
-      {
-        email: 'beaprovider3@reportnet.net',
-        roles: ['DATA_STEWARD'],
-        country: null
-      }
-    ]
-  };
-  // const response = await apiDataflow.getRepresentativesUsersList(dataflowId);
+  // const response = {
+  //   data: [
+  //     {
+  //       email: 'beaprovider@reportnet.net',
+  //       roles: ['LEAD_REPORTER'],
+  //       country: 'ES'
+  //     },
+  //     {
+  //       email: 'beaprovider2@reportnet.net',
+  //       roles: ['REPORTER_READ', 'REPORTER_READ'],
+  //       country: 'ES'
+  //     },
+  //     {
+  //       email: 'nationalspain@reportnet.net',
+  //       roles: ['NATIONAL_COORDINATOR'],
+  //       country: 'ES'
+  //     },
+  //     {
+  //       email: 'bse.ies.so@gmail.com',
+  //       roles: ['DATA_CUSTODIAN'],
+  //       country: null
+  //     },
+  //     {
+  //       email: 'beaprovider2@reportnet.net',
+  //       roles: ['DATA_CUSTODIAN'],
+  //       country: null
+  //     },
+  //     {
+  //       email: 'beaprovider3@reportnet.net',
+  //       roles: ['DATA_STEWARD'],
+  //       country: null
+  //     }
+  //   ]
+  // };
+  const response = await apiDataflow.getRepresentativesUsersList(dataflowId);
   const usersList = parseCountriesUserList(response.data);
   response.data = sortBy(usersList, 'country');
   return response;
@@ -638,7 +638,7 @@ const parseAllDataflowsUserList = allDataflowsUserListDTO => {
 const parseCountriesUserList = usersListDTO => {
   usersListDTO.forEach((user, usersIndex) => {
     user.roles.forEach((role, roleIndex) => {
-      usersListDTO[usersIndex].roles[roleIndex] = getUserRoleLabel(role);
+      usersListDTO[usersIndex].roles[roleIndex] = UserRoleUtils.getUserRoleLabel(role);
     });
   });
   const usersList = [];
