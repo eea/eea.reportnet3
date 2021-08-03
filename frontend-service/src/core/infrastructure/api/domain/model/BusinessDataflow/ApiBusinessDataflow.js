ec890844fc9d82eafdb8d6fbb2d3e52518b59ee0
igor-co <icojocaru-ext@tracasa.es>
2021-08-03 09:44:42 +0200

    Update endpoint

--- frontend-service/src/core/infrastructure/api/domain/model/BusinessDataflow/ApiBusinessDataflow.js
+++ frontend-service/src/core/infrastructure/api/domain/model/BusinessDataflow/ApiBusinessDataflow.js
@@ -5,16 +5,16 @@ import { HTTPRequester } from 'core/infrastructure/HTTPRequester';
 export const apiBusinessDataflow = {
   all: async () => await HTTPRequester.get({ url: getUrl(BusinessDataflowConfig.all) }),
 
-  create: async (name, description, obligationId, type, groupCompaniesId, fmeUserId) => {
+  create: async (name, description, obligationId, groupCompaniesId, fmeUserId) => {
     return await HTTPRequester.post({
       url: getUrl(BusinessDataflowConfig.createDataflow),
-      data: { name, description, obligationId, type, groupCompaniesId, fmeUserId }
+      data: { name, description, obligationId, type: 'BUSINESS', groupCompaniesId, fmeUserId }
     });
   },
 
-  edit: async (dataflowId, description, name, type, groupCompaniesId, fmeUserId) =>
+  edit: async (dataflowId, description, name, groupCompaniesId, fmeUserId) =>
     await HTTPRequester.update({
       url: getUrl(BusinessDataflowConfig.createDataflow),
-      data: { description, id: dataflowId, name, type, groupCompaniesId, fmeUserId }
+      data: { description, id: dataflowId, name, type: 'BUSINESS', groupCompaniesId, fmeUserId }
     })
 };
