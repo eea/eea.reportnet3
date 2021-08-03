929cc16e50434d38273d3cdc1d2ad0812a254c94
cristian.martinez <cristian.martinez@altia.es>
2021-08-03 08:27:49 +0200

    Removed data queries in SQL script.

--- database/src/main/resources/db/migration/V41_Alter_data_provider_fk.sql
+++ database/src/main/resources/db/migration/V41_Alter_data_provider_fk.sql
@@ -1,7 +1,3 @@
-INSERT INTO data_provider_group (id, type, name) SELECT id, type, label from data_provider;
-
-update data_provider set group_id = id;
-
 ALTER TABLE public.data_provider ADD CONSTRAINT data_provider_fk FOREIGN KEY (group_id) REFERENCES public.data_provider_group(id);
 
 ALTER TABLE public.data_provider DROP COLUMN "type";
