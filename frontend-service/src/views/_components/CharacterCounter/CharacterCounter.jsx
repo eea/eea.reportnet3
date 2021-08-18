f63b32d827b20d08a30206b9a85f6cdc2fb931bb
ktklein <ktklein-ext@tracasa.es>
2021-08-18 09:21:21 +0200

    update CharacterCounter component and CharacterCounter styles

--- frontend-service/src/views/_components/CharacterCounter/CharacterCounter.jsx
+++ frontend-service/src/views/_components/CharacterCounter/CharacterCounter.jsx
@@ -2,8 +2,9 @@ import styles from './CharacterCounter.module.scss';
 
 const CharacterCounter = ({ currentLength, maxLength }) => {
   return (
-      <p className={currentLength > 245 ? styles.redCharacterCount : styles.characterCount}>{`${currentLength}/${maxLength}`}
-      </p> 
+    <p className={currentLength > 245 ? styles.redCharacterCount : styles.characterCount}>
+      {`${currentLength}/${maxLength}`}
+    </p>
   );
 };
 export { CharacterCounter };
