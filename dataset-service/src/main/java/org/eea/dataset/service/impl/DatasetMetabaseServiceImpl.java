9b9c43d60500ad6d9214f8ec7d1387ff3ef743e8
vicente.cano <vicente.cano@altia.es>
2021-08-27 13:56:09 +0200

    FIX cacheable

--- dataset-service/src/main/java/org/eea/dataset/service/impl/DatasetMetabaseServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/DatasetMetabaseServiceImpl.java
@@ -69,6 +69,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
@@ -234,7 +235,8 @@ public class DatasetMetabaseServiceImpl implements DatasetMetabaseService {
    */
   @Override
   @Transactional
-  @CacheEvict(value = "dataFlowId", key = "#datasetId")
+  @Caching(evict = {@CacheEvict(value = "dataFlowId", key = "#datasetId"),
+      @CacheEvict(value = "datasetSchemaByDatasetId", key = "#datasetId")})
   public void deleteDesignDataset(Long datasetId) {
     dataSetMetabaseRepository.deleteNativeDataset(datasetId);
   }
