3c4dd3f44c89db0df00207253d62284af6c4b96d
Arrastia <arrastia@pm.me>
2021-09-22 10:34:23 +0200

    UPDATE - MenuItem to avoid Y scroll & show tooltip instead

---
+++ frontend-service/src/views/_components/Menu/_components/MenuItem/MenuItem.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react';
+import { Fragment } from 'react';
+import ReactTooltip from 'react-tooltip';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import isNil from 'lodash/isNil';
+
+import styles from './MenuItem.module.scss';
+
+import { AwesomeIcons } from 'conf/AwesomeIcons';
+
+export const MenuItem = ({ item }) => {
+  const ref = useRef(null);
+
+  const isEllipsisActive = () => ref.current?.offsetWidth < ref.current?.scrollWidth;
+
+  return (
+    <Fragment>
+      <li className={'p-menuitem'} data-for={item.label} data-tip>
+        <span
+          className={`p-menuitem-link ${item.disabled ? styles.menuItemDisabled : null} ${styles.menuItem}`}
+          disabled={item.disabled}
+          onClick={e => {
+            e.preventDefault();
+            if (!item.disabled) {
+              item.command();
+            } else {
+              this.setState(state => ({ ...state, menuClick: true }));
+            }
+          }}
+          ref={ref}>
+          {!isNil(item.icon) && <FontAwesomeIcon icon={AwesomeIcons(item.icon)} role="presentation" />}
+          <span>{item.label}</span>
+        </span>
+      </li>
+
+      {isEllipsisActive() && (
+        <ReactTooltip className={styles.tooltip} effect="solid" id={item.label} place="right">
+          {item.label}
+        </ReactTooltip>
+      )}
+    </Fragment>
+  );
+};
