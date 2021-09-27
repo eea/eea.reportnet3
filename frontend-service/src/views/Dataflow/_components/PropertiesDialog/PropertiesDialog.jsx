dfb8bca62da0d00536863c7194dc55fda9704b35
Arrastia <arrastia@pm.me>
2021-09-27 13:50:20 +0200

    UPDATE - PropertyItem component

--- frontend-service/src/views/Dataflow/_components/PropertiesDialog/PropertiesDialog.jsx
+++ frontend-service/src/views/Dataflow/_components/PropertiesDialog/PropertiesDialog.jsx
@@ -7,13 +7,10 @@ import styles from './PropertiesDialog.module.scss';
 
 import { Button } from 'views/_components/Button';
 import { Dialog } from 'views/_components/Dialog';
+import { PropertyItem } from './_components/PropertyItem';
 
 import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
 import { UserContext } from 'views/_functions/Contexts/UserContext';
-
-import { RodUrl } from 'repositories/config/RodUrl';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { AwesomeIcons } from 'conf/AwesomeIcons';
 
 export const PropertiesDialog = ({ dataflowState, manageDialogs }) => {
   const { description, isPropertiesDialogVisible, name, obligations, status } = dataflowState;
@@ -24,7 +21,6 @@ export const PropertiesDialog = ({ dataflowState, manageDialogs }) => {
   } = useContext(UserContext);
 
   const [dialogHeight, setDialogHeight] = useState(null);
-  const [isOpen, setIsOpen] = useState(true);
 
   const propertiesRef = useRef(null);
 
@@ -34,29 +30,23 @@ export const PropertiesDialog = ({ dataflowState, manageDialogs }) => {
     }
   }, [propertiesRef.current, isPropertiesDialogVisible]);
 
-  const parseObligations = () => {
-    return {
-      label: 'obligation',
-      data: {
-        comment: obligations.comment,
-        description: obligations.description,
-        id: obligations.obligationId,
-        nextReportDue: !isNil(obligations.expirationDate) ? dayjs(obligations.expirationDate).format(dateFormat) : '-',
-        reportingFrequency: obligations.reportingFrequency,
-        title: obligations.title
-      }
-    };
+  const getObligationsContent = () => {
+    const date = !isNil(obligations.expirationDate) ? dayjs(obligations.expirationDate).format(dateFormat) : '-';
+
+    return [
+      { id: 0, labelKey: 'title:', labelValue: obligations.title },
+      { id: 1, labelKey: 'description:', labelValue: obligations.description },
+      { id: 2, labelKey: 'comment:', labelValue: obligations.comment },
+      { id: 3, labelKey: 'nextReportDue:', labelValue: date },
+      { id: 5, labelKey: 'Frecuency:', labelValue: obligations.reportingFrequency },
+      { id: 4, labelKey: 'id:', labelValue: obligations.obligationId }
+    ];
   };
 
-  const parseLegalInstrument = () => {
-    return {
-      label: 'legalInstrument',
-      data: {
-        shortName: isNil(obligations.legalInstrument) ? '' : obligations.legalInstrument.alias,
-        legalName: isNil(obligations.legalInstrument) ? '' : obligations.legalInstrument.title
-      }
-    };
-  };
+  const parseLegalInstrument = () => ({
+    shortName: isNil(obligations.legalInstrument) ? '' : obligations.legalInstrument.alias,
+    legalName: isNil(obligations.legalInstrument) ? '' : obligations.legalInstrument.title
+  });
 
   const renderDialogFooter = (
     <Button
@@ -76,85 +66,22 @@ export const PropertiesDialog = ({ dataflowState, manageDialogs }) => {
         onHide={() => manageDialogs('isPropertiesDialogVisible', false)}
         visible={isPropertiesDialogVisible}>
         <div className={styles.propertiesWrap} ref={propertiesRef} style={{ height: dialogHeight }}>
-          <div style={{ marginTop: '1rem', marginBottom: '2rem' }}>
-            <h3 className={styles.title}>
-              <FontAwesomeIcon
-                className={styles.icon}
-                icon={AwesomeIcons(isOpen ? 'angleDown' : 'angleRight')}
-                onClick={() => setIsOpen(!isOpen)}
-              />
-              {resourcesContext.messages['dataflowDetails']}
-            </h3>
-            <div className={`${styles.content} ${isOpen ? '' : styles.hide}`}>
-              <span>
-                <strong>Dataflow name: </strong>
-                {name}
-              </span>
-              <span>
-                <strong>Dataflow description: </strong>
-                {description}
-              </span>
-              <span>
-                <strong>Dataflow status: </strong>
-                {status}
-              </span>
-            </div>
-          </div>
-          <div style={{ marginTop: '2rem', marginBottom: '1rem' }}>
-            <h3 className={styles.title}>
-              <FontAwesomeIcon
-                className={styles.icon}
-                icon={AwesomeIcons(isOpen ? 'angleDown' : 'angleRight')}
-                onClick={() => setIsOpen(!isOpen)}
-              />
-              {resourcesContext.messages['obligation']}
-            </h3>
-            <div className={`${styles.content}`}>
-              <span>
-                <strong>Title: </strong>
-                {parseObligations().data.title}
-              </span>
-              <span>
-                <strong>Description: </strong>
-                {parseObligations().data.description}
-              </span>
-              <span>
-                <strong>Comment: </strong>
-                {parseObligations().data.comment}
-              </span>
-              <span>
-                <strong>Next Report Due: </strong>
-                {parseObligations().data.nextReportDue}
-              </span>
-              <span>
-                <strong>Id: </strong>
-                {parseObligations().data.id}
-              </span>
-            </div>
-
-            <Button
-              className={'p-button-secondary-transparent'}
-              icon={'externalUrl'}
-              onMouseDown={() => window.open(`${RodUrl.obligations}${obligations.obligationId}`)}
-              tooltip={resourcesContext.messages['viewMore']}
-            />
-          </div>
-          <div style={{ marginTop: '2rem', marginBottom: '1rem' }}>
-            <h3 className={styles.title}>
-              <FontAwesomeIcon
-                className={styles.icon}
-                icon={AwesomeIcons(isOpen ? 'angleDown' : 'angleRight')}
-                onClick={() => setIsOpen(!isOpen)}
-              />
-              {resourcesContext.messages['obligation']}
-            </h3>
-            <Button
-              className={'p-button-secondary-transparent'}
-              icon={'externalUrl'}
-              onMouseDown={() => window.open(`${RodUrl.instruments}${obligations.legalInstrument.id}`)}
-              tooltip={resourcesContext.messages['viewMore']}
-            />
-          </div>
+          <PropertyItem
+            content={[
+              { id: 0, labelKey: 'Dataflow name:', labelValue: name },
+              { id: 1, labelKey: 'Dataflow description:', labelValue: description },
+              { id: 2, labelKey: 'Dataflow status:', labelValue: status }
+            ]}
+            title={resourcesContext.messages['dataflowDetails']}
+          />
+          <PropertyItem content={getObligationsContent()} title={resourcesContext.messages['obligation']} />
+          <PropertyItem
+            content={[
+              { id: 0, labelKey: 'Short name: ', labelValue: parseLegalInstrument().shortName },
+              { id: 1, labelKey: 'Legal name: ', labelValue: parseLegalInstrument().legalName }
+            ]}
+            title={resourcesContext.messages['legalInstrument']}
+          />
         </div>
       </Dialog>
     )
--- frontend-service/src/views/Dataflow/_components/PropertiesDialog/_components/PropertyItem/PropertyItem.jsx
+++ frontend-service/src/views/Dataflow/_components/PropertiesDialog/_components/PropertyItem/PropertyItem.jsx
@@ -1,30 +1,66 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { AwesomeIcons } from 'conf/AwesomeIcons';
 
-import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
-
 import styles from './PropertyItem.module.scss';
 
-export const PropertyItem = ({ onRedirect, title }) => {
+import { Button } from 'views/_components/Button';
+
+import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
+
+import { RodUrl } from 'repositories/config/RodUrl';
+
+export const PropertyItem = ({ content, title }) => {
   const resourcesContext = useContext(ResourcesContext);
 
   const [isOpen, setIsOpen] = useState(true);
 
+  const onToggleVisibility = () => setIsOpen(prevState => !prevState);
+
   return (
-    <div>
-      <div className={styles.titleWrapper}>
-        <h3 className={styles.title}>
-          <FontAwesomeIcon
-            className={styles.icon}
-            icon={AwesomeIcons(isOpen ? 'angleDown' : 'angleRight')}
-            onClick={() => setIsOpen(!isOpen)}
-          />
-          {title}
-        </h3>
+    <div style={{ marginTop: '1rem', marginBottom: '2rem' }}>
+      <h3 className={styles.title}>
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={AwesomeIcons(isOpen ? 'angleDown' : 'angleRight')}
+          onClick={onToggleVisibility}
+        />
+        {title}
+
+        {/* <Button
+              className={'p-button-secondary-transparent'}
+              icon={'externalUrl'}
+              onMouseDown={() => window.open(`${RodUrl.obligations}${obligations.obligationId}`)}
+              tooltip={resourcesContext.messages['viewMore']}
+            /> */}
+      </h3>
+      <div className={`${styles.content} ${isOpen ? '' : styles.hide}`}>
+        {content.map(item => (
+          <span key={item.id}>
+            <strong>{item.labelKey}</strong>
+            {item.labelValue || '-'}
+          </span>
+        ))}
       </div>
     </div>
   );
+};
+
+PropertyItem.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      labelKey: PropTypes.string.isRequired,
+      labelValue: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
+
+PropertyItem.defaultProps = {
+  content: [],
+  title: ''
 };
