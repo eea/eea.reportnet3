e12f09173dbabf20c00e466fa49db1d13fced2e3
Carlos Felipe Parra Canto <cfparra-ext@tracasa.es>
2021-09-29 11:19:58 +0200

    UPDATE - Character counter styles

--- frontend-service/src/views/_components/CharacterCounter/CharacterCounter.jsx
+++ frontend-service/src/views/_components/CharacterCounter/CharacterCounter.jsx
@@ -3,17 +3,18 @@ import isNil from 'lodash/isNil';
 import styles from './CharacterCounter.module.scss';
 
 const CharacterCounter = ({ currentLength, inputRef, maxLength, style }) => {
+  const getCounterClassName = () => {
+    if (isNil(maxLength)) return '';
+
+    return currentLength > maxLength
+      ? styles.errorCharacterCount
+      : maxLength - currentLength <= 10
+      ? styles.warningCharacterCount
+      : '';
+  };
+
   return (
-    <p
-      className={`${styles.characterCount} ${
-        currentLength > maxLength
-          ? styles.errorCharacterCount
-          : maxLength - currentLength <= 10
-          ? styles.warningCharacterCount
-          : ''
-      }`}
-      ref={inputRef}
-      style={style}>
+    <p className={`${styles.characterCount} ${getCounterClassName()}`} ref={inputRef} style={style}>
       {isNil(maxLength) ? `${currentLength}` : `${currentLength}/${maxLength}`}
     </p>
   );
