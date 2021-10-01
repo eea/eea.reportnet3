3052c417586ac25a66657ba12a17a6d7f411c56e
igor-co <icojocaru-ext@tracasa.es>
2021-10-01 12:00:31 +0200

    Add dropdown

--- frontend-service/src/views/_components/TimezoneCalendar/TimezoneCalendar.jsx
+++ frontend-service/src/views/_components/TimezoneCalendar/TimezoneCalendar.jsx
@@ -7,10 +7,14 @@ import utc from 'dayjs/plugin/utc';
 import { Button } from 'views/_components/Button';
 import { Calendar } from 'primereact/calendar';
 import { Dropdown } from 'views/_components/Dropdown';
-import { InputMask } from 'views/_components/InputMask';
+// import { InputMask } from 'views/_components/InputMask';
+import { InputMask } from 'primereact/inputmask';
 import { InputText } from 'views/_components/InputText';
 
+import timezones from 'timezones-list';
+
 import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
+import { uniqBy } from 'lodash';
 
 export const TimezoneCalendar = ({ onSaveDate = () => {} }) => {
   const resourcesContext = useContext(ResourcesContext);
@@ -18,7 +22,14 @@ export const TimezoneCalendar = ({ onSaveDate = () => {} }) => {
   dayjs.extend(timezone);
 
   const [date, setDate] = useState(new Date());
-  console.log(dayjs('2014-06-01 12:00').tz('America/New_York'));
+  const [inputValue, setInputValue] = useState('');
+  const [selectedTimeZone, setSelectedTimeZone] = useState({ utcOffset: '+00:00', tzCode: 'Africa/Abidjan' });
+
+  const getUtcOffsets = () => {
+    let res = timezones.map(zone => ({ utcOffset: zone.utc, tzCode: zone.tzCode }));
+    return uniqBy(res, 'utcOffset');
+  };
+
   const renderButtons = () => {
     return (
       <div>
@@ -34,14 +45,35 @@ export const TimezoneCalendar = ({ onSaveDate = () => {} }) => {
   };
 
   const renderCalendar = () => {
-    return <Calendar inline={true} onChange={e => setDate(e.value)} showWeek={true} value={date} />;
+    return (
+      <Calendar inline monthNavigator onChange={e => setDate(e.value)} showWeek value={date} yearRange="1900:2100" />
+    );
   };
 
   const renderInput = () => {
     return <InputText onChange={e => setDate(new Date(e.target.value))} value={date} />;
   };
   const renderInputMask = () => {
-    return <InputMask onChange={e => setDate(new Date(e.target.value))} value={date} />;
+    return (
+      <InputMask
+        onBlur={e => setDate(new Date(inputValue))}
+        onChange={e => setInputValue(e.target.value)}
+        mask={`99-9999-99 99:99:99`}
+        value={date}
+      />
+    );
+  };
+  const renderDropdown = () => {
+    return (
+      <Dropdown
+        onChange={e => setSelectedTimeZone(e.value)}
+        optionLabel="utcOffset"
+        optionValue="tzCode"
+        options={getUtcOffsets()}
+        placeholder="Select GMT offset"
+        value={selectedTimeZone}
+      />
+    );
   };
 
   const renderTimezoneDropdown = () => {};
@@ -49,6 +81,8 @@ export const TimezoneCalendar = ({ onSaveDate = () => {} }) => {
   return (
     <div>
       {renderCalendar()}
+      {renderInputMask()}
+      {renderDropdown()}
       {renderInput()}
       {renderTimezoneDropdown()}
       {renderButtons()}
