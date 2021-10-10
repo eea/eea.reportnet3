bfbebb5448d5f263106a9772815cbb20f488e53e
igor-co <icojocaru-ext@tracasa.es>
2021-10-10 13:11:20 +0200

    Fix disable save button

--- frontend-service/src/views/_components/TimezoneCalendar/TimezoneCalendar.jsx
+++ frontend-service/src/views/_components/TimezoneCalendar/TimezoneCalendar.jsx
@@ -84,7 +84,7 @@ export const TimezoneCalendar = ({ onSaveDate = () => {}, value, isInModal, isDi
       <div className={styles.buttonWrapper}>
         <Button
           className="p-button p-component p-button-primary p-button-animated-blink p-button-text-icon-left"
-          disabled={!dayjs(new Date(date)).isValid() || hasError}
+          disabled={!dayjs(parseDate(date)).isValid() || hasError}
           icon="save"
           label={resourcesContext.messages['save']}
           onClick={() => onSaveDate(dayjs.utc(parseDate(date)).utcOffset(selectedOffset.value))}
