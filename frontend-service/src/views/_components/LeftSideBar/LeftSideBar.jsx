447b2111212705e3c1e4ff17bdb12dbaf71bc9d5
Carlos Felipe Parra Canto <cfparra-ext@tracasa.es>
2021-11-03 13:14:57 +0100

    FIX - Joyride re-use

--- frontend-service/src/views/_components/LeftSideBar/LeftSideBar.jsx
+++ frontend-service/src/views/_components/LeftSideBar/LeftSideBar.jsx
@@ -30,14 +30,14 @@ const LeftSideBar = withRouter(({ history, setIsNotificationVisible }) => {
   const [run, setRun] = useState(false);
 
   const handleJoyrideCallback = data => {
-    const { action, status, type } = data;
+    const { action, index, status, type } = data;
 
     if ([ACTIONS.CLOSE].includes(action) || [STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
       setHelpIndex(0);
       setRun(false);
     } else {
       if ([EVENTS.STEP_AFTER, EVENTS.TARGET_NOT_FOUND].includes(type)) {
-        setHelpIndex(helpIndex + (data.action === 'prev' ? -1 : 1));
+        setHelpIndex(index + (action === ACTIONS.PREV ? -1 : 1));
       } else {
         if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
           setRun(false);
