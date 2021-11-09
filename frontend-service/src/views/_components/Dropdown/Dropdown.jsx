739d396445d192db21ae3533c26e8c3695581221
igor-co <icojocaru-ext@tracasa.es>
2021-11-09 08:37:28 +0100

    Fix error on up button press

--- frontend-service/src/views/_components/Dropdown/Dropdown.jsx
+++ frontend-service/src/views/_components/Dropdown/Dropdown.jsx
@@ -189,13 +189,16 @@ export class Dropdown extends Component {
   onUpKey(event) {
     if (this.props.options) {
       let selectedItemIndex = this.findOptionIndex(this.props.value);
-      let prevItem = this.findPrevVisibleItem(selectedItemIndex);
 
-      if (prevItem) {
-        this.selectItem({
-          originalEvent: event,
-          option: prevItem
-        });
+      if (selectedItemIndex !== -1) {
+        let prevItem = this.findPrevVisibleItem(selectedItemIndex);
+
+        if (prevItem) {
+          this.selectItem({
+            originalEvent: event,
+            option: prevItem
+          });
+        }
       }
     }
 
