cefbfc6b7304921173150f44829a39715885e4c4
Michael Coloma Calva <mcoloma-ext@outlook.es>
2021-11-16 10:00:07 +0100

    Prettier effect is applied

--- frontend-service/src/views/_components/GoTopButton/GoTopButton.jsx
+++ frontend-service/src/views/_components/GoTopButton/GoTopButton.jsx
@@ -5,31 +5,28 @@ import styles from './GoTopButton.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { AwesomeIcons } from 'conf/AwesomeIcons';
 
-
 const GoTopButton = ({ visibleSinceHeight }) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-    const [scrollPosition, setScrollPosition] = useState(0)
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
 
-    const handleScroll = () => {
-        const position = window.pageYOffset;
-        setScrollPosition(position);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
+  }, [scrollPosition]);
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-
-    }, [scrollPosition])
-
-    return (
-        scrollPosition > visibleSinceHeight  && 
-        <div className={`${styles.goTopButton}`} onClick={() => window.scrollTo(0, 0)}>
-            <FontAwesomeIcon icon={AwesomeIcons('angleUp')} />
-        </div>
+  return (
+    scrollPosition > visibleSinceHeight && (
+      <div className={`${styles.goTopButton}`} onClick={() => window.scrollTo(0, 0)}>
+        <FontAwesomeIcon icon={AwesomeIcons('angleUp')} />
+      </div>
     )
-}
+  );
+};
 
 export { GoTopButton };
-
