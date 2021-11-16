8b457aa773eb1b434a50f075566d36db8e262402
Miriam Gaviria <mgaviria-ext@tracasa.es>
2021-11-16 13:45:44 +0100

    Fix - FE error notification when users list is downloaded

--- frontend-service/src/views/_components/Layout/MainLayout/_components/GlobalNotifications/GlobalNotifications.jsx
+++ frontend-service/src/views/_components/Layout/MainLayout/_components/GlobalNotifications/GlobalNotifications.jsx
@@ -98,7 +98,7 @@ const GlobalNotifications = () => {
       }
     } catch (error) {
       console.error('GlobalNotifications - downloadUsersListFile.', error);
-      notificationContext.add({ type: 'DOWNLOAD_QC_RULES_FILE_ERROR' });
+      notificationContext.add({ type: 'DOWNLOAD_USERS_LIST_FILE_ERROR' });
     } finally {
       notificationContext.clearHiddenNotifications();
     }
