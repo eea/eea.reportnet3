7bbd928ebbb05ca83b7755f6a104ce10a0e5cf94
cristian.martinez <cristian.martinez@altia.es>
2021-11-17 13:42:12 +0100

    Added underscore to variables

--- database/src/main/resources/db/migration/V56__alter_table_temp_user.sql
+++ database/src/main/resources/db/migration/V56__alter_table_temp_user.sql
@@ -1,1 +1,4 @@
-ALTER TABLE public.temp_user ADD COLUMN IF NOT EXISTS dataproviderid int8 NULL;
+ALTER TABLE public.temp_user ADD COLUMN IF NOT EXISTS data_provider_id int8 NULL;
+ALTER TABLE public.temp_user RENAME COLUMN usertype TO user_type;
+ALTER TABLE public.temp_user RENAME COLUMN dataflowid TO dataflow_id;
+
