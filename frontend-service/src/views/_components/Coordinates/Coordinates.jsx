881a76c7da4b9d16a7251fdab3744a90d1234148
Kiko <fjlabiano@itracasa.es>
2021-11-23 20:46:55 +0100

    update

--- frontend-service/src/views/_components/Coordinates/Coordinates.jsx
+++ frontend-service/src/views/_components/Coordinates/Coordinates.jsx
@@ -2,11 +2,14 @@ import { useContext, useEffect, useState } from 'react';
 import isNil from 'lodash/isNil';
 import classNames from 'classnames';
 
+import { config } from 'conf';
+
 import styles from './Coordinates.module.scss';
 
 import { Button } from 'views/_components/Button';
 import { Dropdown } from 'views/_components/Dropdown';
 import { InputText } from 'views/_components/InputText';
+import { TooltipButton } from 'views/_components/TooltipButton';
 
 import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
 
@@ -44,18 +47,61 @@ export const Coordinates = ({
   const checkCoordinates = (lat, long) => {
     setHasErrors({
       latitude: checkEmptyCoordinate(lat),
-      longitude: checkEmptyCoordinate(long)
+      longitude: checkEmptyCoordinate(long),
+      latOutOfBounds: !inBounds(lat, 'latitude'),
+      longOutOfBounds: !inBounds(long, 'longitude')
     });
   };
 
   const checkEmptyCoordinate = coord => (!isNil(coord) ? coord.toString().trim() === '' : true);
 
+  const inBounds = (coord, coordType) => {
+    console.log(crsValue);
+    const parsedCoord = parseFloat(coord) || 0;
+    if (crsValue.value !== 'EPSG:3035') {
+      if (coordType === 'latitude') {
+        console.log(parsedCoord, config.MIN_GEOGRAPHICAL_LAT_COORD, config.MAX_GEOGRAPHICAL_LAT_COORD);
+        return parsedCoord >= config.MIN_GEOGRAPHICAL_LAT_COORD && parsedCoord <= config.MAX_GEOGRAPHICAL_LAT_COORD;
+      } else {
+        console.log(parsedCoord, config.MIN_GEOGRAPHICAL_LONG_COORD, config.MAX_GEOGRAPHICAL_LONG_COORD);
+        return parsedCoord >= config.MIN_GEOGRAPHICAL_LONG_COORD && parsedCoord <= config.MAX_GEOGRAPHICAL_LONG_COORD;
+      }
+    } else {
+      if (coordType === 'latitude') {
+        return parsedCoord >= config.MIN_METRICAL_X_COORD && parsedCoord <= config.MAX_METRICAL_X_COORD;
+      } else {
+        return parsedCoord >= config.MIN_METRICAL_Y_COORD && parsedCoord <= config.MAX_METRICAL_Y_COORD;
+      }
+    }
+  };
+
+  const renderLabel = keys => {
+    if (!xyLabels) {
+      return resourcesContext.messages[isCellEditor ? keys.geographicalShort : keys.geographical];
+    } else {
+      return resourcesContext.messages[keys.metrical];
+    }
+  };
+
+  const renderTooltipButtonMessage = () => {
+    if (!xyLabels) {
+      return `${resourcesContext.messages['coordsOutOfBoundsTooltip']} ${resourcesContext.messages['coordsOutOfBoundsTooltipGeographical']}`;
+    } else {
+      return `${resourcesContext.messages['coordsOutOfBoundsTooltip']} ${resourcesContext.messages['coordsOutOfBoundsTooltipMetrical']}`;
+    }
+  };
+
   return (
     <div>
       <div className={styles.coordinatesWrapper}>
-        <label className={styles.epsg}>{resourcesContext.messages[!xyLabels ? 'latitude' : 'x']}:</label>
+        <label className={styles.epsg}>
+          {renderLabel({ geographical: 'latitude', geographicalShort: 'lat', metrical: 'x' })}:
+        </label>
         <InputText
-          className={classNames({ [styles.error]: hasErrors.latitude, [styles.isCellEditor]: isCellEditor })}
+          className={classNames({
+            [styles.error]: hasErrors.latitude || hasErrors.latOutOfBounds,
+            [styles.isCellEditor]: isCellEditor
+          })}
           disabled={disabled}
           id={`${id}_lat`}
           keyfilter="coordinates"
@@ -63,7 +109,7 @@ export const Coordinates = ({
             onBlur([e.target.value, longitude].join(', '));
             checkCoordinates(e.target.value, longitude);
           }}
-          onChange={e => setLatitude(e.target.value)}
+          onChange={event => setLatitude(event.target.value)}
           onFocus={e => {
             e.preventDefault();
             onFocus();
@@ -73,10 +119,13 @@ export const Coordinates = ({
           value={latitude}
         />
         <label className={classNames(styles.epsg, styles.longitude)}>
-          {resourcesContext.messages[!xyLabels ? 'longitude' : 'y']}:
+          {renderLabel({ geographical: 'longitude', geographicalShort: 'long', metrical: 'y' })}:
         </label>
         <InputText
-          className={classNames({ [styles.error]: hasErrors.longitude, [styles.isCellEditor]: isCellEditor })}
+          className={classNames({
+            [styles.error]: hasErrors.longitude || hasErrors.longOutOfBounds,
+            [styles.isCellEditor]: isCellEditor
+          })}
           disabled={disabled}
           id={`${id}_long`}
           keyfilter="coordinates"
@@ -84,7 +133,7 @@ export const Coordinates = ({
             onBlur([latitude, e.target.value].join(', '));
             checkCoordinates(latitude, e.target.value);
           }}
-          onChange={e => setLongitude(e.target.value)}
+          onChange={event => setLongitude(event.target.value)}
           onFocus={e => {
             e.preventDefault();
             onFocus();
@@ -96,7 +145,13 @@ export const Coordinates = ({
       </div>
       {hasErrorMessage && (
         <p className={styles.errorMessage}>
-          {showMessageError && <span className={styles.pointError}>{errorMessage}</span>}
+          {showMessageError && <span className={styles.pointError}>{errorMessage}. </span>}
+          {(hasErrors.latOutOfBounds || hasErrors.longOutOfBounds) && (
+            <span>
+              <span className={styles.pointError}>{resourcesContext.messages['coordsOutOfBounds']}</span>
+              <TooltipButton message={renderTooltipButtonMessage()} />
+            </span>
+          )}
         </p>
       )}
       <div className={`${!isCellEditor ? styles.pointEpsgWrapper : ''}`}>
@@ -105,7 +160,7 @@ export const Coordinates = ({
           appendTo={document.body}
           ariaLabel={'crs'}
           className={styles.epsgSwitcher}
-          disabled={crsDisabled}
+          disabled={crsDisabled || hasErrors.latOutOfBounds || hasErrors.longOutOfBounds}
           onChange={e => onCrsChange(e.target.value)}
           optionLabel="label"
           options={crsOptions}
@@ -114,7 +169,7 @@ export const Coordinates = ({
         />
         <Button
           className={`p-button-secondary-transparent button ${styles.mapButton}`}
-          disabled={false}
+          disabled={hasErrors.latOutOfBounds || hasErrors.longOutOfBounds}
           icon="marker"
           onClick={() => onMapOpen(initialGeoJson)}
           tooltip={resourcesContext.messages['selectGeographicalDataOnMap']}
