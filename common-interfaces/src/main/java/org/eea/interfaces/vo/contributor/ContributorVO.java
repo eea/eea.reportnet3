1cd9435d488c494731ba6f4ea82b364593824143
cristian.martinez <cristian.martinez@altia.es>
2021-11-24 11:43:27 +0100

    Added Eclipse Equals and Hashcode to ContributorVO

--- common-interfaces/src/main/java/org/eea/interfaces/vo/contributor/ContributorVO.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/contributor/ContributorVO.java
@@ -1,7 +1,6 @@
 package org.eea.interfaces.vo.contributor;
 
 import java.io.Serializable;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,7 +12,6 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode
 public class ContributorVO implements Serializable {
 
   /**
@@ -33,4 +31,43 @@ public class ContributorVO implements Serializable {
   /** Is the contributor temporary?. */
   private boolean invalid;
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((account == null) ? 0 : account.hashCode());
+    result = prime * result + ((dataProviderId == null) ? 0 : dataProviderId.hashCode());
+    result = prime * result + (invalid ? 1231 : 1237);
+    result = prime * result + ((role == null) ? 0 : role.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ContributorVO other = (ContributorVO) obj;
+    if (account == null) {
+      if (other.account != null)
+        return false;
+    } else if (!account.equals(other.account))
+      return false;
+    if (dataProviderId == null) {
+      if (other.dataProviderId != null)
+        return false;
+    } else if (!dataProviderId.equals(other.dataProviderId))
+      return false;
+    if (invalid != other.invalid)
+      return false;
+    if (role == null) {
+      if (other.role != null)
+        return false;
+    } else if (!role.equals(other.role))
+      return false;
+    return true;
+  }
 }
