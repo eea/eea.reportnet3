1971c274fd3fd104403abd2370f4aa16bb2323d8
Eduardo <ealfaro@tracasa.es>
2021-12-01 14:04:40 +0100

    Fix empty line and export

--- frontend-service/src/views/_components/ScrollToTop/ScrollToTop.jsx
+++ frontend-service/src/views/_components/ScrollToTop/ScrollToTop.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => {
+export const ScrollToTop = () => {
   const { pathname } = useLocation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
 };
-
-export { ScrollToTop };
