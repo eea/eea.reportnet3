fe5672699efbdddb232e92a8cda0df713815b1da
igor-co <icojocaru-ext@tracasa.es>
2021-12-13 15:19:53 +0100

    Add Sql sentence validation component

---
+++ frontend-service/src/views/DatasetDesigner/_components/Validations/_components/SqlSentence/_components/SqlSentenceValidation/SqlSentenceValidation.jsx
@@ -0,0 +1,101 @@
+import { useContext, useEffect, useState } from 'react';
+
+import styles from './SqlSentenceValidation.module.scss';
+
+import { useParams } from 'react-router-dom';
+
+import { Button } from 'views/_components/Button';
+import { Column } from 'primereact/column';
+import { DataTable } from 'views/_components/DataTable';
+import { Dialog } from 'views/_components/Dialog';
+import { Spinner } from 'views/_components/Spinner';
+
+import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
+
+import { ValidationService } from 'services/ValidationService';
+
+export const SqlSentenceValidation = ({
+  isVisibleSqlSentenceValidationDialog,
+  setIsVisibleSqlSentenceValidationDialog,
+  sqlSentence
+}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [sqlResponse, setSqlResponse] = useState();
+  const [columns, setColumns] = useState();
+
+  const resourcesContext = useContext(ResourcesContext);
+
+  const { datasetId } = useParams();
+
+  useEffect(() => {
+    validateSqlSentence();
+  }, []);
+
+  useEffect(() => {
+    if (sqlResponse) {
+      setColumns(generateColumns());
+    }
+  }, [sqlResponse]);
+
+  const generateColumns = () => {
+    const [firstRow] = sqlResponse;
+
+    const columnData = Object.keys(firstRow).map(key => ({ field: key, header: key.replace('*', '.') }));
+
+    return columnData.map(col => <Column field={col.field} header={col.header} key={col.field} />);
+  };
+
+  const validateSqlSentence = async () => {
+    setIsLoading(true);
+    try {
+      const showInternalFields = true;
+      const response = await ValidationService.runSqlRule(datasetId, sqlSentence, showInternalFields);
+
+      setSqlResponse(response);
+    } catch (error) {
+      console.error('SqlSentenceValidation - validateSqlSentence.', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const footer = (
+    <Button
+      className="p-button-secondary p-button-right-aligned"
+      icon={'cancel'}
+      label={resourcesContext.messages['close']}
+      onClick={() => setIsVisibleSqlSentenceValidationDialog(false)}
+    />
+  );
+  const renderTable = () => {
+    return (
+      <DataTable paginator={false} value={sqlResponse}>
+        {columns}
+      </DataTable>
+    );
+  };
+
+  const renderDialogContent = () => {
+    if (sqlResponse) {
+      return renderTable();
+    } else {
+      return (
+        <div className={styles.errorMessageWrapper}>
+          <p>{resourcesContext.messages['notValidSql']}</p>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <Dialog
+      contentStyle={{ height: '78vh' }}
+      style={{ width: '90%' }}
+      footer={footer}
+      header={resourcesContext.messages['sqlSentenceValidationDialogTitle']}
+      onHide={() => setIsVisibleSqlSentenceValidationDialog(false)}
+      visible={isVisibleSqlSentenceValidationDialog}>
+      {isLoading ? <Spinner /> : renderDialogContent()}
+    </Dialog>
+  );
+};
