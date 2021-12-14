3828de213ca1f2c20ad00a0cc3846d0a3209b1a2
sandra.laguna <slaguna-ext@tracasa.es>
2021-12-14 09:50:58 +0100

    change reference dataset list message

--- frontend-service/src/views/ReferenceDataflow/_components/ReferencingDataflows/ReferencingDataflows.jsx
+++ frontend-service/src/views/ReferenceDataflow/_components/ReferencingDataflows/ReferencingDataflows.jsx
@@ -70,6 +70,7 @@ const ReferencingDataflows = ({ referenceDataflowId }) => {
     return renderDialogLayout(
       <div className={styles.noReferencingWrap}>
         <h3>{resourcesContext.messages['noReferencingDataflows']}</h3>
+        <h3>{resourcesContext.messages['noReferencingDataflows2']}</h3>
       </div>
     );
   }
