031e4b6b8ca54d1880d76eda288bd9d08c34f0dc
Michael Coloma Calva <mcoloma-ext@outlook.es>
2021-12-16 09:53:22 +0100

    UPDATE to improve code in applyFiltersUtils

--- frontend-service/src/views/_components/MyFilters/_functions/Utils/ApplyFiltersUtils.js
+++ frontend-service/src/views/_components/MyFilters/_functions/Utils/ApplyFiltersUtils.js
@@ -6,30 +6,20 @@ import { TextUtils } from 'repositories/_utils/TextUtils';
 const { areEquals } = TextUtils;
 
 const getEndOfDay = date => new Date(dayjs(date).endOf('day').format()).getTime();
-
 const getStartOfDay = date => new Date(dayjs(date).startOf('day').format()).getTime();
 
 const applyDates = ({ filterBy, filterByKeys, item }) => {
-  const checkResultDates = (previousValue, currentValue) => previousValue && currentValue;
   const filteredKeys = filterByKeys.DATE.filter(key => Object.keys(filterBy).includes(key));
 
-  let result = [];
-
-  for (let index = 0; index < filteredKeys.length; index++) {
-    const filteredKey = filteredKeys[index];
-    const dates = filterBy[filteredKey];
-    const value = new Date(item[filteredKey]).getTime();
-
-    if (dates[0] && !dates[1]) result.push(value >= getStartOfDay(dates[0]) && getEndOfDay(dates[0]) >= value);
-
-    if (dates[0] && dates[1]) result.push(value >= getStartOfDay(dates[0]) && getEndOfDay(dates[1]) >= value);
-  }
-
-  if (result.length > 0) {
-    return result.reduce(checkResultDates);
-  }
-
-  return true;
+  return filteredKeys
+    .map(filteredKey => {
+      const dates = filterBy[filteredKey];
+      const value = new Date(item[filteredKey]).getTime();
+      if (dates[0] && !dates[1]) return value >= getStartOfDay(dates[0]) && getEndOfDay(dates[0]) >= value;
+      else if (dates[0] && dates[1]) return value >= getStartOfDay(dates[0]) && getEndOfDay(dates[1]) >= value;
+      else return true;
+    })
+    .reduce((previousValue, currentValue) => previousValue && currentValue);
 };
 
 const applyInputs = ({ filterBy, filterByKeys, item }) => {
