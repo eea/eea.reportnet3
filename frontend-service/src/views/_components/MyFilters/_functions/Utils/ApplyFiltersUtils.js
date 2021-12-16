1920255f572e2368a63fd3c65cf8857a66df1dc3
Michael Coloma Calva <mcoloma-ext@outlook.es>
2021-12-16 08:39:29 +0100

    UPDATE to fix error when there're somes dates together in the filter

--- frontend-service/src/views/_components/MyFilters/_functions/Utils/ApplyFiltersUtils.js
+++ frontend-service/src/views/_components/MyFilters/_functions/Utils/ApplyFiltersUtils.js
@@ -10,16 +10,23 @@ const getEndOfDay = date => new Date(dayjs(date).endOf('day').format()).getTime(
 const getStartOfDay = date => new Date(dayjs(date).startOf('day').format()).getTime();
 
 const applyDates = ({ filterBy, filterByKeys, item }) => {
+  const checkResultDates = (previousValue, currentValue) => previousValue && currentValue;
   const filteredKeys = filterByKeys.DATE.filter(key => Object.keys(filterBy).includes(key));
+
+  let result = [];
 
   for (let index = 0; index < filteredKeys.length; index++) {
     const filteredKey = filteredKeys[index];
     const dates = filterBy[filteredKey];
     const value = new Date(item[filteredKey]).getTime();
 
-    if (dates[0] && !dates[1]) return value >= getStartOfDay(dates[0]) && getEndOfDay(dates[0]) >= value;
+    if (dates[0] && !dates[1]) result.push(value >= getStartOfDay(dates[0]) && getEndOfDay(dates[0]) >= value);
 
-    if (dates[0] && dates[1]) return value >= getStartOfDay(dates[0]) && getEndOfDay(dates[1]) >= value;
+    if (dates[0] && dates[1]) result = result.push(value >= getStartOfDay(dates[0]) && getEndOfDay(dates[1]) >= value);
+  }
+
+  if (result.length > 0) {
+    return result.reduce(checkResultDates);
   }
 
   return true;
