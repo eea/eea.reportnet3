5ec94e0f73d04217397850b0635a4502c2aac827
ruben.lozano <ruben.lozano@altia.es>
2022-01-03 12:43:38 +0100

    ADDED new access

--- common-utitlities/src/main/java/org/eea/security/jwt/utils/EntityAccessService.java
+++ common-utitlities/src/main/java/org/eea/security/jwt/utils/EntityAccessService.java
@@ -1,11 +1,24 @@
 package org.eea.security.jwt.utils;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.eea.exception.EEAException;
 import org.eea.interfaces.controller.dataflow.DataFlowController.DataFlowControllerZuul;
+import org.eea.interfaces.controller.dataset.DatasetMetabaseController.DataSetMetabaseControllerZuul;
+import org.eea.interfaces.controller.ums.UserManagementController.UserManagementControllerZull;
 import org.eea.interfaces.vo.dataflow.enums.TypeDataflowEnum;
 import org.eea.interfaces.vo.enums.EntityClassEnum;
+import org.eea.interfaces.vo.ums.ResourceAccessVO;
+import org.eea.security.authorization.ObjectAccessRoleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import feign.FeignException;
 
 
 
@@ -19,6 +32,12 @@ public class EntityAccessService {
   /** The dataflow controller zuul. */
   @Autowired
   private DataFlowControllerZuul dataflowControllerZuul;
+
+  @Autowired
+  private DataSetMetabaseControllerZuul datasetMetabaseControllerZuul;
+
+  @Autowired
+  private UserManagementControllerZull userManagementControllerZull;
 
   /**
    * Checks if is reference dataflow draft.
@@ -47,5 +66,84 @@ public class EntityAccessService {
     return dataflowControllerZuul.accessEntity(dataflowType, entity, entityId);
   }
 
+
+  /**
+   * Access super user.
+   *
+   * @param entity the entity
+   * @param entityId the entity id
+   * @return true, if successful
+   * @throws EEAException
+   */
+  public boolean accessSuperUser(EntityClassEnum entity, Long entityId) {
+    boolean result = false;
+    if (EntityClassEnum.DATASET.equals(entity)) {
+      List<ObjectAccessRoleEnum> objectAccessRoles = List.of(ObjectAccessRoleEnum.DATASET_STEWARD,
+          ObjectAccessRoleEnum.DATASET_CUSTODIAN, ObjectAccessRoleEnum.DATASET_OBSERVER,
+          ObjectAccessRoleEnum.DATASET_LEAD_REPORTER, ObjectAccessRoleEnum.DATASET_REPORTER_READ,
+          ObjectAccessRoleEnum.DATASET_REPORTER_WRITE, ObjectAccessRoleEnum.DATASET_OBSERVER,
+          ObjectAccessRoleEnum.DATASET_NATIONAL_COORDINATOR,
+          ObjectAccessRoleEnum.DATASCHEMA_CUSTODIAN, ObjectAccessRoleEnum.DATASCHEMA_STEWARD,
+          ObjectAccessRoleEnum.DATASCHEMA_EDITOR_READ, ObjectAccessRoleEnum.DATASCHEMA_EDITOR_WRITE,
+          ObjectAccessRoleEnum.TESTDATASET_CUSTODIAN, ObjectAccessRoleEnum.TESTDATASET_STEWARD,
+          ObjectAccessRoleEnum.REFERENCEDATASET_OBSERVER,
+          ObjectAccessRoleEnum.REFERENCEDATASET_CUSTODIAN,
+          ObjectAccessRoleEnum.REFERENCEDATASET_STEWARD, ObjectAccessRoleEnum.EUDATASET_CUSTODIAN,
+          ObjectAccessRoleEnum.EUDATASET_STEWARD, ObjectAccessRoleEnum.DATACOLLECTION_CUSTODIAN,
+          ObjectAccessRoleEnum.DATACOLLECTION_STEWARD,
+          ObjectAccessRoleEnum.DATASET_NATIONAL_COORDINATOR);
+      result = (canAccess(entityId, objectAccessRoles.toArray(ObjectAccessRoleEnum[]::new))
+          || dataflowControllerZuul.accessReferenceEntity(entity, entityId));
+    } else {
+      result = canAccess(entityId,
+          List.of(ObjectAccessRoleEnum.DATAFLOW_CUSTODIAN, ObjectAccessRoleEnum.DATAFLOW_OBSERVER,
+              ObjectAccessRoleEnum.DATAFLOW_STEWARD,
+              ObjectAccessRoleEnum.DATAFLOW_NATIONAL_COORDINATOR)
+              .toArray(ObjectAccessRoleEnum[]::new))
+          || dataflowControllerZuul.accessReferenceEntity(entity, entityId);
+    }
+    return result;
+  }
+
+  /**
+   * Can access.
+   *
+   * @param idEntity the id entity
+   * @param objectAccessRoles the object access roles
+   * @return true, if successful
+   * @throws EEAException
+   */
+  private boolean canAccess(Long idEntity, ObjectAccessRoleEnum... objectAccessRoles) {
+    boolean canAccess = false;
+    Collection<String> authorities =
+        SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+            .map(authority -> authority.getAuthority()).collect(Collectors.toList());
+    List<String> roles = Arrays.asList(objectAccessRoles).stream()
+        .map(objectAccessRoleEnum -> objectAccessRoleEnum.getAccessRole(idEntity))
+        .collect(Collectors.toList());
+
+    canAccess = roles.stream().anyMatch(authorities::contains);
+    // No authority found in the current token. Check against keycloak to find it
+    if (!canAccess) {
+      // if there were some change at User rights that wasn't be propagated to the
+      // token yet
+      List<ResourceAccessVO> resourceAccessVOS = null;
+      try {
+        resourceAccessVOS = userManagementControllerZull.getResourcesByUser();
+      } catch (FeignException e) {
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+      }
+      // ObjectAccessRoleEnum expression has the following formate
+      // ROLE_DATASCHEMA-1-DATA_CUSTODIAN
+      List<String> resourceRoles = resourceAccessVOS.stream().map(resourceAccessVO -> {
+        StringBuilder builder = new StringBuilder("ROLE_");
+        return builder.append(resourceAccessVO.getResource().toString()).append("-")
+            .append(resourceAccessVO.getId()).append("-").append(resourceAccessVO.getRole())
+            .toString().toUpperCase();
+      }).collect(Collectors.toList());
+      canAccess = roles.stream().anyMatch(resourceRoles::contains);
+    }
+    return canAccess;
+  }
 
 }
--- validation-service/src/main/java/org/eea/validation/controller/ValidationControllerImpl.java
+++ validation-service/src/main/java/org/eea/validation/controller/ValidationControllerImpl.java
@@ -214,7 +214,7 @@ public class ValidationControllerImpl implements ValidationController {
    */
   @Override
   @GetMapping(value = "listGroupValidations/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  @PreAuthorize("secondLevelAuthorize(#datasetId,'DATASET_STEWARD','DATASET_CUSTODIAN','DATASET_NATIONAL_COORDINATOR','DATASET_OBSERVER','DATASET_LEAD_REPORTER','DATASET_REPORTER_WRITE','DATASET_REPORTER_READ','DATASCHEMA_STEWARD','DATASCHEMA_CUSTODIAN','DATASCHEMA_EDITOR_WRITE','DATASCHEMA_EDITOR_READ','EUDATASET_CUSTODIAN','EUDATASET_STEWARD','EUDATASET_OBSERVER','DATACOLLECTION_CUSTODIAN','DATACOLLECTION_STEWARD','DATACOLLECTION_OBSERVER','TESTDATASET_CUSTODIAN','TESTDATASET_STEWARD','REFERENCEDATASET_CUSTODIAN','REFERENCEDATASET_STEWARD','REFERENCEDATASET_OBSERVER')")
+  @PreAuthorize("checkAccessSuperUser('DATASET',#datasetId)")
   @ApiOperation(value = "Gets all the failed validations for a given dataset grouped by code",
       hidden = true)
   @ApiResponse(code = 400, message = EEAErrorMessage.DATASET_INCORRECT_ID)
