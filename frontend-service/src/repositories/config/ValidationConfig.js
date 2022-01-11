ef71c6c8c605b44ad3ba0cc478e101308771a1a5
igor-co <icojocaru-ext@tracasa.es>
2022-01-11 13:20:22 +0100

    Update to correct endpoint

--- frontend-service/src/repositories/config/ValidationConfig.js
+++ frontend-service/src/repositories/config/ValidationConfig.js
@@ -7,7 +7,7 @@ export const ValidationConfig = {
   generateQCRulesFile: '/rules/exportQC/{:datasetId}',
   generateShowValidationsFile: '/validation/export/{:datasetId}',
   getAll: '/rules/{:datasetSchemaId}/dataflow/{:dataflowId}',
-  getAllQCsHistoricInfo: '/rules/historicInfo?datasetId={:datasetId}',
+  getAllQCsHistoricInfo: '/rules/historicDatasetRules?datasetId={:datasetId}',
   getQcHistoricInfo: '/rules/historicInfo?datasetId={:datasetId}&ruleId={:ruleId}',
   runSqlRule: '/rules/runSqlRule?datasetId={:datasetId}&showInternalFields={:showInternalFields}',
   update: '/rules/updateRule?datasetId={:datasetId}',
