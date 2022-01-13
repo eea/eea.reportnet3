d45b8a7d39e7124fa544a675fd7b632403aac530
Arrastia <arrastia@pm.me>
2022-01-13 15:44:15 +0100

    UPDATE - Referencing dataflows to use useFilters

--- frontend-service/src/views/ReferenceDataflow/_components/ReferencingDataflows/ReferencingDataflows.jsx
+++ frontend-service/src/views/ReferenceDataflow/_components/ReferencingDataflows/ReferencingDataflows.jsx
@@ -4,29 +4,33 @@ import isEmpty from 'lodash/isEmpty';
 
 import styles from './ReferencingDataflows.module.scss';
 
-import { ReferenceDataflowService } from 'services/ReferenceDataflowService';
-
-import { referencingDataflowsReducer } from './_functions/referencingDataflowsReducer';
-
 import { Column } from 'primereact/column';
 import { DataTable } from 'views/_components/DataTable';
 import { MyFilters } from 'views/_components/MyFilters';
 import { Spinner } from 'views/_components/Spinner';
 
+import { ReferenceDataflowService } from 'services/ReferenceDataflowService';
+
+import { referencingDataflowsReducer } from './_functions/referencingDataflowsReducer';
+
+import { useFilters } from 'views/_functions/Hooks/useFilters';
+
 import { ResourcesContext } from 'views/_functions/Contexts/ResourcesContext';
 import { NotificationContext } from 'views/_functions/Contexts/NotificationContext';
 
-const ReferencingDataflows = ({ referenceDataflowId }) => {
+export const ReferencingDataflows = ({ referenceDataflowId }) => {
   const notificationContext = useContext(NotificationContext);
   const resourcesContext = useContext(ResourcesContext);
 
   const [state, dispatch] = useReducer(referencingDataflowsReducer, {
     dataflows: [],
-    error: '',
-    filteredData: [],
     pagination: { first: 0, page: 0, rows: 10 },
     requestStatus: 'idle'
   });
+
+  const { filteredData } = useFilters('referencingDataflows');
+
+  const filterOptions = [{ type: 'INPUT', nestedOptions: [{ key: 'name', label: resourcesContext.messages['name'] }] }];
 
   useEffect(() => {
     onLoadDataflows();
@@ -53,13 +57,8 @@ const ReferencingDataflows = ({ referenceDataflowId }) => {
     dispatch({ type: 'ON_PAGINATE', payload: { pagination } });
   };
 
-  const onLoadFilteredData = dataflows => {
-    dispatch({ type: 'ON_LOAD_FILTERED_DATA', payload: { dataflows } });
-  };
-
-  const filterOptions = [{ type: 'INPUT', nestedOptions: [{ key: 'name', label: resourcesContext.messages['name'] }] }];
-
   const renderNameColumnTemplate = dataflow => <div>{dataflow.name}</div>;
+
   const renderIdColumnTemplate = dataflow => <div>{dataflow.id}</div>;
 
   const renderDialogLayout = children => (
@@ -80,7 +79,7 @@ const ReferencingDataflows = ({ referenceDataflowId }) => {
   }
 
   const renderDialogContent = () => {
-    if (isEmpty(state.filteredData)) {
+    if (isEmpty(filteredData)) {
       return (
         <div className={styles.notMatchingWrap}>
           <h3>{resourcesContext.messages['dataflowsNotMatchingFilter']}</h3>
@@ -95,7 +94,7 @@ const ReferencingDataflows = ({ referenceDataflowId }) => {
         paginator={true}
         rows={state.pagination.rows}
         rowsPerPageOptions={[5, 10, 15]}
-        value={state.filteredData}>
+        value={filteredData}>
         <Column
           body={renderNameColumnTemplate}
           header={resourcesContext.messages['referencingDataflowNameColumnLabel']}
@@ -111,15 +110,8 @@ const ReferencingDataflows = ({ referenceDataflowId }) => {
 
   return renderDialogLayout(
     <Fragment>
-      <MyFilters
-        className="referencingDataflows"
-        data={state.dataflows}
-        getFilteredData={onLoadFilteredData}
-        options={filterOptions}
-      />
+      <MyFilters className="referencingDataflows" data={state.dataflows} options={filterOptions} />
       {renderDialogContent()}
     </Fragment>
   );
 };
-
-export { ReferencingDataflows };
