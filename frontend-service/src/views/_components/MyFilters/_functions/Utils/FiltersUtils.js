e02a3e67d8d73e12475e721d4dfea4e4ccf10f9b
Michael Coloma Calva <mcoloma-ext@outlook.es>
2022-01-14 10:59:29 +0100

    Update getOptionsTypes to allow values to false

--- frontend-service/src/views/_components/MyFilters/_functions/Utils/FiltersUtils.js
+++ frontend-service/src/views/_components/MyFilters/_functions/Utils/FiltersUtils.js
@@ -1,3 +1,4 @@
+import { isNil } from 'lodash';
 import isDate from 'lodash/isDate';
 import isEmpty from 'lodash/isEmpty';
 import uniq from 'lodash/uniq';
@@ -30,7 +31,8 @@ const getLabelsAnimationDateInitial = (options, filterBy) =>
   options.filter(option => option?.type === 'DATE').map(option => ({ [option.key]: !isEmpty(filterBy[option.key]) }));
 
 const getOptionsTypes = (data, nestedOptionKey) => {
-  const options = uniq(data.map(item => item[nestedOptionKey])).filter(item => item);
+  const options = uniq(data.map(item => item[nestedOptionKey])).filter(item => !isNil(item));
+
   return options.map(option => ({ type: option, value: option }));
 };
 
