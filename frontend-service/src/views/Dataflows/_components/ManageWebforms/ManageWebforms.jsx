ea87b180b971cf9041e990a09f6213874dbf74b4
igor-co <icojocaru-ext@tracasa.es>
2022-01-18 17:13:18 +0100

    Add data table with columns object

--- frontend-service/src/views/Dataflows/_components/ManageWebforms/ManageWebforms.jsx
+++ frontend-service/src/views/Dataflows/_components/ManageWebforms/ManageWebforms.jsx
@@ -2,8 +2,11 @@ import { Fragment, useState, useEffect, useContext } from 'react';
 
 import styles from './ManageWebforms.module.scss';
 
-import { Dialog } from 'views/_components/Dialog';
+import { Column } from 'primereact/column';
+
 import { Button } from 'views/_components/Button';
+import { DataTable } from 'views/_components/DataTable';
+import { Dialog } from 'views/_components/Dialog';
 
 import { WebformService } from 'services/WebformService';
 
@@ -14,35 +17,120 @@ export const ManageWebforms = ({ onCloseDialog, isDialogVisible }) => {
   const resourcesContext = useContext(ResourcesContext);
   const notificationContext = useContext(NotificationContext);
 
+  const [isLoadingStatus, setLoadingStatus] = useState('idle');
+  const [isPending, setIsPending] = useState(false);
+  const [selectedWebformId, setSelectedWebformId] = useState(null);
   const [webforms, setWebforms] = useState([]);
 
   useEffect(() => {
     getWebformList();
   }, []);
 
+  useEffect(() => {
+    setIsPending(isPending);
+  }, [isLoadingStatus]);
+
   console.log(`webforms`, webforms);
 
   // Tabla de Webforms
-  // => objeto de las columnas
-  //                - id
+  // => objeto de las columnas  //
   //                webformName
   //                actions
   //                        delete => botón de borrar y que devuelva un error si el webform está en uso en algún dataset
-  //                          download => JSON
-  //                          edit
+  //                        download => JSON
+  //                        edit
 
   // Endpoints
-  // create: un Post con el endpoint /webform/webformConfig. Espera un objeto WebformConfigVO que tiene "name" y "content", siendo content el json en string
-  // update: un Put con el endpoint /webform/webformConfig. Espera un objeto WebformConfigVO que tiene name, content e idReferenced (el id del webform)
-  // delete: un Delete con el endpoint /webform/webformConfig/{id} // Check that endpoint is correct
+  // delete: /webform/webformConfig/{id} // Check that endpoint is correct
+  // download: /webform/webformConfig/{id} // Check that endpoint is correct
+
+  // Confirm dialog: Delete webform
+  //
+
+  const getTableColumns = () => {
+    const columns = [
+      { key: 'label', header: resourcesContext.messages['name'] },
+      {
+        key: 'actions',
+        header: resourcesContext.messages['actions'],
+        template: getActionsTemplate
+      }
+    ];
+
+    return columns.map(column => (
+      <Column
+        body={column.template}
+        columnResizeMode="expand"
+        field={column.key}
+        header={column.header}
+        key={column.key}
+        sortable={column.key !== 'actions'}
+      />
+    ));
+  };
+
+  const getBtnIcon = (id, iconName) => {
+    if (id === selectedWebformId && isPending) {
+      return 'spinnerAnimate';
+    }
+    return iconName;
+  };
+
+  const onShowDeleteDialog = id => {
+    setSelectedWebformId(id);
+    //todo add confirmation dialog
+  };
+
+  const onEdit = id => {
+    setSelectedWebformId(id);
+    //todo add edit dialog
+  };
+
+  const onClickDownload = id => {
+    setSelectedWebformId(id);
+    //todo add download dialog
+  };
+
+  const getActionsTemplate = row => {
+    return (
+      <Fragment>
+        <Button
+          className={`p-button-rounded p-button-secondary-transparent p-button-animated-blink ${styles.actionButton}`}
+          disabled={isPending}
+          icon={getBtnIcon(row.id, 'edit')}
+          onClick={() => onEdit(row.id)}
+          tooltip={resourcesContext.messages['edit']}
+          tooltipOptions={{ position: 'top' }}
+          type="button"
+        />
+        <Button
+          className={`p-button-rounded p-button-secondary-transparent p-button-animated-blink ${styles.actionButton}`}
+          disabled={isPending}
+          icon={getBtnIcon(row.id, 'download')}
+          onClick={() => onClickDownload(row.id)}
+          tooltip={resourcesContext.messages['download']}
+          tooltipOptions={{ position: 'top' }}
+          type="button"
+        />
+        <Button
+          className={`p-button-rounded p-button-secondary-transparent p-button-animated-blink ${styles.deleteRowButton}`}
+          disabled={isPending}
+          icon={getBtnIcon(row.id, 'trash')}
+          onClick={() => onShowDeleteDialog(row.id)}
+          tooltip={resourcesContext.messages['delete']}
+          tooltipOptions={{ position: 'top' }}
+          type="button"
+        />
+      </Fragment>
+    );
+  };
 
   const footer = (
     <Fragment>
       <Button
         className="p-button-primary"
-        // disabled={isLoading}
-        // icon={isLoading ? 'spinnerAnimate' : 'key'}
-        icon={'plus'}
+        disabled={isPending}
+        icon={isPending ? 'spinnerAnimate' : 'plus'}
         label={resourcesContext.messages['add']}
         // onClick={}
       />
@@ -56,29 +144,41 @@ export const ManageWebforms = ({ onCloseDialog, isDialogVisible }) => {
   );
 
   const getWebformList = async () => {
-    // setWebformOptionsLoadingStatus('pending');
+    setLoadingStatus('pending');
 
     try {
       const data = await WebformService.getAll();
       setWebforms(data);
-      //   setWebformOptionsLoadingStatus('success');
+      setLoadingStatus('success');
     } catch (error) {
       console.error('ManageWebforms - getWebformList.', error);
-      //   setWebformOptionsLoadingStatus('failed');
-      notificationContext.add({ type: 'LOADING_WEBFORM_OPTIONS_ERROR' }, true);
+      setLoadingStatus('failed');
+      notificationContext.add({ type: 'LOADING_WEBFORM_OPTIONS_ERROR' }, true); // Todo add correct notification
+    } finally {
+      setLoadingStatus('idle');
     }
   };
+
   return (
     <Dialog
       blockScroll={false}
       className="responsiveDialog"
       footer={footer}
-      //   header={resourcesContext.messages['apiKeyDialogHead']}
-      header="Manage Webforms "
+      header={resourcesContext.messages['manageWebformsDialogHeader']}
       modal
       onHide={onCloseDialog}
       visible={isDialogVisible}>
-      <div>content</div>
+      <DataTable
+        autoLayout
+        //   className={styles.dialogContent}
+        hasDefaultCurrentPage
+        paginator
+        rows={10}
+        rowsPerPageOptions={[5, 10, 15]}
+        totalRecords={webforms.length}
+        value={webforms}>
+        {getTableColumns()}
+      </DataTable>
     </Dialog>
   );
 };
