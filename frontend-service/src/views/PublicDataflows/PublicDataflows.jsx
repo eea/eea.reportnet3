ba1fdc51ff5ba2fad4673469db30c6854f03063a
Miriam Gaviria <mgaviria-ext@tracasa.es>
2022-01-25 12:01:06 +0100

    Delete - unnecessary line

--- frontend-service/src/views/PublicDataflows/PublicDataflows.jsx
+++ frontend-service/src/views/PublicDataflows/PublicDataflows.jsx
@@ -55,7 +55,6 @@ export const PublicDataflows = () => {
 
   const currentPageTemplate = {
     layout: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport',
-    // eslint-disable-next-line react/no-multi-comp
     CurrentPageReport: options => {
       return (
         <span className={styles.currentPageWrapper}>
