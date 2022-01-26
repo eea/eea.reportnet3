ae5dcec68cd9b5172b749453b1692c986ddc0a79
Miriam Gaviria <mgaviria-ext@tracasa.es>
2022-01-26 08:08:43 +0100

    Delete - unnecessary endpoint

--- frontend-service/src/repositories/config/DatasetConfig.js
+++ frontend-service/src/repositories/config/DatasetConfig.js
@@ -34,8 +34,6 @@ export const DatasetConfig = {
   exportDatasetDataExternal:
     '/dataset/exportFileThroughIntegration?datasetId={:datasetId}&integrationId={:integrationId}',
   exportTableData: '/dataset/exportFile?datasetId={:datasetId}&tableSchemaId={:tableSchemaId}&mimeType={:fileType}',
-  exportTableDataFiltered:
-    '/dataset/exportFile?datasetId={:datasetId}&tableSchemaId={:tableSchemaId}&mimeType={:fileType}',
   exportTableSchema:
     '/dataschema/v1/{:datasetSchemaId}/exportFieldSchemas?datasetId={:datasetId}&tableSchemaId={:tableSchemaId}&mimeType={:fileType}',
   importFileDataset: '/dataset/v1/{:datasetId}/importFileData?delimiter={:delimiter}',
