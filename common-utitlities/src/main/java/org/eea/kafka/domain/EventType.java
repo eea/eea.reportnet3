6bc9621f43e885d02c3fd3d79bf8cbd9abc203b9
vicente.cano <vicente.cano@altia.es>
2022-01-27 11:12:28 +0100

    ADD now importing zip ignores the files that are incorrect but goes on the correct ones

--- common-utitlities/src/main/java/org/eea/kafka/domain/EventType.java
+++ common-utitlities/src/main/java/org/eea/kafka/domain/EventType.java
@@ -30,6 +30,10 @@ public enum EventType {
   IMPORT_REPORTING_FAILED_NAMEFILE_EVENT(LiteralConstants.DATA_REPORTING_TOPIC,
       "import_reporting_namefile_failed_event", true),
 
+  /** The import namefile warning event. */
+  IMPORT_NAMEFILE_WARNING_EVENT(LiteralConstants.DATA_REPORTING_TOPIC,
+      "import_namefile_warning_event", true),
+
   /** The import reporting failed no headers matching event. */
   IMPORT_REPORTING_FAILED_NO_HEADERS_MATCHING_EVENT(LiteralConstants.DATA_REPORTING_TOPIC,
       "import_reporting_no_headers_failed_event", true),
