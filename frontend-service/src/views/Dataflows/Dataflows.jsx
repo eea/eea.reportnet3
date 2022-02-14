2a717288b98ecca8e51dc5f19b465c3452e960eb
Eduardo <ealfaro@tracasa.es>
2022-02-14 13:53:03 +0100

    Fix isAdmin condition

--- frontend-service/src/views/Dataflows/Dataflows.jsx
+++ frontend-service/src/views/Dataflows/Dataflows.jsx
@@ -611,7 +611,7 @@ export const Dataflows = () => {
       }
     ];
 
-    if (isAdmin) {
+    if (!isAdmin) {
       filters.push({
         key: 'userRole',
         label: resourcesContext.messages['userRole'],
