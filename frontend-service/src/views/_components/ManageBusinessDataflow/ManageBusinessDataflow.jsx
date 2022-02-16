ede5be1d80879c4ea7fe9e99ae0bd65e7810eff0
ktklein <ktklein-ext@tracasa.es>
2022-02-16 11:39:06 +0100

    UPDATE - Pr comments

--- frontend-service/src/views/_components/ManageBusinessDataflow/ManageBusinessDataflow.jsx
+++ frontend-service/src/views/_components/ManageBusinessDataflow/ManageBusinessDataflow.jsx
@@ -333,7 +333,7 @@ export const ManageBusinessDataflow = ({
               <div className={`formField ${errors.description.hasErrors ? 'error' : ''}`}>
                 <InputTextarea
                   className={styles.inputTextArea}
-                  disabled={isAdmin && state.status === 'DRAFT'}
+                  disabled={isAdmin && state.status === config.dataflowStatus.OPEN}
                   id="dataflowDescription"
                   onBlur={checkErrors}
                   onChange={event => setDescription(event.target.value)}
@@ -373,7 +373,7 @@ export const ManageBusinessDataflow = ({
                   appendTo={document.body}
                   ariaLabel="fmeUsers"
                   className={styles.fmeUsersWrapper}
-                  disabled={isAdmin && state.status === 'DRAFT'}
+                  disabled={isAdmin && state.status === config.dataflowStatus.OPEN}
                   name="fmeUsers"
                   onChange={event => onSelectFmeUser(event.target.value)}
                   onFocus={() => handleErrors({ field: 'fmeUsers', hasErrors: false, message: '' })}
@@ -385,7 +385,7 @@ export const ManageBusinessDataflow = ({
               </div>
               <div className={`${styles.search}`}>
                 <Button
-                  disabled={isAdmin && state.status === 'DRAFT'}
+                  disabled={isAdmin && state.status === config.dataflowStatus.OPEN}
                   icon="search"
                   label={resourcesContext.messages['searchObligations']}
                   onClick={() => manageDialogs('isReportingObligationsDialogVisible', true)}
