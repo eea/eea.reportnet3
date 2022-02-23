d10a36144eda8251a44553f815d2114b8c22fc01
cristian.martinez <cristian.martinez@altia.es>
2022-02-23 14:03:16 +0100

    Fixed deleted dataflows processes not showing on the table.

--- recordstore-service/src/main/java/org/eea/recordstore/mapper/ProcessMapper.java
+++ recordstore-service/src/main/java/org/eea/recordstore/mapper/ProcessMapper.java
@@ -3,7 +3,9 @@ package org.eea.recordstore.mapper;
 import org.eea.interfaces.vo.recordstore.ProcessVO;
 import org.eea.mapper.IMapper;
 import org.eea.recordstore.persistence.domain.EEAProcess;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 
 /**
@@ -11,5 +13,20 @@ import org.mapstruct.Mapper;
  */
 @Mapper(componentModel = "spring")
 public interface ProcessMapper extends IMapper<EEAProcess, ProcessVO> {
+
+  String DELETED = "DELETED";
+
+  /**
+   * Sets the names.
+   *
+   * @param processVO the new names
+   */
+  @AfterMapping
+  default void setNames(@MappingTarget ProcessVO processVO) {
+    processVO.setDataflowName(
+        processVO.getDataflowName() != null ? processVO.getDataflowName() : DELETED);
+    processVO
+        .setDatasetName(processVO.getDatasetName() != null ? processVO.getDatasetName() : DELETED);
+  }
 }
 
