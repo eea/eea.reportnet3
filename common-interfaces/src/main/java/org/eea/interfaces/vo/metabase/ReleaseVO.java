cc346e965cc1defb2eeeb0763abd20f84b4fa7e8
Bea <beatrizsevilla@altia.es>
2022-05-04 13:16:01 +0200

    fix releasedate

--- common-interfaces/src/main/java/org/eea/interfaces/vo/metabase/ReleaseVO.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/metabase/ReleaseVO.java
@@ -1,7 +1,6 @@
 package org.eea.interfaces.vo.metabase;
 
 import java.io.Serializable;
-import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -21,7 +20,7 @@ public class ReleaseVO implements Serializable {
   private Long id;
 
   /** The date released. */
-  private Date dateReleased;
+  private String dateReleased;
 
   /** The data collection release. */
   private Boolean dcrelease;
