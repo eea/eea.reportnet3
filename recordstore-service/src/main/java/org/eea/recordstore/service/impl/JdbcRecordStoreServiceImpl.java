af73fe4bb487dce8515d5304c95e5b25ea7217da
vicente.cano <vicente.cano@altia.es>
2022-06-16 11:50:03 +0200

    FIX removing reporters from list

--- recordstore-service/src/main/java/org/eea/recordstore/service/impl/JdbcRecordStoreServiceImpl.java
+++ recordstore-service/src/main/java/org/eea/recordstore/service/impl/JdbcRecordStoreServiceImpl.java
@@ -176,8 +176,6 @@ public class JdbcRecordStoreServiceImpl implements RecordStoreService {
   @Value("classpath:datasetInitCommandsCitus.txt")
   private Resource resourceDistributeFirstFile;
 
-  @Value("classpath:datasetCitusLoop.txt")
-  private Resource resourceLoop;
 
   /** The path snapshot. */
   @Value("${pathSnapshot}")
