782885d9b9f6679db4fc5038e25cb641a21c5f7d
Maria Favva <maria.favva@gmail.com>
2022-09-20 16:58:04 +0300

    implementation for new orchestrator microservice. Refs#154450

---
+++ orchestrator-service/src/main/java/org/eea/orchestrator/scheduling/JobForRestartingInProgressTasks.java
@@ -0,0 +1,95 @@
+package org.eea.orchestrator.scheduling;
+
+import org.eea.interfaces.controller.ums.UserManagementController.UserManagementControllerZull;
+import org.eea.interfaces.controller.validation.ValidationController.ValidationControllerZuul;
+import org.eea.interfaces.vo.ums.TokenVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.math.BigInteger;
+import java.util.List;
+
+@Component
+public class JobForRestartingInProgressTasks {
+
+    @Value(value = "${scheduling.inProgress.task.max.time}")
+    private long maxTimeInMinutesForInProgressTasks;
+
+    /**
+     * The admin user.
+     */
+    @Value("${eea.keycloak.admin.user}")
+    private String adminUser;
+
+    /**
+     * The admin pass.
+     */
+    @Value("${eea.keycloak.admin.password}")
+    private String adminPass;
+
+
+    private static final String BEARER = "Bearer ";
+
+    /**
+     * The Constant LOG.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(JobForRestartingInProgressTasks.class);
+
+    @Autowired
+    private ValidationControllerZuul validationControllerZuul;
+
+    @Autowired
+    private UserManagementControllerZull userManagementControllerZull;
+
+    @PostConstruct
+    private void init() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.initialize();
+        scheduler.schedule(() -> restartInProgressTasksExceedingTime(),
+                new CronTrigger("0 */5 * * * *"));
+    }
+
+    /**
+     * The job runs every 5 minutes. It finds task ids for tasks that have status=IN_PROGRESS for more than maxTimeInMinutesForInProgressTasks
+     * and sets their status to status=IN_QUEUE.
+     */
+    public void restartInProgressTasksExceedingTime() {
+        LOG.info("Running scheduled task restartInProgressTasksExceedingTime");
+        try {
+            List<BigInteger> tasksInProgress = validationControllerZuul.listTasksInProgress(maxTimeInMinutesForInProgressTasks);
+            TokenVO tokenVo = userManagementControllerZull.generateToken(adminUser, adminPass);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(adminUser, BEARER + tokenVo.getAccessToken(), null);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            tasksInProgress.stream().forEach(taskId -> validationControllerZuul.restartTask(taskId.longValue()));
+        } catch (Exception e) {
+            LOG.error("Error while running scheduled task restartInProgressTasksExceedingTime " + e.getMessage());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
