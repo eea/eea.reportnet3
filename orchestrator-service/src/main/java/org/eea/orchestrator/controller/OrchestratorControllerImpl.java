f2c352c3cd77693903a51d62d73d9d79b1c8718c
Maria Favva <maria.favva@gmail.com>
2022-11-18 10:07:27 +0200

    fix for method not found.Refs#155243

--- orchestrator-service/src/main/java/org/eea/orchestrator/controller/OrchestratorControllerImpl.java
+++ orchestrator-service/src/main/java/org/eea/orchestrator/controller/OrchestratorControllerImpl.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/orchestrator")
+@RequestMapping(value = "/processes")
 public class OrchestratorControllerImpl implements OrchestratorController {
 
     private transient CommandGateway commandGateway;
