e383c0d52c4c54844cc5739912238e4673d89fe7
Maria Favva <maria.favva@gmail.com>
2022-12-12 16:10:32 +0200

    removed unused import.Refs#155243

--- orchestrator-service/src/main/java/org/eea/orchestrator/axon/sagas/ReleaseSaga.java
+++ orchestrator-service/src/main/java/org/eea/orchestrator/axon/sagas/ReleaseSaga.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.*;
