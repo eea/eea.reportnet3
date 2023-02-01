27566ca54301c6662cd51ff7ce1ed299aa01a547
Dimosthenis Kitsios <Dimosthenis.KITSIOS@trasys.gr>
2023-02-01 14:39:57 +0000

    Logging

--- dataset-service/src/main/java/org/eea/dataset/service/file/CSVSegmentedReaderStrategy.java
+++ dataset-service/src/main/java/org/eea/dataset/service/file/CSVSegmentedReaderStrategy.java
@@ -284,6 +284,7 @@ public class CSVSegmentedReaderStrategy {
             if (idTableSchema != null) {
                 final FieldSchema fieldSchema =
                         fileCommon.findIdFieldSchema(value, idTableSchema, dataSetSchema);
+                LOG.info("Processing findIdFieldSchema result {} for value {}, idTableSchema {} and dataSetSchema {}", fieldSchema, value, idTableSchema, dataSetSchema);
                 if (null != fieldSchema) {
                     atLeastOneFieldSchema = true;
                     header.setIdFieldSchema(fieldSchema.getIdFieldSchema());
