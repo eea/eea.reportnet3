4483f461dacc1e131044f6f5c690101019276571
Dimosthenis Kitsios <Dimosthenis.KITSIOS@trasys.gr>
2023-06-16 08:22:10 +0000

    Test Download file

--- dataset-service/src/main/java/org/eea/dataset/controller/TestDremioControllerImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/controller/TestDremioControllerImpl.java
@@ -1,21 +1,27 @@
 package org.eea.dataset.controller;
 
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.eea.datalake.service.S3ConvertService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 @RequestMapping("/dremio")
@@ -40,14 +46,15 @@ public class TestDremioControllerImpl {
     public void run() {
         SqlRowSet rs = dremioJdbcTemplate.queryForRowSet("SELECT * FROM \"rn3-dataset.rn3-dataset\".\"tab.csv\";");
         while (rs.next()) {
-            System.out.println(rs.getString("A") + "," + rs.getString("B") + "," + rs.getString("C") + "," + rs.getString("D"));
+            LOG.info(rs.getString("A") + "," + rs.getString("B") + "," + rs.getString("C") + "," + rs.getString("D"));
         }
     }
 
 
     @GetMapping("/export")
     public void export(@ApiParam(type = "String", value = "filename")
-        @RequestParam("filename") String filename) throws IOException {
+        @RequestParam("filename") String filename,
+        HttpServletResponse response) throws IOException {
 
         try {
             GetObjectRequest objectRequest = GetObjectRequest
@@ -64,21 +71,28 @@ public class TestDremioControllerImpl {
             LOG.info("Local file {}", myFile);
             OutputStream os = new FileOutputStream(myFile);
             os.write(data);
-            System.out.println("Successfully obtained bytes from an S3 object");
+            LOG.info("Successfully obtained bytes from an S3 object");
             os.close();
 
-            s3ConvertService.convertParquetToCSV(myFile, new File("/reportnet3-data/input/importFiles/"+filename+".csv"));
+            File toExport = new File("/reportnet3-data/input/importFiles/"+filename+".csv");
+            s3ConvertService.convertParquetToCSV(myFile, toExport);
 
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=" + FilenameUtils.getName(filename));
+            LOG.info("FilenameUtils.getName(filename) value: {}", FilenameUtils.getName(filename));
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            OutputStream out = response.getOutputStream();
+            FileInputStream in = new FileInputStream(toExport);
+            // copy from in to out
+            IOUtils.copyLarge(in, out);
+            out.close();
+            in.close();
+            // delete the file after downloading it
+            FileUtils.forceDelete(toExport);
+        } catch (IOException | ResponseStatusException ex) {
+            LOG.error("IOException/ResponseStatusException ", ex);
         } catch (S3Exception e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
+            LOG.error("S3Exception ", e);
         }
-
-
     }
-
-
 }
