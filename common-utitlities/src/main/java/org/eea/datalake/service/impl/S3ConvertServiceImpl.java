a277ccf4577c669007957c84d04442b2696aa6ca
Dimosthenis Kitsios <Dimosthenis.KITSIOS@trasys.gr>
2023-06-22 10:09:22 +0000

    Parquet to xlsx convertion

--- common-utitlities/src/main/java/org/eea/datalake/service/impl/S3ConvertServiceImpl.java
+++ common-utitlities/src/main/java/org/eea/datalake/service/impl/S3ConvertServiceImpl.java
@@ -6,12 +6,14 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.parquet.Preconditions;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.eea.datalake.service.S3ConvertService;
 import org.eea.datalake.service.model.ParquetStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,15 +26,8 @@ public class S3ConvertServiceImpl implements S3ConvertService {
     private static final Logger LOG = LoggerFactory.getLogger(S3ConvertServiceImpl.class);
 
     @Override
-    public void convertParquetToCSV(File parquetFile, File csvOutputFile) throws IOException {
-        Preconditions.checkArgument(parquetFile.getName().endsWith(".parquet"),
-            "parquet file should have .parquet extension");
-        Preconditions.checkArgument(csvOutputFile.getName().endsWith(".csv"),
-            "csv file should have .csv extension");
-        Preconditions.checkArgument(!csvOutputFile.exists(),
-            "Output file " + csvOutputFile.getAbsolutePath() + " already exists");
-
-        LOG.info("Converting {} to {}", parquetFile.getName(), csvOutputFile.getName());
+    public void convertParquetToCSV(File parquetFile, File csvOutputFile) {
+        validateFileFormat(parquetFile, csvOutputFile);
 
         try (InputStream inputStream = new FileInputStream(parquetFile);
             CSVWriter csvWriter = new CSVWriter(new FileWriter(csvOutputFile),
@@ -73,14 +68,7 @@ public class S3ConvertServiceImpl implements S3ConvertService {
 
     @Override
     public void convertParquetToJSON(File parquetFile, File jsonOutputFile) {
-        Preconditions.checkArgument(parquetFile.getName().endsWith(".parquet"),
-            "parquet file should have .parquet extension");
-        Preconditions.checkArgument(jsonOutputFile.getName().endsWith(".json"),
-            "csv file should have .json extension");
-        Preconditions.checkArgument(!jsonOutputFile.exists(),
-            "Output file " + jsonOutputFile.getAbsolutePath() + " already exists");
-
-        LOG.info("Converting {} to {}", parquetFile.getName(), jsonOutputFile.getName());
+        validateFileFormat(parquetFile, jsonOutputFile);
 
         try (InputStream inputStream = new FileInputStream(parquetFile);
             FileWriter fw = new FileWriter(jsonOutputFile, true);
@@ -132,14 +120,7 @@ public class S3ConvertServiceImpl implements S3ConvertService {
 
     @Override
     public void convertParquetToXML(File parquetFile, File xmlOutputFile) {
-        Preconditions.checkArgument(parquetFile.getName().endsWith(".parquet"),
-            "parquet file should have .parquet extension");
-        Preconditions.checkArgument(xmlOutputFile.getName().endsWith(".xml"),
-            "csv file should have .xml extension");
-        Preconditions.checkArgument(!xmlOutputFile.exists(),
-            "Output file " + xmlOutputFile.getAbsolutePath() + " already exists");
-
-        LOG.info("Converting {} to {}", parquetFile.getName(), xmlOutputFile.getName());
+        validateFileFormat(parquetFile, xmlOutputFile);
 
         try (InputStream inputStream = new FileInputStream(parquetFile);
             FileWriter fw = new FileWriter(xmlOutputFile, true);
@@ -150,8 +131,6 @@ public class S3ConvertServiceImpl implements S3ConvertService {
                 .<GenericRecord>builder(parquetStream)
                 .disableCompatibility()
                 .build();
-
-            Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
 
             bw.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
             bw.write("<Records>\n");
@@ -176,5 +155,56 @@ public class S3ConvertServiceImpl implements S3ConvertService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void convertParquetToExcel(File parquetFile, File excelOutputFile) {
+        validateFileFormat(parquetFile, excelOutputFile);
+
+        try (InputStream inputStream = new FileInputStream(parquetFile)) {
+            ParquetStream parquetStream = new ParquetStream(inputStream);
+            ParquetReader<GenericRecord> r = AvroParquetReader
+                .<GenericRecord>builder(parquetStream)
+                .disableCompatibility()
+                .build();
+
+            SXSSFWorkbook workbook = new SXSSFWorkbook(1);
+            Sheet sheet = workbook.createSheet();
+
+            int counter = 0;
+            Row row = sheet.createRow(counter++);
+            GenericRecord record = r.read();
+
+            int size = record.getSchema().getFields().size();
+            for (int i = 0; i < size; i++) {
+                row.createCell(i).setCellValue(record.get(i).toString());
+            }
+
+            while ((record = r.read()) != null) {
+                row = sheet.createRow(counter++);
+                for (int i = 0; i < size; i++) {
+                    row.createCell(i).setCellValue(record.get(i).toString());
+                }
+            }
+
+            try (FileOutputStream outputStream = new FileOutputStream(excelOutputFile)) {
+                workbook.write(outputStream);
+            }
+            workbook.dispose();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void validateFileFormat(File parquetFile, File csvOutputFile) {
+        Preconditions.checkArgument(parquetFile.getName().endsWith(".parquet"),
+            "parquet file should have .parquet extension");
+        Preconditions.checkArgument(csvOutputFile.getName().endsWith(".csv"),
+            "csv file should have .csv extension");
+        Preconditions.checkArgument(!csvOutputFile.exists(),
+            "Output file " + csvOutputFile.getAbsolutePath() + " already exists");
+
+        LOG.info("Converting {} to {}", parquetFile.getName(), csvOutputFile.getName());
     }
 }
