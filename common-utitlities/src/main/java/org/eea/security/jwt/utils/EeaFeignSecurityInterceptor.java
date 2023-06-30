477dc2105e2d47d8bc6bc1c979b2ad9afbe094a7
Ntenia Athanasakou <antonia.ath@gmail.com>
2023-06-30 14:52:11 +0300

    Added fix for promoting folder by path. Refs #251839

--- common-utitlities/src/main/java/org/eea/security/jwt/utils/EeaFeignSecurityInterceptor.java
+++ common-utitlities/src/main/java/org/eea/security/jwt/utils/EeaFeignSecurityInterceptor.java
@@ -1,5 +1,6 @@
 package org.eea.security.jwt.utils;
 
+import org.eea.utils.LiteralConstants;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -18,11 +19,22 @@ public class EeaFeignSecurityInterceptor implements RequestInterceptor {
 
   private static final String AUTHORIZATION_HEADER = "Authorization";
 
+  private static final String DREMIO_PROMOTE_BY_PATH_CONSTANT = "dremio%3A";
 
   @Override
   public void apply(RequestTemplate template) {
     SecurityContext securityContext = SecurityContextHolder.getContext();
     Authentication authentication = securityContext.getAuthentication();
+    if (template.url().contains(DREMIO_PROMOTE_BY_PATH_CONSTANT)){
+      //TODO this is done to encode the path of the folder for promoting. It needs to be enhanced
+      String[] urlInParts = template.url().split(DREMIO_PROMOTE_BY_PATH_CONSTANT);
+      if(urlInParts.length == 2) {
+        String dremioPath = urlInParts[1];
+        dremioPath = dremioPath.replace("/", "%2F");
+        String fullUrl = urlInParts[0] + DREMIO_PROMOTE_BY_PATH_CONSTANT + dremioPath;
+        template.uri(fullUrl);
+      }
+    }
     if (template.url().contains("api/v3/catalog")) {
       return;
     }
