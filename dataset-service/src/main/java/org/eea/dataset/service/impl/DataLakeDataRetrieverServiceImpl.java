281415eb482224033d05f00f93cc43b011be6a8e
Dimosthenis Kitsios <Dimosthenis.KITSIOS@trasys.gr>
2023-08-09 09:57:39 +0000

    Display dc data on UI - Fix path DC

--- dataset-service/src/main/java/org/eea/dataset/service/impl/DataLakeDataRetrieverServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/DataLakeDataRetrieverServiceImpl.java
@@ -88,9 +88,9 @@ public class DataLakeDataRetrieverServiceImpl implements DataLakeDataRetrieverSe
             S3PathResolver s3PathResolver = new S3PathResolver(dataset.getDataflowId(),
                 dataset.getDataProviderId()!=null ? dataset.getDataProviderId() : 47, datasetId, tableSchemaVO.getNameTableSchema());
             LOG.info("For datasetId {} s3PathResolver : {}", datasetId, s3PathResolver);
-            LOG.info("s3Helper.checkFolderExist(s3PathResolver, S3_TABLE_NAME_DC_FOLDER_PATH) : {}", s3Helper.checkFolderExist(s3PathResolver, S3_TABLE_NAME_DC_FOLDER_PATH));
+            LOG.info("s3Helper.checkFolderExist(s3PathResolver, S3_TABLE_NAME_DC_FOLDER_PATH) : {}", s3Helper.checkTableNameDCFolderExist(s3PathResolver));
             LOG.info("dremioHelperService.checkFolderPromoted(s3PathResolver) : {}",  dremioHelperService.checkFolderPromoted(s3PathResolver));
-            if (s3Helper.checkFolderExist(s3PathResolver, S3_TABLE_NAME_DC_FOLDER_PATH) && dremioHelperService.checkFolderPromoted(s3PathResolver)) {
+            if (s3Helper.checkTableNameDCFolderExist(s3PathResolver) && dremioHelperService.checkFolderPromoted(s3PathResolver)) {
                 LOG.info("s3Helper.buildRecordsCountQuery(s3PathResolver) : {}",  s3Helper.buildRecordsCountQuery(s3PathResolver));
                 totalRecords = dremioJdbcTemplate.queryForObject(s3Helper.buildRecordsCountQuery(s3PathResolver), Long.class);
                 LOG.info("For datasetId {} totalRecords : {}", datasetId, totalRecords);
