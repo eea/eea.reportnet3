899fb59d40bf745107a42ecae1e105de800e239d
Dimosthenis Kitsios <Dimosthenis.KITSIOS@trasys.gr>
2023-08-29 11:30:21 +0000

    Remove forward slash

--- common-utitlities/src/main/java/org/eea/utils/LiteralConstants.java
+++ common-utitlities/src/main/java/org/eea/utils/LiteralConstants.java
@@ -231,13 +231,13 @@ public final class LiteralConstants {
   public static final String S3_TABLE_NAME_VALIDATE_QUERY_PATH = S3_DEFAULT_BUCKET + ".\"%s\".\"%s\".\"%s\".\"current\".\"%s_validate\".\"%s\"";
 
   /** The Constant S3_TABLE_NAME_PATH: {@value}. */
-  public static final String S3_TABLE_NAME_PATH = "/%s/%s/%s/current/%s/%s";
+  public static final String S3_TABLE_NAME_PATH = "%s/%s/%s/current/%s/%s";
 
   /** The Constant S3_TABLE_NAME_FOLDER_PATH: {@value}. */
   public static final String S3_TABLE_NAME_FOLDER_PATH = "%s/%s/%s/current/%s";
 
   /** The Constant S3_CURRENT_PATH: {@value}. */
-  public static final String S3_CURRENT_PATH = "/%s/%s/%s/current";
+  public static final String S3_CURRENT_PATH = "%s/%s/%s/current";
 
   /** The Constant S3_TABLE_NAME_QUERY_PATH: {@value}. */
   public static final String S3_TABLE_NAME_QUERY_PATH = S3_DEFAULT_BUCKET + ".\"%s\".\"%s\".\"%s\".\"current\".\"%s\".\"%s\"";
@@ -246,7 +246,7 @@ public final class LiteralConstants {
   public static final String S3_VALIDATION_DC_PATH = "%s/%s/%s/current/validation/%s/%s/%s";
 
   /** The Constant S3_VALIDATION_TABLE_PATH: {@value}. */
-  public static final String S3_VALIDATION_TABLE_PATH = "/%s/%s/%s/current/validation";
+  public static final String S3_VALIDATION_TABLE_PATH = "%s/%s/%s/current/validation";
 
   /** The Constant S3_VALIDATION_DC_QUERY_PATH: {@value}. */
   public static final String S3_VALIDATION_DC_QUERY_PATH = S3_DEFAULT_BUCKET + ".\"%s\".\"%s\".\"%s\".\"current\".\"validation\".\"%s\".\"%s\".\"%s\"";
