3b4a4018c12576b36c80cde4367794585ef68b42
leonidas <lz@eworx.gr>
2023-10-12 11:29:27 +0300

    Fixed api call. Refs #159803

--- frontend-service/src/repositories/config/AddOrganizationsConfig.js
+++ frontend-service/src/repositories/config/AddOrganizationsConfig.js
@@ -1,5 +1,5 @@
 export const AddOrganizationsConfig = {
-  createProvider: '/representative/createProvider',
+  createProvider: '/representative/provider/create',
   getOrganizations:
     '/representative/dataProvider/?pageNum={:pageNum}&pageSize={:numberRows}&asc={:sortOrder}&sortedColumn={:sortField}&providerCode={:providerCode}&groupId={:groupId}&label={:label}'
 };
