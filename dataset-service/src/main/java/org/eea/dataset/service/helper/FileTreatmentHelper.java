afb890dab3928450d3d27bd8d0dd14cb94654691
Ntenia Athanasakou <antonia.ath@gmail.com>
2023-11-30 11:55:42 +0200

    Allowed etlImport for reference datasets. Refs #260518

--- dataset-service/src/main/java/org/eea/dataset/service/helper/FileTreatmentHelper.java
+++ dataset-service/src/main/java/org/eea/dataset/service/helper/FileTreatmentHelper.java
@@ -655,7 +655,7 @@ public class FileTreatmentHelper implements DisposableBean {
         List<RecordValue> allRecords = new ArrayList<>();
 
         tableValueFor(datasetId, dataset, readOnlyTables, fixedNumberTables, allRecords,
-                tableWithAttachmentFieldSet, datasetSchema.getTableSchemas());
+                tableWithAttachmentFieldSet, datasetSchema);
         recordRepository.saveAll(allRecords);
         LOG.info("Data saved for datasetId {}", datasetId);
         // now the view is not updated, update the check to false
@@ -1971,18 +1971,20 @@ public class FileTreatmentHelper implements DisposableBean {
          * @param fixedNumberTables the fixed number tables
          * @param allRecords the all records
          * @param tableWithAttachmentFieldSet the table with attachment field set
-         * @param tableSchemas the table schemas
+         * @param dataSetSchema the dataset schema
          */
         private void tableValueFor (Long datasetId, DatasetValue dataset, List < String > readOnlyTables,
                 List < String > fixedNumberTables, List < RecordValue > allRecords,
-                Set < String > tableWithAttachmentFieldSet, List < TableSchema > tableSchemas){
+                Set < String > tableWithAttachmentFieldSet, DataSetSchema dataSetSchema){
             for (TableValue tableValue : dataset.getTableValues()) {
                 // Check if the table with idTableSchema has been populated already
                 Long oldTableId =
                         datasetService.findTableIdByTableSchema(datasetId, tableValue.getIdTableSchema());
                 fillTableId(tableValue.getIdTableSchema(), dataset.getTableValues(), oldTableId);
-                if (!readOnlyTables.contains(tableValue.getIdTableSchema())
-                        && !fixedNumberTables.contains(tableValue.getIdTableSchema())) {
+                DatasetTypeEnum datasetType = datasetService.getDatasetType(datasetId);
+                if ((!readOnlyTables.contains(tableValue.getIdTableSchema())
+                        && !fixedNumberTables.contains(tableValue.getIdTableSchema()))
+                || datasetType.equals(DatasetTypeEnum.REFERENCE) || dataSetSchema.isReferenceDataset()) {
                     // Put an empty value to the field if it's an attachment type if and only if table has
                     // fields of this type
                     if (tableWithAttachmentFieldSet.contains(tableValue.getIdTableSchema())) {
@@ -2022,7 +2024,7 @@ public class FileTreatmentHelper implements DisposableBean {
                 } else if (!readOnlyTables.contains(tableValue.getIdTableSchema())
                         && fixedNumberTables.contains(tableValue.getIdTableSchema())) {
                     ObjectId tableSchemaIdTemp = new ObjectId(tableValue.getIdTableSchema());
-                    TableSchema tableSchema = tableSchemas.stream()
+                    TableSchema tableSchema = dataSetSchema.getTableSchemas().stream()
                             .filter(tableSchemaIt -> tableSchemaIt.getIdTableSchema().equals(tableSchemaIdTemp))
                             .findFirst().orElse(null);
                     if (tableSchema != null) {
