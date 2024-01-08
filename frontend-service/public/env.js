6aa6ca99c846b8f3ec1e581512d779413e30b265
leonidas <lz@eworx.gr>
2024-01-08 12:12:51 +0200

    Fixed env.js

--- frontend-service/public/env.js
+++ frontend-service/public/env.js
@@ -24,6 +24,9 @@ window.env = {
    */
   REACT_APP_BACKEND: 'https://sandbox-api.reportnet.europa.eu',
   EULOGIN_URL:
-      'https://sandbox-auth.reportnet.europa.eu/auth/realms/Reportnet/protocol/openid-connect/auth?client_id=reportnet&redirect_uri=https%3A%2F%2Fsandbox.reportnet.europa.eu%2Feulogin%2F&response_mode=fragment&response_type=code&scope=openid',
+    'https://sandbox-auth.reportnet.europa.eu/auth/realms/Reportnet/protocol/openid-connect/auth?client_id=reportnet&redirect_uri=https%3A%2F%2Fsandbox.reportnet.europa.eu%2Feulogin%2F&response_mode=fragment&response_type=code&scope=openid',
   WEBSOCKET_URL: 'wss://sandbox.reportnet.europa.eu/communication/reportnet-websocket',
+
+  REACT_APP_EULOGIN: false,
+  DOCUMENTATION_FOLDER: 'test'
 };
