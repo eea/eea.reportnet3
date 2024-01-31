25a2cd185b0bb829a0785ae1d70c9a0a73b130d3
Maria Favva <maria.favva@gmail.com>
2024-01-31 10:19:10 +0200

    remove redundant if statement.Refs#264064

--- validation-service/src/main/java/org/eea/validation/service/impl/LoadValidationsHelperDL.java
+++ validation-service/src/main/java/org/eea/validation/service/impl/LoadValidationsHelperDL.java
@@ -2,7 +2,6 @@ package org.eea.validation.service.impl;
 
 import org.eea.datalake.service.DremioHelperService;
 import org.eea.datalake.service.S3Helper;
-import org.eea.datalake.service.S3Service;
 import org.eea.datalake.service.annotation.ImportDataLakeCommons;
 import org.eea.datalake.service.model.S3PathResolver;
 import org.eea.exception.EEAException;
@@ -15,7 +14,6 @@ import org.eea.interfaces.vo.dataset.enums.EntityTypeEnum;
 import org.eea.interfaces.vo.dataset.enums.ErrorTypeEnum;
 import org.eea.interfaces.vo.dataset.schemas.DataSetSchemaVO;
 import org.eea.interfaces.vo.dataset.schemas.TableSchemaVO;
-import org.eea.multitenancy.DatasetId;
 import org.eea.validation.service.DataLakeValidationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +22,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -73,6 +70,8 @@ public class LoadValidationsHelperDL {
                     fieldValueFilter, pageable, headerField, asc, true);
             validation.setErrors(errors);
             validation.setTotalErrors(dremioJdbcTemplate.queryForObject(s3Helper.buildRecordsCountQuery(s3PathResolver), Long.class));
+            validation.setTotalFilteredRecords(Long.valueOf(dataLakeValidationService.findGroupRecordsByFilter(s3PathResolver, levelErrorsFilter,
+                            typeEntitiesFilter, tableFilter, fieldValueFilter, pageable, headerField, asc, false).size()));
             DataSetSchemaVO schema = datasetSchemaControllerZuul.findDataSchemaByDatasetId(datasetId);
             List<String> tableNames = schema.getTableSchemas().stream().map(TableSchemaVO::getNameTableSchema).collect(Collectors.toList());
             AtomicReference<Long> totalRecords = new AtomicReference<>(0L);
@@ -84,12 +83,6 @@ public class LoadValidationsHelperDL {
                 }
             });
             validation.setTotalRecords(totalRecords.get());
-            s3PathResolver.setTableName(S3_VALIDATION);
-            if (s3Helper.checkFolderExist(s3PathResolver, S3_TABLE_NAME_FOLDER_PATH)) {
-                validation.setTotalFilteredRecords(
-                        Long.valueOf(dataLakeValidationService.findGroupRecordsByFilter(s3PathResolver, levelErrorsFilter,
-                                        typeEntitiesFilter, tableFilter, fieldValueFilter, pageable, headerField, asc, false).size()));
-            }
         }
         LOG.info(
                 "Total validations founded in datasetId {}: {}. Now in page {}, {} validation errors by page",
