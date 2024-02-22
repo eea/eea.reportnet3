b705bcbd4308241340a202a6e67326eeba528c45
EvanNik <evangelos.nikolis@trasys.gr>
2024-02-22 16:48:59 +0200

    Revert "Revert "Revert "Revert "Revert "add datasource bean"""""
    This reverts commit 4a223ee4dd2e323f6de36166a34bcf4f91317ff3.

--- user-management-service/src/main/java/org/eea/ums/configuration/UserManagementConfiguration.java
+++ user-management-service/src/main/java/org/eea/ums/configuration/UserManagementConfiguration.java
@@ -1,15 +1,10 @@
 package org.eea.ums.configuration;
 
 import org.eea.security.jwt.configuration.EeaEnableSecurity;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.sql.DataSource;
 
 /**
  * The Class UserManagementConfiguration.
@@ -18,40 +13,6 @@ import javax.sql.DataSource;
 @EeaEnableSecurity
 public class UserManagementConfiguration implements WebMvcConfigurer {
 
-
-  /** The url. */
-  @Value("${spring.datasource.metasource.url}")
-  private String url;
-
-  /** The username. */
-  @Value("${spring.datasource.metasource.username}")
-  private String username;
-
-  /** The password. */
-  @Value("${spring.datasource.metasource.password}")
-  private String password;
-
-  /** The driver. */
-  @Value("${spring.datasource.metasource.driver-class-name}")
-  private String driver;
-
-
-  /**
-   * Data source data source.
-   *
-   * @return the data source
-   */
-  @Bean
-  @Primary
-  public DataSource metabaseDatasource() {
-    DriverManagerDataSource metaDataSource = new DriverManagerDataSource();
-    metaDataSource.setDriverClassName(driver);
-    metaDataSource.setUrl(url);
-    metaDataSource.setUsername(username);
-    metaDataSource.setPassword(password);
-
-    return metaDataSource;
-  }
 
   /**
    * Rest template.
