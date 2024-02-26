bb12af237966898c2ef5e616d09ae23f6a9fe64f
EvanNik <evangelos.nikolis@trasys.gr>
2024-02-26 18:53:54 +0200

    remove dummy

--- user-management-service/src/main/java/org/eea/ums/service/impl/UserNationalCoordinatorServiceImpl.java
+++ user-management-service/src/main/java/org/eea/ums/service/impl/UserNationalCoordinatorServiceImpl.java
@@ -117,7 +117,7 @@ public class UserNationalCoordinatorServiceImpl implements UserNationalCoordinat
     if (redisLockService.checkAndAcquireLock(lockKey, value, lockExpirationInMillis)) {
       kafkaSenderUtils.releaseNotificableKafkaEvent(EventType.NATIONAL_COORDINATOR_ADDING_PROCESS_STARTED_EVENT, null, notificationVO);
 
-    checkUser(userNationalCoordinatorVO);//
+    checkUser(userNationalCoordinatorVO);
 
       // check Country
       List<DataProviderVO> providers = representativeControllerZuul
