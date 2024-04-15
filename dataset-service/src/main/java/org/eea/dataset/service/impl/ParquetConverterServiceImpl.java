9a9541ad554f7347e82d60b27d25d51cc534030a
Ntenia Athanasakou <antonia.ath@gmail.com>
2024-04-15 14:37:51 +0300

    Added lazy annotation for fileTreatmentHelper

--- dataset-service/src/main/java/org/eea/dataset/service/impl/ParquetConverterServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/ParquetConverterServiceImpl.java
@@ -35,6 +35,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
@@ -75,6 +76,7 @@ public class ParquetConverterServiceImpl implements ParquetConverterService {
     @Autowired
     private FileCommonUtils fileCommonUtils;
 
+    @Lazy
     @Autowired
     private FileTreatmentHelper fileTreatmentHelper;
 
