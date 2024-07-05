ae2c3e869f87fd2802e29dc1e1cff08433a979a6
EvanNik <evangelos.nikolis@trasys.gr>
2024-07-05 14:54:54 +0300

    update POM fix MR conflicts

--- dataset-service/src/main/java/org/eea/dataset/service/impl/ParquetConverterServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/ParquetConverterServiceImpl.java
@@ -230,7 +230,7 @@ public class ParquetConverterServiceImpl implements ParquetConverterService {
             "the conversion of the csv to parquet will use a dremio query", importFileInDremioInfo);
         String createTableQuery = getTableQuery(importFileInDremioInfo, parquetInnerFolderPath, dremioPathForCsvFile, csvFile.getName(), dataSetSchema);
         String processId = dremioHelperService.executeSqlStatement(createTableQuery);
-        dremioHelperService.ckeckIfDremioProcessFinishedSuccessfully(createTableQuery, processId);
+        dremioHelperService.checkIfDremioProcessFinishedSuccessfully(createTableQuery, processId, null);
       }
     }
     //refresh the metadata
