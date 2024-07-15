1898db8979f5b5b94abac1e41e18a16e26f9109b
Christos Kalaitzis <p70969@nrb.be>
2024-07-15 16:45:23 +0300

    265684 History of Job monitoring

--- orchestrator-service/src/main/java/org/eea/orchestrator/persistence/repository/JobHistoryExtendedRepositoryImpl.java
+++ orchestrator-service/src/main/java/org/eea/orchestrator/persistence/repository/JobHistoryExtendedRepositoryImpl.java
@@ -25,7 +25,8 @@ public class JobHistoryExtendedRepositoryImpl implements JobHistoryExtendedRepos
     private static final String COUNT_JOB_HISTORY = "select count(*) from job_history";
 
     /* This query counts the rows of job history that is already grouped by job_id at the nested query. Counts how many unique jobs exist inside job_history table */
-    private static final String COUNT_UNIQUE_JOBS_FILTERED = "select count(*) from (select count(*) from job_history jh group by jh.job_id)";
+    private static final String COUNT_UNIQUE_JOBS_FILTERED_PART_1 = "select count(*) from (select count(*) from job_history jh ";
+    private static final String COUNT_UNIQUE_JOBS_FILTERED_PART_2 = " group by jh.job_id)";
 
     /** The entity manager. */
     @PersistenceContext(name = "orchestratorEntityManagerFactory")
@@ -66,10 +67,10 @@ public class JobHistoryExtendedRepositoryImpl implements JobHistoryExtendedRepos
     @Override
     public Long countFilteredJobs(Long jobId, String jobType, Long dataflowId, String dataflowName, Long providerId, Long datasetId, String datasetName, String creatorUsername, String jobStatus) {
         StringBuilder stringQuery = new StringBuilder();
-        stringQuery.append(COUNT_JOB_HISTORY);
+        stringQuery.append(COUNT_UNIQUE_JOBS_FILTERED_PART_1);
         addFilters( stringQuery, jobId, jobType, dataflowId, dataflowName, providerId, datasetId, datasetName, creatorUsername, jobStatus);
-        Query query = entityManager.createNativeQuery(COUNT_UNIQUE_JOBS_FILTERED);
-        LOG.info(query.toString());
+        stringQuery.append(COUNT_UNIQUE_JOBS_FILTERED_PART_2);
+        Query query = entityManager.createNativeQuery(stringQuery.toString());
 
         return Long.valueOf(query.getSingleResult().toString());
     }
