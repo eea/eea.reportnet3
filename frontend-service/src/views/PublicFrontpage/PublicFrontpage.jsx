bcb82d5d053ddcffcd5af2ab555b8e0bdfa10cbe
Ntenia Athanasakou <antonia.ath@gmail.com>
2024-12-12 14:45:29 +0200

    Fix for email. Refs #281717

--- frontend-service/src/views/PublicFrontpage/PublicFrontpage.jsx
+++ frontend-service/src/views/PublicFrontpage/PublicFrontpage.jsx
@@ -106,7 +106,7 @@ export const PublicFrontpage = () => {
                     <h4>Need any help?</h4>
                     <p>Please contact us at</p>
                     <p>
-                      <a href="mailto:ServiceDesk@eea.europa.eu">hServiceDesk@eea.europa.eu</a>
+                      <a href="mailto:ServiceDesk@eea.europa.eu">ServiceDesk@eea.europa.eu</a>
                     </p>
                   </div>
                   <hr className={styles.separator} />
