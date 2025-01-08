aa5279ace0941587cda7fbc3b1c39012df621456
EvanNik <evangelos.nikolis@trasys.gr>
2025-01-08 15:29:42 +0200

    Fix MR

--- dataset-service/src/main/java/org/eea/dataset/persistence/data/repository/RecordRepositoryImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/persistence/data/repository/RecordRepositoryImpl.java
@@ -1152,13 +1152,14 @@ public class RecordRepositoryImpl implements RecordExtendedQueriesRepository {
       StringBuilder directionQueryBuilder, TableVO result, String filter,
       List<ErrorTypeEnum> errorList, List<String> idRules, String fieldSchema, ExportFilterVO filters,
       Long datasetId, Boolean isExport, SortField... sortFields) {
+    String datasetSchemaId = datasetMetabaseService.findDatasetSchemaIdById(datasetId);
 
-if (Boolean.TRUE.equals(isExport)) {
+    if (Boolean.TRUE.equals(isExport)) {
       int pageNumber = pageable.getPageNumber() + 1;
       int pageSize = pageable.getPageSize();
 
       List<RecordValue> recordValues = fileCommon.getRecordValuesPaginated(datasetId, idTableSchema, PageRequest.of(pageNumber, pageSize), filters );
-      List<RecordVO> recordVOs = recordNoValidationMapper.entityListToClass(sanitizeRecords(recordValues));
+      List<RecordVO> recordVOs = recordNoValidationMapper.entityListToClass(sanitizeRecords(recordValues, datasetSchemaId));
       result.setRecords(recordVOs);
     } else {
       String fieldValue = filters.getFieldValue();    String formatedQuery =
@@ -1167,8 +1168,8 @@ if (Boolean.TRUE.equals(isExport)) {
             directionQueryBuilder.substring(1));
 
     // Query without order or with it
-    Query query = entityManager.createQuery(formatedQuery);
-query.setFirstResult(pageable.getPageSize() * pageable.getPageNumber());
+      Query query = entityManager.createQuery(formatedQuery);
+      query.setFirstResult(pageable.getPageSize() * pageable.getPageNumber());
       query.setMaxResults(pageable.getPageSize());
       query.setParameter(ID_TABLE_SCHEMA, idTableSchema);
       if (null != idRules && !idRules.isEmpty()) {
@@ -1190,25 +1191,25 @@ query.setFirstResult(pageable.getPageSize() * pageable.getPageNumber());
 
       List<RecordVO> recordVOs;
       TenantResolver.setTenantName(String.format(LiteralConstants.DATASET_FORMAT_NAME, datasetId));
-      String datasetSchemaId = datasetMetabaseService.findDatasetSchemaIdById(datasetId);
-    if (null == sortFields) {
-      // Query without order.
-      List<RecordValue> a = query.getResultList();
-        recordVOs = recordNoValidationMapper.entityListToClass(sanitizeRecords(a, datasetSchemaId));
-        if (BooleanUtils.isTrue(isExport) && recordVOs.isEmpty()) {
-          LOG.error("After mapping, no records found. The Query is: {}. FirstResult is : {}, Max results are {}, IdTableSchema is {}", formatedQuery, query.getFirstResult(), query.getMaxResults(), idTableSchema);
+
+      if (null == sortFields) {
+        // Query without order.
+        List<RecordValue> a = query.getResultList();
+          recordVOs = recordNoValidationMapper.entityListToClass(sanitizeRecords(a, datasetSchemaId));
+          if (BooleanUtils.isTrue(isExport) && recordVOs.isEmpty()) {
+            LOG.error("After mapping, no records found. The Query is: {}. FirstResult is : {}, Max results are {}, IdTableSchema is {}", formatedQuery, query.getFirstResult(), query.getMaxResults(), idTableSchema);
+          }
+        } else {
+          // Query with order.
+          List<Object[]> a = query.getResultList();
+          recordVOs = recordNoValidationMapper.entityListToClass(sanitizeOrderedRecords(a, datasetSchemaId));
         }
-      } else {
-        // Query with order.
-        List<Object[]> a = query.getResultList();
-        recordVOs = recordNoValidationMapper.entityListToClass(sanitizeOrderedRecords(a, datasetSchemaId));
+        LOG.info(
+            "Filtering the table in dataset {} by fieldValue as : %{}%, by idTableSchema as {}, by idRules as {}, by errorList as {}, by fieldSchema as {}, with PageSize {} and PageNumber {}",
+            datasetId, fieldValue, idTableSchema, idRules, errorList, fieldSchema,
+            pageable.getPageSize(), pageable.getPageNumber());
+        result.setRecords(recordVOs);
       }
-      LOG.info(
-          "Filtering the table in dataset {} by fieldValue as : %{}%, by idTableSchema as {}, by idRules as {}, by errorList as {}, by fieldSchema as {}, with PageSize {} and PageNumber {}",
-          datasetId, fieldValue, idTableSchema, idRules, errorList, fieldSchema,
-          pageable.getPageSize(), pageable.getPageNumber());
-      result.setRecords(recordVOs);
-    }
   }
 
   /**
