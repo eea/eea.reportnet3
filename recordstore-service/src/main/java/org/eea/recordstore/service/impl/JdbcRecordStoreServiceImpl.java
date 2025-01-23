082e37fe60766a84f82e410581e7c61460fe7f05
EvanNik <evangelos.nikolis@trasys.gr>
2025-01-23 15:14:47 +0200

    Fix issue on reporters while trying to copy to EU dataset

--- recordstore-service/src/main/java/org/eea/recordstore/service/impl/JdbcRecordStoreServiceImpl.java
+++ recordstore-service/src/main/java/org/eea/recordstore/service/impl/JdbcRecordStoreServiceImpl.java
@@ -2249,9 +2249,14 @@ public class JdbcRecordStoreServiceImpl implements RecordStoreService {
                 }
               });
 
-      Long providerId = jobControllerZuul.findProviderIdById(finalJobId);
-      deletePreviousValidationsFromDC(datasetId, dataflowId, providerId);
-      addNewValidationsToDC(datasetId, dataflowId, providerId, finalProcessVO, finalJobId);
+      //only when we have release, we have a release and provider id for removal and addition of validations
+      if (finalJobId != null) {
+        Long providerId = jobControllerZuul.findProviderIdById(finalJobId);
+        if (providerId != null && taskType == TaskType.RELEASE_TASK) {
+          deletePreviousValidationsFromDC(datasetId, dataflowId, providerId);
+          addNewValidationsToDC(datasetId, dataflowId, providerId, finalProcessVO, finalJobId);
+        }
+      }
 
       LOG.info("Updating task status of task with id {} ith idSnapshot {} and processId {} to FINISHED", task.getId(), idSnapshot, processId);
       taskService.updateStatusAndFinishedDate(ProcessStatusEnum.FINISHED.toString(), new Date(), task.getId());
