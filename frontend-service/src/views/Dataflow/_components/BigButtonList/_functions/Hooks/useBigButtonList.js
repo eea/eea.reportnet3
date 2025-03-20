d22404bf888742d9a0e976762f0bf37637fc3cd7
chris-karabel <abcoroum@gmail.com>
2025-03-20 15:46:50 +0200

    feat: SortDatasetSchemas
    (feat #284071)

--- frontend-service/src/views/Dataflow/_components/BigButtonList/_functions/Hooks/useBigButtonList.js
+++ frontend-service/src/views/Dataflow/_components/BigButtonList/_functions/Hooks/useBigButtonList.js
@@ -491,7 +491,7 @@ const useBigButtonList = ({
           }
         ],
         visibility: true
-      }));
+      })).sort((a, b) => a.caption.localeCompare(b.caption));
 
   const euDatasetModels = isNil(dataflowState.data.euDatasets)
     ? []
@@ -514,7 +514,7 @@ const useBigButtonList = ({
           }
         ],
         visibility: true
-      }));
+      })).sort((a, b) => a.caption.localeCompare(b.caption));
 
   const isReleasing = dataflowState?.data?.datasets?.some(dataset => dataset.isReleasing);
 
