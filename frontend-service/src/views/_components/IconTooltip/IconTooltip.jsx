ec5d5a656fa81965104cfa89878dfdddd114c231
chris-karabel <abcoroum@gmail.com>
2025-04-25 11:19:07 +0300

    feat: Validation messages - change them to be selectable
    (feat #284078)

--- frontend-service/src/views/_components/IconTooltip/IconTooltip.jsx
+++ frontend-service/src/views/_components/IconTooltip/IconTooltip.jsx
@@ -1,19 +1,32 @@
 import styles from './IconTooltip.module.css';
-
 import { Button } from 'views/_components/Button';
+import { Dialog } from 'views/_components/Dialog';
+import { useState } from 'react';
 
-export const IconTooltip = ({ className = '', levelError, message, style }) => {
+export const IconTooltip = ({ className = '', levelError, message, style, recordData }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const openDialog = () => {
+    console.log(recordData);
+    setIsDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+
   const buttonProps = {
     className: className,
     icon: '',
     style: style,
     tooltip: message,
-    type: 'button'
+    type: 'button',
+    onClick: openDialog
   };
 
   switch (levelError) {
     case 'INFO':
-      buttonProps.icon = 'warning';
+      buttonProps.icon = 'info';
       buttonProps.className = `${styles.buttonCustom} ${styles.info} ${className}`;
       break;
     case 'WARNING':
@@ -21,7 +34,7 @@ export const IconTooltip = ({ className = '', levelError, message, style }) => {
       buttonProps.className = `${styles.buttonCustom} ${styles.warning} ${className}`;
       break;
     case 'ERROR':
-      buttonProps.icon = 'warning';
+      buttonProps.icon = 'error';
       buttonProps.className = `${styles.buttonCustom} ${styles.error} ${className}`;
       break;
     case 'BLOCKER':
@@ -31,10 +44,28 @@ export const IconTooltip = ({ className = '', levelError, message, style }) => {
     case '':
       buttonProps.icon = '';
       buttonProps.className = ` ${className}`;
+      buttonProps.onClick = null;
       break;
     default:
+      buttonProps.onClick = openDialog;
       break;
   }
 
-  return <Button {...buttonProps} />;
+  return (
+    <>
+      <Button {...buttonProps} />
+      {isDialogOpen && (
+        <Dialog
+          className={styles.dialog}
+          header={levelError}
+          onHide={closeDialog}
+          visible={isDialogOpen}
+          isIconTooltip={true}
+        >
+          <p>{message} ({levelError})</p>
+
+        </Dialog>
+      )}
+    </>
+  );
 };
