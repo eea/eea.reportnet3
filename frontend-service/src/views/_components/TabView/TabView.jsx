613caa4a99f1ca8c168e8d3815d23a5739854487
John <cj@eworx.gr>
2025-06-12 15:34:59 +0300

    Remove user conditions from switching tab views

--- frontend-service/src/views/_components/TabView/TabView.jsx
+++ frontend-service/src/views/_components/TabView/TabView.jsx
@@ -257,11 +257,9 @@ export const TabView = ({
           onTabEditingHeader={onTabEditingHeader}
           onTabHasErrors={onTabHasErrors}
           onTabHeaderClick={event => {
-            if ((isCustodian || isAdmin) && isDataflowCustodian) {
-              onTabHeaderClick(event, tab, index);
-              if (!isUndefined(onTabEditingHeader)) {
-                onTabEditingHeader(false);
-              }
+            onTabHeaderClick(event, tab, index);
+            if (!isUndefined(onTabEditingHeader)) {
+              onTabEditingHeader(false);
             }
           }}
           onTabMouseWheel={onTabMouseWheel}
