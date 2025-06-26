ec7e46bdadae369f426a89039465d0e34740d92b
coroum <ck@eworx.gr>
2025-06-26 11:49:48 +0300

    feat:  - remove providers when changing group in edit Refs  #288174

--- frontend-service/src/views/_components/ManageBusinessDataflow/ManageBusinessDataflow.jsx
+++ frontend-service/src/views/_components/ManageBusinessDataflow/ManageBusinessDataflow.jsx
@@ -225,20 +225,6 @@ export const ManageBusinessDataflow = ({
   };
 
   const onManageBusinessDataflow = async () => {
-    if (selectedGroup) {
-      if (selectedGroup.dataProviderGroupId === state.dataProviderGroupId) {
-        console.log("same");
-      } else {
-        try {
-          await RepresentativeService.deleteAllLeadReporters(dataflowId);
-          // Optional: add a success notification here
-          notificationContext.add({ type: 'DELETE_ALL_LEAD_REPORTERS_SUCCESS' }, true);
-        } catch (error) {
-          console.error('Dataflow - onDeleteAllLeadReporters.', error);
-          notificationContext.add({ type: 'DELETE_ALL_LEAD_REPORTERS_ERROR' }, true);
-        }
-      }
-    }
     if (checkErrors()) return;
 
     try {
