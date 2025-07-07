58078bdb20e6b211caad20878e9860dab3d726b6
leonidas <lz@eworx.gr>
2025-07-07 11:11:07 +0300

    Implementation of subtables under root table for entity type webforms. Refs #289615

--- frontend-service/src/views/Webforms/_components/WebformTable/_components/WebformRecord/WebformRecord.jsx
+++ frontend-service/src/views/Webforms/_components/WebformTable/_components/WebformRecord/WebformRecord.jsx
@@ -321,10 +321,16 @@ export const WebformRecord = ({
           field => !isNil(field?.referencedField?.idPk) && field?.referencedField?.idPk !== rootPkFieldId
         )[0]?.referencedField?.idPk;
 
-        const referencePkValue = record.elements.find(
+        let referencePkValue = record.elements.find(
           elementField =>
             elementField.fieldSchema === referencePkFieldId || elementField.fieldSchemaId === referencePkFieldId
         )?.value;
+
+        if (isEmpty(referencePkValue) && !isNil(referencePkFieldId)) {
+          referencePkValue = record.elements.find(
+            elementField => elementField.fieldSchema === rootPkFieldId || elementField.fieldSchemaId === rootPkFieldId
+          )?.value;
+        }
 
         const fkFields = element?.elements
           .filter(element => !isNil(element.referenceParentField))
