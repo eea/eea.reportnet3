dd386c81ee3c35b7d8501f78a17f274aa2d00b08
Dimitris1990 <dimipetro90@gmail.com>
2025-08-01 17:35:38 +0300

    Adds a new field to hold the first date the reporting dataset was released Refs #266544

--- common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/ReportingDatasetPublicVO.java
+++ common-interfaces/src/main/java/org/eea/interfaces/vo/dataset/ReportingDatasetPublicVO.java
@@ -29,6 +29,9 @@ public class ReportingDatasetPublicVO implements Serializable {
   /** The date released. */
   private Date dateReleased;
 
+  /** The first release date. */
+  private Date firstReleaseDate;
+
   /** The data provider id. */
   private Long dataProviderId;
 
