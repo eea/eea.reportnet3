7fc4578092fe0c27b0b27dc30835933c4628ce57
John <cj@eworx.gr>
2025-10-03 16:08:22 +0300

    feat: add view mode functionality and related messages in entity webforms Refs #289622

--- frontend-service/src/views/Webforms/EntitiesWebform/_functions/Reducers/entitiesWebformReducer.js
+++ frontend-service/src/views/Webforms/EntitiesWebform/_functions/Reducers/entitiesWebformReducer.js
@@ -55,6 +55,9 @@ export const entitiesWebformReducer = (state, { type, payload }) => {
     case 'SET_IS_ADD_ENTITY_ID_DIALOG_VISIBLE':
       return { ...state, isAddEntityIdDialogVisible: payload.value };
 
+    case 'SET_IS_VIEW_MODE':
+      return { ...state, isViewMode: payload.value };
+
     case 'ON_REFRESH':
       return { ...state, isRefresh: payload.value };
 
