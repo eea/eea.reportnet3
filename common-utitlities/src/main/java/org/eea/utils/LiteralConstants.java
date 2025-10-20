71a894fce99ea105510279533222884746907959
PANAGIOTOU Michail <P63925@nrb.be>
2025-10-20 20:01:04 +0300

    Remove timestamp from s3 folder name

--- common-utitlities/src/main/java/org/eea/utils/LiteralConstants.java
+++ common-utitlities/src/main/java/org/eea/utils/LiteralConstants.java
@@ -203,7 +203,7 @@ public final class LiteralConstants {
   public static final String S3_DATA_COLLECTION_PATTERN = "dc-%s";
 
   /** The Constant S3_SNAPSHOT_PATTERN: {@value}. */
-  public static final String S3_SNAPSHOT_PATTERN = "snap-%s-%s";
+  public static final String S3_SNAPSHOT_PATTERN = "snap-%s";
 
   /** The Constant S3_EU_DATASET_PATTERN: {@value}. */
   public static final String S3_EU_DATASET_PATTERN = "eu-%s";
