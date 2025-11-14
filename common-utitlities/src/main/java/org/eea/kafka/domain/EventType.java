68edb667f3911ea6f92e192c953799ce7a9a6aa4
Dimitris1990 <dimipetro90@gmail.com>
2025-11-14 05:30:48 +0200

    Creates new EventType for multiline import warning #290477

--- common-utitlities/src/main/java/org/eea/kafka/domain/EventType.java
+++ common-utitlities/src/main/java/org/eea/kafka/domain/EventType.java
@@ -105,6 +105,10 @@ public enum EventType {
   IMPORT_MISMATCH_OF_DATA_WARNING_EVENT(LiteralConstants.DATA_REPORTING_TOPIC,
       "import_mismatch_of_data_warning_event", true),
 
+  /** The import with read only tables warning event. */
+  IMPORT_MULTILINE_TEXT_CHAR_LIMIT_WARNING_EVENT(LiteralConstants.DATA_REPORTING_TOPIC,
+      "import_multiline_text_char_limit_warning_event", true),
+
   /** The import reporting failed no headers matching event. */
   IMPORT_REPORTING_FAILED_NO_HEADERS_MATCHING_EVENT(LiteralConstants.DATA_REPORTING_TOPIC,
       "import_reporting_no_headers_failed_event", true),
