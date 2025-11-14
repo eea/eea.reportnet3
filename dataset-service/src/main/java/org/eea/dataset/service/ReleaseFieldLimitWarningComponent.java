0ad44918fe9aa0b9ff611461abeb247fd414495a
Dimitris1990 <dimipetro90@gmail.com>
2025-11-14 05:36:15 +0200

    Adds new method to handle the csv line info and send warning event #290477

--- dataset-service/src/main/java/org/eea/dataset/service/ReleaseFieldLimitWarningComponent.java
+++ dataset-service/src/main/java/org/eea/dataset/service/ReleaseFieldLimitWarningComponent.java
@@ -1,8 +1,11 @@
 package org.eea.dataset.service;
 
 import org.eea.datalake.service.model.SpatialFieldInfo;
+import org.eea.datalake.service.model.TextFieldLengthInfo;
 import org.eea.exception.EEAException;
 
 public interface ReleaseFieldLimitWarningComponent {
   void releaseFieldSizeNotification(SpatialFieldInfo spatialFieldInfo, Long dataflowId, Long datasetId) throws EEAException;
+
+  void releaseMultilineTextCharLimitWarning(TextFieldLengthInfo info, Long dataflowId, Long datasetId) throws EEAException;
 }
