9bd4a0050cfdfa2cae2ec98d80ec7516994660ca
Apostolos Boutos <Apostolos.BOUTOS@trasys.gr>
2025-12-05 17:05:38 +0200

    Task #294585 citus empty datasets will no longer be created for bigData dataflows

--- dataset-service/src/main/java/org/eea/dataset/service/impl/DataCollectionServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/DataCollectionServiceImpl.java
@@ -382,7 +382,8 @@ public class DataCollectionServiceImpl implements DataCollectionService {
   @Override
   @Async
   public void updateDataCollection(Long dataflowId, boolean referenceDataflow) {
-    manageDataCollection(dataflowId, null, false, false, false, referenceDataflow, false);
+    final Boolean isBigDataflow = dataflowControllerZuul.isBigDataflow(dataflowId);
+    manageDataCollection(dataflowId, null, false, false, false, referenceDataflow, false, isBigDataflow);
     LOG.info("Successfully updated data collection for dataflowId {}", dataflowId);
   }
 
@@ -404,7 +405,7 @@ public class DataCollectionServiceImpl implements DataCollectionService {
       boolean stopAndNotifySQLErrors, boolean manualCheck, boolean showPublicInfo,
       boolean referenceDataflow, boolean stopAndNotifyPKError) {
 
-    Boolean isBigDataflow = dataflowControllerZuul.isBigDataflow(dataflowId);
+    final Boolean isBigDataflow = dataflowControllerZuul.isBigDataflow(dataflowId);
     if(Boolean.TRUE.equals(isBigDataflow)){
       //check if there are tables converted to Iceberg and convert them back to Parquet
       List<DataSetMetabaseVO> datasets = datasetMetabaseService.findDataSetByDataflowIds(Collections.singletonList(dataflowId));
@@ -423,7 +424,7 @@ public class DataCollectionServiceImpl implements DataCollectionService {
     }
 
     manageDataCollection(dataflowId, dueDate, true, stopAndNotifySQLErrors, manualCheck,
-        referenceDataflow, stopAndNotifyPKError);
+        referenceDataflow, stopAndNotifyPKError, isBigDataflow);
     LOG.info("Managed creating data collection for dataflowId {}", dataflowId);
 
     updateReportingDatasetsVisibility(dataflowId, showPublicInfo);
@@ -477,7 +478,7 @@ public class DataCollectionServiceImpl implements DataCollectionService {
    */
   private void manageDataCollection(Long dataflowId, LocalDateTime dueDate, boolean isCreation,
       boolean stopAndNotifySQLErrors, boolean manualCheck, boolean referenceDataflow,
-      boolean stopAndNotifyPKError) {
+      boolean stopAndNotifyPKError, Boolean isBigDataflow) {
     String time = Timestamp.valueOf(LocalDateTime.now()).toString();
 
     boolean rulesOk = true;
@@ -620,7 +621,7 @@ public class DataCollectionServiceImpl implements DataCollectionService {
         processDataCollectionAndRoles(dataflowId, dueDate, isCreation, manualCheck, time, designs,
             referenceDatasets, representatives, map, dataCollectionIds, datasetIdsEmails,
             referenceDatasetIdsEmails, datasetIdsAndSchemaIds, euDatasetIds, connection, statement,
-            referenceDataflow);
+            referenceDataflow, isBigDataflow);
 
       } catch (SQLException e) {
         LOG.error("Error rolling back manageDataCollection for dataflowId {}. Message: {}", dataflowId, e.getMessage(), e);
@@ -766,7 +767,7 @@ public class DataCollectionServiceImpl implements DataCollectionService {
       Map<Long, String> map, List<Long> dataCollectionIds, Map<Long, List<String>> datasetIdsEmails,
       Map<Long, List<String>> referenceDatasetIdsEmails, Map<Long, String> datasetIdsAndSchemaIds,
       List<Long> euDatasetIds, Connection connection, Statement statement,
-      boolean referenceDataflow) throws SQLException {
+      boolean referenceDataflow, Boolean isBigData) throws SQLException {
     try {
       connection.setAutoCommit(false);
 
@@ -921,7 +922,18 @@ public class DataCollectionServiceImpl implements DataCollectionService {
 
       // 10. Create schemas for each dataset
       // This method will release the lock
-      recordStoreControllerZuul.createSchemas(datasetIdsAndSchemaIds, dataflowId, isCreation, true);
+      if (Boolean.FALSE.equals(isBigData)) {
+          recordStoreControllerZuul.createSchemas(datasetIdsAndSchemaIds, dataflowId, isCreation, true);
+      }
+      else {
+          // Release the lock
+          final String methodSignature = isCreation ? LockSignature.CREATE_DATA_COLLECTION.getValue()
+                  : LockSignature.UPDATE_DATA_COLLECTION.getValue();
+          final Map<String, Object> lockCriteria = new HashMap<>();
+          lockCriteria.put(LiteralConstants.SIGNATURE, methodSignature);
+          lockCriteria.put(LiteralConstants.DATAFLOWID, dataflowId);
+          lockService.removeLockByCriteria(lockCriteria);
+      }
     } catch (SQLException e) {
       LOG.error("Error persisting changes. Rolling back...", e);
       releaseLockAndRollback(connection, dataflowId, isCreation);
