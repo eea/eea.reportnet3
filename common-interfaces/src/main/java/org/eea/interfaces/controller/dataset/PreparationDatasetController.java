b6e79651004c325c2d1d30f85f67e01b973dddec
Christos michael <christos.m.michael@gmail.com>
2026-02-02 13:40:59 +0200

    preparation dataset retrieve data controller tablevueDL

--- common-interfaces/src/main/java/org/eea/interfaces/controller/dataset/PreparationDatasetController.java
+++ common-interfaces/src/main/java/org/eea/interfaces/controller/dataset/PreparationDatasetController.java
@@ -2,6 +2,8 @@ package org.eea.interfaces.controller.dataset;
 
 import java.util.List;
 import org.eea.interfaces.vo.dataset.PreparationDatasetVO;
+import org.eea.interfaces.vo.dataset.TableVO;
+import org.eea.interfaces.vo.dataset.enums.ErrorTypeEnum;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -33,4 +35,19 @@ public interface PreparationDatasetController {
     @DeleteMapping("/preparations/{id}")
     void deletePreparationDatasetById(
             @PathVariable("id") Long preparationId);
+
+    @GetMapping("/preparations/TableValueDatasetDL/{id}")
+    TableVO getPreparationTableValuesDL(
+            @PathVariable("id") Long preparationId,
+            @RequestParam("code") String preparationCode,
+            @RequestParam("idTableSchema") String idTableSchema,
+            @RequestParam(value = "pageNum", defaultValue = "0", required = false) Integer pageNum,
+            @RequestParam(value = "pageSize", required = false) Integer pageSize,
+            @RequestParam(value = "fields", required = false) String fields,
+            @RequestParam(value = "levelError", required = false) ErrorTypeEnum[] levelError,
+            @RequestParam(value = "idRules", required = false) String[] idRules,
+            @RequestParam(value = "fieldSchemaId", required = false) String fieldSchemaId,
+            @RequestParam(value = "fieldValue", required = false) String fieldValue,
+            @RequestParam(value = "qcCodes", required = false) String[] qcCodes
+    );
 }
