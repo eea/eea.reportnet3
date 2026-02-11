248c36028414a3d032e910f68bc5f6f46b74f40a
Apostolos Boutos <Apostolos.BOUTOS@trasys.gr>
2026-02-11 12:02:09 +0200

    Task #294585 Remove any uneccesary citus schema creation in big data
    - Fix the initialization of BigData tables during creation of data collections
    - Update unit tests

--- dataset-service/src/main/java/org/eea/dataset/service/impl/DataCollectionServiceImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/service/impl/DataCollectionServiceImpl.java
@@ -943,27 +943,7 @@ public class DataCollectionServiceImpl implements DataCollectionService {
       LOG.info("Metabase changes completed on DataCollection creation");
 
       // 10. Create schemas for each dataset
-      // If this is not a big dataflow create schemas as normally
-      // otherwise skip schema creation and release locks
-      if (!isBigDataflow) {
-          recordStoreControllerZuul.createSchemas(datasetIdsAndSchemaIds, dataflowId, isCreation, true);
-      }
-      else {
-          // Release the lock
-          final String methodSignature = isCreation ? LockSignature.CREATE_DATA_COLLECTION.getValue()
-                  : LockSignature.UPDATE_DATA_COLLECTION.getValue();
-          final Map<String, Object> lockCriteria = new HashMap<>();
-          lockCriteria.put(LiteralConstants.SIGNATURE, methodSignature);
-          lockCriteria.put(LiteralConstants.DATAFLOWID, dataflowId);
-          lockService.removeLockByCriteria(lockCriteria);
-
-          // command to assign national coordinators and end the dataCollectionProcess.
-          final Map<String, Object> result = new HashMap<>();
-          result.put("dataflowId", dataflowId);
-          result.put("isCreation", isCreation);
-          kafkaSenderUtils.releaseKafkaEvent(EventType.DATACOLLECTION_NATIONAL_COORDINATOR_EVENT,
-                  result);
-      }
+      recordStoreControllerZuul.createSchemas(datasetIdsAndSchemaIds, dataflowId, isCreation, true);
     } catch (SQLException e) {
       LOG.error("Error persisting changes. Rolling back...", e);
       releaseLockAndRollback(connection, dataflowId, isCreation);
