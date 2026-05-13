c586be5e55a008a1f1a81f7f068d0c32dd33b8ea
Maria Markopoulou <mm@eworx.gr>
2026-05-13 10:57:38 +0300

    Exclude ORDER BY clause from big data count queries. Refs 302358

--- dataset-service/src/main/java/org/eea/dataset/service/helper/FileTreatmentHelper.java
+++ dataset-service/src/main/java/org/eea/dataset/service/helper/FileTreatmentHelper.java
@@ -968,7 +968,7 @@ public class FileTreatmentHelper implements DisposableBean {
         if (!filters.getQcCodes().isEmpty()) {
             qcCodes = new String[]{filters.getQcCodes()};
         }
-        StringBuilder filteredQuery = DataLakeDataRetrieverUtils.buildFilteredQuery(dataset, null, null, filters.getFieldValue(), fieldIdMap, filters.getLevelError(), qcCodes, validationTablePath);
+        StringBuilder filteredQuery = DataLakeDataRetrieverUtils.buildFilteredQuery(dataset, null, null, filters.getFieldValue(), fieldIdMap, filters.getLevelError(), qcCodes, validationTablePath, false);
         filteredQuery.append(" limit " + FILE_EXPORT_LIMIT);
         dataQuery.append(filteredQuery);
         return dataQuery;
