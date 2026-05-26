3aab2494e3c02a5ca4596f4bda3ea1b463d81fff
John <cj@eworx.gr>
2026-05-26 12:22:27 +0300

    Enhance dataset deletion handling for prep sets by adding deleteDatasetCode state and adding prep code to Context. Refs #297443

--- frontend-service/src/views/Dataset/Dataset.jsx
+++ frontend-service/src/views/Dataset/Dataset.jsx
@@ -1428,27 +1428,27 @@ export const Dataset = ({ isReferenceDatasetReferenceDataflow }) => {
     let subtitle = metadata?.dataflow.sncData
       ? metadata?.dataflow.bigData
         ? TextUtils.parseText(resourcesContext.messages['sncBigDataDataflowNamed'], {
-            name: `${metadata?.dataflow.name} - ${
+            name: `${metadata?.dataflow.name} ${
               isTestDataset
-                ? resourcesContext.messages['testDataset']
+                ? ` - ${resourcesContext.messages['testDataset']}`
                 : code
-                ? selectedPreparationSet?.datasetName || ''
-                : datasetName
+                ? ` - ${selectedPreparationSet?.datasetName || ''}`
+                : ''
             }`
           })
         : TextUtils.parseText(resourcesContext.messages['sncCitusDataflowNamed'], {
-            name: `${metadata?.dataflow.name} - ${
-              isTestDataset ? resourcesContext.messages['testDataset'] : datasetName
+            name: `${metadata?.dataflow.name} ${
+              isTestDataset ? ` - ${resourcesContext.messages['testDataset']}` : datasetName
             }`
           })
       : metadata?.dataflow.bigData
       ? TextUtils.parseText(resourcesContext.messages['bigDataDataflowNamed'], {
-          name: `${metadata?.dataflow.name} - ${
+          name: `${metadata?.dataflow.name} ${
             isTestDataset
-              ? resourcesContext.messages['testDataset']
+              ? ` - ${resourcesContext.messages['testDataset']}`
               : code
-              ? selectedPreparationSet?.datasetName || ''
-              : datasetName
+              ? ` - ${selectedPreparationSet?.datasetName || ''}`
+              : ''
           }`
         })
       : `${metadata?.dataflow.name} - ${isTestDataset ? resourcesContext.messages['testDataset'] : datasetName}`;
@@ -1583,7 +1583,6 @@ export const Dataset = ({ isReferenceDatasetReferenceDataflow }) => {
     }
     return null;
   };
-
   return layout(
     <SnapshotContext.Provider
       value={{
@@ -1667,13 +1666,17 @@ export const Dataset = ({ isReferenceDatasetReferenceDataflow }) => {
                   editingStatus?.isEditing ||
                   !hasWritePermissions ||
                   isTableDataRestorationInProgress ||
-                  actionsContext.isInProgress
+                  (actionsContext.deleteDatasetProcessing && actionsContext.deleteDatasetCode !== null)
                 }
                 icon={
-                  actionsContext.isInProgress && actionsContext.deleteDatasetProcessing ? 'spinnerAnimate' : 'trash'
+                  (actionsContext.isInProgress && actionsContext.deleteDatasetProcessing) ||
+                  (actionsContext.isInProgress && actionsContext.deleteDatasetCode !== null)
+                    ? 'spinnerAnimate'
+                    : 'trash'
                 }
                 label={
-                  actionsContext.isInProgress && actionsContext.deleteDatasetProcessing
+                  (actionsContext.isInProgress && actionsContext.deleteDatasetProcessing) ||
+                  (actionsContext.isInProgress && actionsContext.deleteDatasetCode !== null)
                     ? resourcesContext.messages['deleteInProgress']
                     : resourcesContext.messages['deleteDatasetData']
                 }
