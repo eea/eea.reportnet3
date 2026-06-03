04a037a303be06c68578355536000b847c740dba
Apostolos Boutos <Apostolos.BOUTOS@trasys.gr>
2026-06-03 17:13:46 +0300

    Bug #304210 Locks during validation on preparation datasets do not work correctly
    - modified convertIcebergToParquetTables() to include only jobs without a preparation code in the eligibility checks as the edit parquet functionality is not implemented for preparation sets yet.

--- dataset-service/src/main/java/org/eea/dataset/controller/DatasetControllerImpl.java
+++ dataset-service/src/main/java/org/eea/dataset/controller/DatasetControllerImpl.java
@@ -3857,7 +3857,11 @@ public class DatasetControllerImpl implements DatasetController {
       return;
     }
 
-    List<JobVO> activeJobsForDatasetId = jobControllerZuul.findActiveJobsRelatedToADatasetId(datasetId, dataflowId, providerId);
+    List<JobVO> activeJobsForDatasetId = jobControllerZuul.findActiveJobsRelatedToADatasetId(datasetId, dataflowId, providerId)
+            .stream()
+            .filter(jobVO -> StringUtils.isBlank(jobVO.getPreparationCode()))
+            .collect(Collectors.toList());
+
     if(activeJobsForDatasetId != null && !activeJobsForDatasetId.isEmpty()){
       List<Long> jobIds = activeJobsForDatasetId.stream().map(JobVO::getId).collect(Collectors.toList());
       LOG.info("Can not convert tables from iceberg to parquet for dataflowId {} datasetId {} providerId {} and user {} because there are active jobs related to the same dataset id. Job ids: {}", dataflowId, datasetId, providerId, username, jobIds);
