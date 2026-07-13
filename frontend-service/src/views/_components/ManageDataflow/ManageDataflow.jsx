2d032ce6b6aef8455dbd53ab3142ff331799cbc1
Christos Karampelas <ck@eworx.gr>
2026-07-13 14:02:07 +0300

    feature: receipt-text-editor-not-multiline(304900)

--- frontend-service/src/views/_components/ManageDataflow/ManageDataflow.jsx
+++ frontend-service/src/views/_components/ManageDataflow/ManageDataflow.jsx
@@ -491,7 +491,7 @@ export const ManageDataflow = ({
               <h4 className={styles.addUserTextLabel}>{resourcesContext.messages['addUserTextToReceiptEdit']}</h4>
               <InputTextarea
                 className={`class`}
-                collapsedHeight={75}
+                collapsedHeight={150}
                 hasMaxCharCounter={true}
                 id="createDataCollectionText"
                 key="createDataCollectionText"
@@ -501,7 +501,6 @@ export const ManageDataflow = ({
                 onFocus={e => {
                   setAddInitialUserText(e.target.value);
                 }}
-                onKeyDown={e => onKeyChange(e)}
                 placeholder={resourcesContext.messages['addUserTextToReceiptNew']}
                 value={addUserText}
               />
