f01e7ab760b6758a354c225635ec8596036883fa
Apostolos Boutos <Apostolos.BOUTOS@trasys.gr>
2026-08-03 19:57:54 +0300

    Feature #304900 RN3: receipt text editor not multiline
    - Modified ReceiptPDFGenerator to print datasets in alphabetical order.

--- dataset-service/src/main/java/org/eea/dataset/service/pdf/ReceiptPDFGenerator.java
+++ dataset-service/src/main/java/org/eea/dataset/service/pdf/ReceiptPDFGenerator.java
@@ -7,6 +7,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.pdfbox.contentstream.PDContentStream;
@@ -258,6 +259,8 @@ public class ReceiptPDFGenerator {
 
 
     spaceBetweenLines = 40f;
+    receipt.getDatasets()
+            .sort(Comparator.comparing(ReportingDatasetVO::getNameDatasetSchema));
     for (ReportingDatasetVO dataset : receipt.getDatasets()) {
       y -= spaceBetweenLines + fontSize;
       y = checkForPageChange(y);
